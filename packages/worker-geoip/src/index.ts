1e801d3276dcf019f5a24ee88c13435bb83495b6
Jeff Wu <jeffywu@pm.me>
2022-10-17 07:25:24 -0700

    update cors

--- packages/worker-geoip/src/index.ts
+++ packages/worker-geoip/src/index.ts
@@ -3,9 +3,17 @@ export default {
     // Simply echos back the cloudflare country information
     try {
       const { country } = (request as any).cf;
-      return new Response(JSON.stringify({ country }));
+      return new Response(JSON.stringify({ country }), {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
     } catch {
-      return new Response(JSON.stringify({ country: 'N/A' }));
+      return new Response(JSON.stringify({ country: 'N/A' }), {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
     }
   },
 };
