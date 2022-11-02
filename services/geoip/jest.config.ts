9594b80701fabd3ab192f7435354e7eef0f50daa
Jeff Wu <jeffywu@pm.me>
2022-11-02 07:21:21 -0700

    moving geoip worker with types resolving

--- services/geoip/jest.config.ts
+++ services/geoip/jest.config.ts
@@ -1,15 +1,16 @@
 /* eslint-disable */
 export default {
-  displayName: 'worker-geoip',
+  displayName: 'geoip',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
+  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/worker-geoip',
+  coverageDirectory: '../../coverage/services/geoip',
 };
