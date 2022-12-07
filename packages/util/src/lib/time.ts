18bede41de6774dfb68e183e134cc142e1ba1b73
Megan Sharon <megan@sprouted.dev>
2022-12-07 15:04:09 -0500

    fix(util): force release

--- packages/util/src/lib/time.ts
+++ packages/util/src/lib/time.ts
@@ -2,6 +2,5 @@ export function getNowSeconds() {
   if (process.env['NODE_ENV'] === 'development' && process.env['FAKE_TIME']) {
     return parseInt(process.env['FAKE_TIME'], 10);
   }
-
   return Math.floor(new Date().getTime() / 1000);
 }
