b2fdc2e62e2f93fb1e50e0e1a19d58fc1027ab66
matthew-garrett <matthew.garrett252@gmail.com>
2023-01-11 16:18:55 -0500

    feat(portfolio): removes comment

--- packages/features/portfolio/src/hooks/use-portfolio-button-bar.tsx
+++ packages/features/portfolio/src/hooks/use-portfolio-button-bar.tsx
@@ -3,8 +3,6 @@ import { PORTFOLIO_ACTIONS } from '@notional-finance/shared-config';
 import { FormattedMessage } from 'react-intl';
 import { useLocation, useHistory } from 'react-router-dom';
 import { ButtonOptionsType } from '@notional-finance/mui';
-
-// NEW HOOK IMPORTS
 import { getNowSeconds } from '@notional-finance/sdk';
 import { ethers } from 'ethers';
 import { useAccount } from '@notional-finance/notionable-hooks';
