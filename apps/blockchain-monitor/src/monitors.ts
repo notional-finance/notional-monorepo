9fd5f44a0abadcd60ea1e9e14d2fda66da1d0cf7
Megan Sharon <megan@sprouted.dev>
2023-01-18 14:38:11 -0500

    build: update timers

--- apps/blockchain-monitor/src/monitors.ts
+++ apps/blockchain-monitor/src/monitors.ts
@@ -8,11 +8,8 @@ import {
 const jobs = {
   dev: new Map([
     [MonitorSchedule.EVERY_MINUTE, []],
-    [
-      MonitorSchedule.EVERY_15_MINUTES,
-      [exchangeRateMonitorMainnet, kpiMonitorMainnet],
-    ],
-    [MonitorSchedule.EVERY_HOUR, []],
+    [MonitorSchedule.EVERY_15_MINUTES, [exchangeRateMonitorMainnet]],
+    [MonitorSchedule.EVERY_HOUR, [kpiMonitorMainnet]],
   ]),
   prod: new Map([
     [MonitorSchedule.EVERY_MINUTE, [exchangeRateMonitorMainnet]],
