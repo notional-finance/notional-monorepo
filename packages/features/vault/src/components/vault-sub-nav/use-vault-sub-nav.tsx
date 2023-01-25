0b3ad7b0bc2ad1bc82de327ba472654be9fe8124
matthew-garrett <matthew.garrett252@gmail.com>
2023-01-25 18:39:56 -0500

    build: addresses comments/fixes css issue

--- packages/features/vault/src/components/vault-sub-nav/use-vault-sub-nav.tsx
+++ packages/features/vault/src/components/vault-sub-nav/use-vault-sub-nav.tsx
@@ -1,15 +1,16 @@
 import { animateScroll as scroll } from 'react-scroll';
+import { FormattedMessage } from 'react-intl';
 import { VAULT_SUB_NAV_ACTIONS } from '@notional-finance/shared-config';
 
 export const useVaultSubNav = () => {
   const subNavData = [
     {
-      label: 'Overview',
+      label: <FormattedMessage defaultMessage={'Overview'} />,
       key: VAULT_SUB_NAV_ACTIONS.OVERVIEW,
       anchor: VAULT_SUB_NAV_ACTIONS.OVERVIEW,
     },
     {
-      label: 'Market Returns',
+      label: <FormattedMessage defaultMessage={'Market Returns'} />,
       key: VAULT_SUB_NAV_ACTIONS.MARKET_RETURNS,
       anchor: VAULT_SUB_NAV_ACTIONS.MARKET_RETURNS,
     },
@@ -19,17 +20,17 @@ export const useVaultSubNav = () => {
     //   anchor: VAULT_SUB_NAV_ACTIONS.YIELD_DRIVERS,
     // },
     {
-      label: 'Strategy Details',
+      label: <FormattedMessage defaultMessage={'Strategy Details'} />,
       key: VAULT_SUB_NAV_ACTIONS.STRATEGY_DETAILS,
       anchor: VAULT_SUB_NAV_ACTIONS.STRATEGY_DETAILS,
     },
     {
-      label: 'Back to top',
+      label: <FormattedMessage defaultMessage={'Back to top'} />,
       key: VAULT_SUB_NAV_ACTIONS.BACK_TO_TOP,
       callback: () => scroll.scrollToTop(),
     },
     {
-      label: 'Full Documentation',
+      label: <FormattedMessage defaultMessage={'Full Documentation'} />,
       key: VAULT_SUB_NAV_ACTIONS.FULL_DOCUMENTATION,
       href: 'https://docs.notional.finance/leveraged-vaults/leveraged-vaults/balancer-aura-wsteth-weth-strategy',
     },
