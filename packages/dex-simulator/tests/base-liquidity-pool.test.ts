50cf2281a289be8586e49b5e8c6fa8ada7c85c86
Jeff Wu <jeff@notional.finance>
2023-02-07 07:23:40 -0800

    test: adding some interim tests for dex simulator

--- packages/dex-simulator/tests/base-liquidity-pool.test.ts
+++ packages/dex-simulator/tests/base-liquidity-pool.test.ts
@@ -1,5 +1,24 @@
+import { BigNumber } from 'ethers';
+import { MockPool } from './harness/MockPool';
+
 describe('base liquidity pool unit tests', () => {
-  // it('returns pro rata claims on lp tokens', () => {});
+  const mockPool = new MockPool(
+    BigNumber.from(10).pow(18),
+    [BigNumber.from(10).pow(18), BigNumber.from(10).pow(18)],
+    [BigNumber.from(10).pow(18), BigNumber.from(10).pow(18)],
+    BigNumber.from(10).pow(18),
+    BigNumber.from(100_000),
+    BigNumber.from(100_000),
+    BigNumber.from(100_000)
+  );
+
+  it('returns pro rata claims on lp tokens', () => {
+    const claims = mockPool.getLPTokenClaims(
+      BigNumber.from(1_000).mul(mockPool.LP_TOKEN_PRECISION)
+    );
+    expect(claims[0]).to;
+  });
+
   // it('returns an oracle valuation of a balance array', () => {
   //   expect(true);
   // });
---
+++ packages/dex-simulator/tests/harness/BalancerPoolHarness.ts
@@ -0,0 +1,211 @@
+import { BigNumber, Signer, ethers } from 'ethers';
+import { PoolTestHarness } from './PoolTestHarness';
+import {
+  BalancerPool,
+  BalancerVault,
+  ERC20,
+} from '@notional-finance/contracts';
+import { getNowSeconds } from '@notional-finance/util';
+
+export class BalancerPoolHarness implements PoolTestHarness {
+  public JoinKind = {
+    EXACT_TOKENS_IN_FOR_BPT_OUT: {
+      kind: 1,
+      // [EXACT_TOKENS_IN_FOR_BPT_OUT, amountsIn, minimumBPT]
+      encoding: ['uint256', 'uint256[]', 'uint256'],
+    },
+    TOKEN_IN_FOR_EXACT_BPT_OUT: {
+      kind: 2,
+      // [TOKEN_IN_FOR_EXACT_BPT_OUT, bptAmountOut, enterTokenIndex]
+      encoding: ['uint256', 'uint256', 'uint256'],
+    },
+    ALL_TOKENS_IN_FOR_EXACT_BPT_OUT: {
+      kind: 3,
+      // [ALL_TOKENS_IN_FOR_EXACT_BPT_OUT, bptAmountOut]
+      encoding: ['uint256', 'uint256'],
+    },
+  };
+
+  // NOTE: Composable Stable Pool V2 has different ExitKind enums
+  public ExitKind = {
+    EXACT_BPT_IN_FOR_ONE_TOKEN_OUT: {
+      kind: 0,
+      // [EXACT_BPT_IN_FOR_ONE_TOKEN_OUT, bptAmountIn, exitTokenIndex]
+      encoding: ['uint256', 'uint256', 'uint256'],
+    },
+    EXACT_BPT_IN_FOR_TOKENS_OUT: {
+      kind: 1,
+      // [EXACT_BPT_IN_FOR_TOKENS_OUT, bptAmountIn]
+      encoding: ['uint256', 'uint256'],
+    },
+    BPT_IN_FOR_EXACT_TOKENS_OUT: {
+      kind: 2,
+      // [BPT_IN_FOR_EXACT_TOKENS_OUT, amountsOut, maxBPTAmountIn]
+      encoding: ['uint256', 'uint256[]', 'uint256'],
+    },
+    MANAGEMENT_FEE_TOKENS_OUT: {
+      kind: 3,
+    },
+  };
+
+  public SwapKind = {
+    GivenIn: 0,
+    GivenOut: 1,
+  };
+
+  constructor(
+    public poolId: string,
+    public balancerPool: BalancerPool,
+    public balancerVault: BalancerVault,
+    public tokens: ERC20[]
+  ) {}
+
+  public async singleSideEntry(
+    signer: Signer,
+    entryTokenIndex: number,
+    entryTokenAmount: BigNumber
+  ) {
+    const tokensIn = Array(this.tokens.length).fill(BigNumber.from(0));
+    tokensIn[entryTokenIndex] = entryTokenAmount;
+
+    return this.multiTokenEntry(signer, tokensIn);
+  }
+
+  public async singleSideExit(
+    signer: Signer,
+    exitTokenIndex: number,
+    lpTokenAmount: BigNumber
+  ) {
+    const address = await signer.getAddress();
+    const userData = ethers.utils.defaultAbiCoder.encode(
+      this.ExitKind.EXACT_BPT_IN_FOR_ONE_TOKEN_OUT.encoding,
+      [
+        this.ExitKind.EXACT_BPT_IN_FOR_ONE_TOKEN_OUT.kind,
+        lpTokenAmount,
+        exitTokenIndex,
+      ]
+    );
+
+    const balanceBefore = await this.tokens[exitTokenIndex].balanceOf(address);
+    const _rcpt = (
+      await this.balancerVault
+        .connect(signer)
+        .exitPool(this.poolId, address, address, {
+          assets: this.tokens.map((_) => _.address),
+          minAmountsOut: Array(this.tokens.length).fill(0),
+          userData,
+          toInternalBalance: false,
+        })
+    ).wait();
+    const balanceAfter = await this.tokens[exitTokenIndex].balanceOf(address);
+
+    // TODO: grab these from events
+    const feesPaid = Array(this.tokens.length).fill(BigNumber.from(0));
+
+    return { tokensOut: balanceAfter.sub(balanceBefore), feesPaid };
+  }
+
+  public async multiTokenEntry(signer: Signer, tokensIn: BigNumber[]) {
+    const address = await signer.getAddress();
+
+    const userData = ethers.utils.defaultAbiCoder.encode(
+      this.JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT.encoding,
+      [this.JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT, tokensIn, 0]
+    );
+
+    const balanceBefore = await this.balancerPool.balanceOf(address);
+    const _rcpt = (
+      await this.balancerVault
+        .connect(signer)
+        .joinPool(this.poolId, address, address, {
+          assets: this.tokens.map((_) => _.address),
+          maxAmountsIn: tokensIn,
+          userData,
+          fromInternalBalance: false,
+        })
+    ).wait();
+    const balanceAfter = await this.balancerPool.balanceOf(address);
+
+    // TODO: grab these from events
+    const feesPaid = Array(this.tokens.length).fill(BigNumber.from(0));
+
+    return { lpTokens: balanceAfter.sub(balanceBefore), feesPaid };
+  }
+
+  public async multiTokenExit(
+    signer: Signer,
+    lpTokenAmount: BigNumber,
+    minTokensOut?: BigNumber[]
+  ) {
+    const address = await signer.getAddress();
+    const userData = ethers.utils.defaultAbiCoder.encode(
+      this.ExitKind.EXACT_BPT_IN_FOR_TOKENS_OUT.encoding,
+      [this.ExitKind.EXACT_BPT_IN_FOR_TOKENS_OUT.kind, lpTokenAmount]
+    );
+
+    const balancesBefore = await Promise.all(
+      this.tokens.map((t) => t.balanceOf(address))
+    );
+    const _rcpt = (
+      await this.balancerVault
+        .connect(signer)
+        .exitPool(this.poolId, address, address, {
+          assets: this.tokens.map((_) => _.address),
+          minAmountsOut: minTokensOut || Array(this.tokens.length).fill(0),
+          userData,
+          toInternalBalance: false,
+        })
+    ).wait();
+    const balancesAfter = await Promise.all(
+      this.tokens.map((t) => t.balanceOf(address))
+    );
+
+    // TODO: grab these from events
+    const feesPaid = Array(this.tokens.length).fill(BigNumber.from(0));
+
+    return {
+      tokensOut: balancesAfter.map((b, i) => b.sub(balancesBefore[i])),
+      feesPaid,
+    };
+  }
+
+  public async trade(
+    signer: Signer,
+    tokensInIndex: number,
+    tokensOutIndex: number,
+    tokensIn: BigNumber
+  ) {
+    const address = await signer.getAddress();
+
+    const balanceBefore = await this.tokens[tokensOutIndex].balanceOf(address);
+    const _rcpt = await (
+      await this.balancerVault.connect(signer).swap(
+        {
+          poolId: this.poolId,
+          kind: this.SwapKind.GivenIn,
+          assetIn: this.tokens[tokensInIndex].address,
+          assetOut: this.tokens[tokensOutIndex].address,
+          amount: tokensIn,
+          userData: '0x',
+        },
+        {
+          sender: address,
+          fromInternalBalance: false,
+          recipient: address,
+          toInternalBalance: false,
+        },
+        0,
+        getNowSeconds() + 1000
+      )
+    ).wait();
+    const balanceAfter = await this.tokens[tokensOutIndex].balanceOf(address);
+
+    // TODO: grab these from events
+    const feesPaid = Array(this.tokens.length).fill(BigNumber.from(0));
+
+    return {
+      tokensOut: balanceAfter.sub(balanceBefore),
+      feesPaid,
+    };
+  }
+}
