ae998bf7e0cf349c6fb4e9c68677a1c735ace5c7
matthew-garrett <matthew.garrett252@gmail.com>
2023-02-15 10:16:16 -0500

    build: fixes side drawer types

--- packages/shared/side-drawer/src/side-drawer-manager/store/use-side-drawer-state.ts
+++ packages/shared/side-drawer/src/side-drawer-manager/store/use-side-drawer-state.ts
@@ -1,10 +1,9 @@
 import { useObservableState } from 'observable-hooks';
-import { SIDE_DRAWERS_TYPE } from '@notional-finance/shared-config';
 import { sideDrawerOpen$, currentSideDrawerKey$ } from './side-drawer-store';
 
 export function useSideDrawerState() {
   const sideDrawerOpen = useObservableState(sideDrawerOpen$, false);
-  const currentSideDrawerKey = useObservableState(currentSideDrawerKey$, null) as SIDE_DRAWERS_TYPE;
+  const currentSideDrawerKey = useObservableState(currentSideDrawerKey$, null);
 
   return {
     sideDrawerOpen,
