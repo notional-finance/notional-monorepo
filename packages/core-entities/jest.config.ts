651eff56f260e5b513b1d0604b5798bc9a402b5d
Jeff Wu <jeff@notional.finance>
2023-02-15 06:17:18 -0800

    refactor: renaming token-balance to core-entities

--- packages/core-entities/jest.config.ts
+++ packages/core-entities/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'token-balance',
+  displayName: 'core-entities',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,6 +11,6 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/token-balance',
+  coverageDirectory: '../../coverage/packages/core-entities',
   setupFilesAfterEnv: ['../../setup-jest.ts'],
 };
