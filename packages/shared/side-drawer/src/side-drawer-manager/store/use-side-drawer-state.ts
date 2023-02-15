6302336b9fe6cf3b435dffb9916172b2a8bca73c
matthew-garrett <matthew.garrett252@gmail.com>
2023-02-15 09:48:49 -0500

    feat(shared): updates type

--- packages/shared/side-drawer/src/side-drawer-manager/store/use-side-drawer-state.ts
+++ packages/shared/side-drawer/src/side-drawer-manager/store/use-side-drawer-state.ts
@@ -1,9 +1,10 @@
 import { useObservableState } from 'observable-hooks';
+import { SIDE_DRAWERS_TYPE } from '@notional-finance/shared-config';
 import { sideDrawerOpen$, currentSideDrawerKey$ } from './side-drawer-store';
 
 export function useSideDrawerState() {
   const sideDrawerOpen = useObservableState(sideDrawerOpen$, false);
-  const currentSideDrawerKey = useObservableState(currentSideDrawerKey$, null);
+  const currentSideDrawerKey = useObservableState(currentSideDrawerKey$, null) as SIDE_DRAWERS_TYPE;
 
   return {
     sideDrawerOpen,
