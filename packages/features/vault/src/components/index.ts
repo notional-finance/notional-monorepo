19f7fb68e48525494fe4a2f384bb776436131c6c
matthew-garrett <matthew.garrett252@gmail.com>
2023-02-15 10:55:11 -0500

    feat(vault): fixes after rebase

--- packages/features/vault/src/components/index.ts
+++ packages/features/vault/src/components/index.ts
@@ -1,5 +1,4 @@
 export * from './confirm-vault-txn/confirm-vault-txn';
-export * from './performance-chart/performance-chart';
 // export * from './slippage-info-box/slippage-info-box';
 export * from './vault-description/vault-description';
 export * from './vault-sub-nav/vault-sub-nav';
