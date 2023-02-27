ced9bcdd700f21bbb8ce3f693366daf9f41d69b0
matthew-garrett <matthew.garrett252@gmail.com>
2023-02-27 15:48:46 -0500

    feat(vault): adds deposit collateral sidedrawer

--- packages/features/vault/src/side-drawers/components/vault-side-drawer.tsx
+++ packages/features/vault/src/side-drawers/components/vault-side-drawer.tsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
 import { ActionSidebar, ToggleSwitchProps } from '@notional-finance/mui';
+import { useLocation } from 'react-router-dom';
 import { TransactionData } from '@notional-finance/trade';
 import { VaultAccount } from '@notional-finance/sdk';
+import { useQueryParams } from '@notional-finance/utils';
 import {
   TransactionConfirmation,
   TradeActionButton,
 } from '@notional-finance/trade';
 import { VaultDetailsTable } from '@notional-finance/risk';
 import { VAULT_ACTIONS } from '@notional-finance/shared-config';
+import { VaultActionContext } from '../../vault-view/vault-action-provider';
+import { formatMaturity } from '@notional-finance/helpers';
 import { useHistory } from 'react-router';
 import { messages } from '../messages';
 
@@ -35,15 +40,25 @@ export const VaultSideDrawer = ({
   updatedVaultAccount,
 }: VaultSideDrawerProps) => {
   const history = useHistory();
+  const { confirm } = useQueryParams();
+  const { pathname: currentPath } = useLocation();
+  const confirmRoute = !!confirm;
+  const {
+    state: { vaultAccount },
+  } = useContext(VaultActionContext);
 
-  return transactionData ? (
+  const formattedMaturity = vaultAccount?.maturity
+    ? formatMaturity(vaultAccount?.maturity)
+    : '';
+
+  return transactionData && confirmRoute ? (
     <TransactionConfirmation
       heading={transactionData?.transactionHeader}
-      onCancel={() => history.push('')}
+      onCancel={() => history.push(`${currentPath}/${action}`)}
       transactionProperties={transactionData?.transactionProperties}
       buildTransactionCall={transactionData?.buildTransactionCall}
       showDrawer={false}
-      onReturnToForm={() => history.push('')}
+      onReturnToForm={() => history.push(`${currentPath}/${action}`)}
     />
   ) : (
     <ActionSidebar
@@ -61,6 +76,7 @@ export const VaultSideDrawer = ({
         <VaultDetailsTable
           key={'vault-risk-table'}
           updatedVaultAccount={updatedVaultAccount}
+          maturity={formattedMaturity}
           vaultAddress={vaultAddress}
         />
       )}
