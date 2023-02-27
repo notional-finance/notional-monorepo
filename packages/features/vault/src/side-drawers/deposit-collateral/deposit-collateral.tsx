b7202a3001ffe5b7db1b06b1e1194933f61fb20d
matthew-garrett <matthew.garrett252@gmail.com>
2023-02-27 15:48:46 -0500

    feat(vault): adds deposit collateral sidedrawer

--- packages/features/vault/src/side-drawers/deposit-collateral/deposit-collateral.tsx
+++ packages/features/vault/src/side-drawers/deposit-collateral/deposit-collateral.tsx
@@ -1,48 +1,45 @@
+import { useContext } from 'react';
 import { WalletDepositInput } from '@notional-finance/trade';
 import { VAULT_ACTIONS } from '@notional-finance/shared-config';
-import { useParams } from 'react-router-dom';
 import { VaultSideDrawer } from '../components/vault-side-drawer';
+import { VaultActionContext } from '../../vault-view/vault-action-provider';
 import { useDepositCollateral } from './use-deposit-collateral';
 import { messages } from '../messages';
 
-interface VaultParams {
-  vaultAddress: string;
-  sideDrawerKey?: VAULT_ACTIONS;
-}
-
 export const DepositCollateral = () => {
-  const { vaultAddress } = useParams<VaultParams>();
-
   const {
-    canSubmit,
-    transactionData,
-    depositError,
-    targetLeverageRatio,
-    primaryBorrowSymbol,
-    updatedVaultAccount,
-    updateDepositCollateralState,
-  } = useDepositCollateral(vaultAddress);
+    updateState,
+    state: {
+      primaryBorrowSymbol,
+      updatedVaultAccount,
+      vaultAddress,
+      buildTransactionCall,
+    },
+  } = useContext(VaultActionContext);
+  const transactionData = useDepositCollateral();
+  const canSubmit = buildTransactionCall ? true : false;
+  const currentVaultAddress = vaultAddress || '';
 
   return (
     <VaultSideDrawer
       action={VAULT_ACTIONS.DEPOSIT_COLLATERAL}
       canSubmit={canSubmit}
       transactionData={transactionData}
-      vaultAddress={vaultAddress}
+      vaultAddress={currentVaultAddress}
       updatedVaultAccount={updatedVaultAccount}
     >
-      {primaryBorrowSymbol && targetLeverageRatio && (
+      {primaryBorrowSymbol && (
         <WalletDepositInput
           availableTokens={[primaryBorrowSymbol]}
           selectedToken={primaryBorrowSymbol}
           onChange={({ inputAmount, hasError }) => {
-            updateDepositCollateralState({
+            updateState({
               depositAmount: inputAmount,
               hasError,
             });
           }}
           inputLabel={messages[VAULT_ACTIONS.DEPOSIT_COLLATERAL]['inputLabel']}
-          errorMsgOverride={depositError}
+          errorMsgOverride={undefined}
         />
       )}
     </VaultSideDrawer>
