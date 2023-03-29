ee5a820e6e7bbf315a143904e28c02a8494623fc
Jeff Wu <jeff@notional.finance>
2023-03-29 05:55:55 -0700

    fix: removing unused enums

--- packages/features/vault/src/side-drawers/manage-vault/use-manage-vault.tsx
+++ packages/features/vault/src/side-drawers/manage-vault/use-manage-vault.tsx
@@ -30,28 +30,15 @@ export function useManageVault() {
       label: <FormattedMessage defaultMessage={'Withdraw / Exit Vault'} />,
       link: `${currentPath}/${VAULT_ACTIONS.WITHDRAW_VAULT}`,
     },
-    [VAULT_ACTIONS.ESTABLISH_ACCOUNT]: {
-      label: <FormattedMessage defaultMessage={'Establish vault'} />,
-      link: `${currentPath}/${VAULT_ACTIONS.ESTABLISH_ACCOUNT}`,
-    },
     [VAULT_ACTIONS.CREATE_VAULT_POSITION]: {
       label: <FormattedMessage defaultMessage={'Create Vault Position'} />,
       link: `${currentPath}/${VAULT_ACTIONS.CREATE_VAULT_POSITION}`,
     },
-    [VAULT_ACTIONS.DELEVERAGE_VAULT]: {
-      label: <FormattedMessage defaultMessage={'DELEVERAGE_VAULT'} />,
-      link: `${currentPath}/${VAULT_ACTIONS.DELEVERAGE_VAULT}`,
-    },
-
     [VAULT_ACTIONS.WITHDRAW_VAULT_POST_MATURITY]: {
       label: (
         <FormattedMessage defaultMessage={'Withdraw Vault Post Maturity'} />
       ),
       link: `${currentPath}/${VAULT_ACTIONS.WITHDRAW_VAULT_POST_MATURITY}`,
-    },
-    [VAULT_ACTIONS.DELEVERAGE_VAULT_DEPOSIT]: {
-      label: <FormattedMessage defaultMessage={'DELEVERAGE_VAULT_DEPOSIT'} />,
-      link: `${currentPath}/${VAULT_ACTIONS.DELEVERAGE_VAULT}`,
     },
   };
 
