a1bbc594467e2952dcb4d9c036fc833d16a9317a
matthew-garrett <matthew.garrett252@gmail.com>
2023-04-28 10:03:48 -0400

    build: reorders landing page components

--- apps/web/src/containers/LandingPageView/index.tsx
+++ apps/web/src/containers/LandingPageView/index.tsx
@@ -31,10 +31,9 @@ export const LandingPageView = () => {
         <HowItWorks />
         <AuditAndSecurity />
         <StatsAndTransparency />
+        <OurBackers />
         <JoinOurCommunity />
         {/* NOTE* This is a temporary fix to get the old landing page components to render correctly. Will remove this once the rest of the components are updated */}
-
-        <OurBackers />
         <ThemeProvider theme={lightTheme}>
           <AskedQuestionsAccordion />
           <FromTheBlog />
