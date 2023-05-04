11722f1ecb3b25a2bc089033e5445472a81af3bc
Jeff Wu <jeff@notional.finance>
2023-05-04 09:48:13 -0700

    refactor: changing definitions to match graphql

--- packages/core-entities/src/Definitions.ts
+++ packages/core-entities/src/Definitions.ts
@@ -4,7 +4,7 @@ import { BaseLiquidityPool } from './exchanges';
 export enum Network {
   All = 'all',
   Mainnet = 'mainnet',
-  Goerli = 'goerli',
+  ArbitrumOne = 'ArbitrumOne',
 }
 
 export enum TokenInterface {
@@ -15,45 +15,70 @@ export enum TokenInterface {
   /** ERC1155 tokens */
   ERC1155 = 'ERC1155',
   /** ERC721 NFTs */
-  ERC721 = 'ERC1155',
+  ERC721 = 'ERC721',
   /** Special handling for ETH */
   ETH = 'ETH',
   /** Special handling for WETH */
   WETH = 'WETH',
-  /** Notional internal ERC20 balances, denominated as 8 decimals  */
-  Notional_InternalERC20 = 'Notional_InternalERC20',
   /** Non-crypto denominated fiat balance (i.e. USD, JPY, EUR) */
   FIAT = 'FIAT',
 }
 
+export enum TokenType {
+  Underlying = 'Underlying',
+  nToken = 'nToken',
+  WrappedfCash = 'WrappedfCash',
+  PrimeCash = 'PrimeCash',
+  PrimeDebt = 'PrimeDebt',
+  fCash = 'fCash',
+  VaultShare = 'VaultShare',
+  VaultDebt = 'VaultDebt',
+  VaultCash = 'VaultCash',
+  NOTE = 'NOTE',
+}
+
 export interface TokenDefinition {
+  /** Defines the ERC1155 or ERC721 id of the token, if it exists */
+  id: string;
   /** Address of the token */
   address: string;
   /** Network the address refers to */
   network: Network;
+  /** String name of the token */
+  name: string;
   /** String based token symbol */
   symbol: string;
   /** Number of native decimal places for the token */
   decimalPlaces: number;
   /** Defines the token standard this token relies on */
   tokenInterface: TokenInterface;
-  /** Defines the ERC1155 or ERC721 id of the token, if it exists */
-  id?: string;
+  /** Defines the token type */
+  tokenType: TokenType;
+  /** Link to the underlying token definition, if it exists */
+  underlying?: TokenDefinition;
   /** Defines the maturity of the token, if it exists */
   maturity?: number;
+  /** Vault Address */
+  vaultAddress?: string;
+  /** Only true for fCash assets */
+  isFCashDebt?: boolean;
 }
 
-export enum OracleInterface {
+export enum OracleType {
   /** Standard Chainlink IAggregator interface */
   Chainlink,
+  fCashOracleRate,
+  fCashSettlementRate,
+  PrimeCashToUnderlyingOracleInterestRate,
+  MoneyMarketToUnderlyingOracleInterestRate,
+  PrimeCashToUnderlyingExchangeRate,
+  PrimeCashToMoneyMarketExchangeRate,
+  PrimeDebtToUnderlyingExchangeRate,
+  PrimeDebtToMoneyMarketExchangeRate,
+  MoneyMarketToUnderlyingExchangeRate,
+  VaultShareOracleRate,
   /** Balancer V2 time weighted average price oracle */
   BalancerV2_TWAP,
-  /** Compound V2 cToken exchange rate */
-  CompoundV2_cToken,
-  /** Notional time weighted average interest rate oracle */
-  Notional_InterestRate,
-  /** Notional leveraged vault price oracle */
-  Notional_VaultOracle,
 }
 
 export interface OracleDefinition {
@@ -62,17 +87,11 @@ export interface OracleDefinition {
   /** Network the address refers to */
   network: Network;
   /** Type of oracle interface */
-  oracleInterface: OracleInterface;
-  /**
-   * Base for the oracle, rate is quoted as 1 unit of this token.
-   * Must be a valid symbol on the same network.
-   */
-  base: string;
-  /**
-   * Quote for the oracle, rate is how many tokens 1 unit of base will purchase.
-   * Must be a valid symbol on the same network
-   */
-  quote: string;
+  oracleType: OracleType;
+  /** Base for the oracle, rate is quoted as 1 unit of this token.  */
+  base: TokenDefinition;
+  /** Quote for the oracle, rate is how many tokens 1 unit of base will purchase.  */
+  quote: TokenDefinition;
   /** Number of decimal places the rate is quoted in */
   decimalPlaces: number;
   /** Maximum update cadence in seconds, if defined. Typically only valid for off-chain oracles */
@@ -80,14 +99,11 @@ export interface OracleDefinition {
 }
 
 export interface ExchangeRate {
-  /** Symbol of the base currency */
-  base: string;
-  /** Symbol of the quote currency */
-  quote: string;
+  oracle: OracleDefinition;
   /** Exchange rate in RATE_PRECISION */
   rate: BigNumber;
   /** Timestamp of the last update, can be used to check for value freshness */
-  validTimestamp: number;
+  timestamp: number;
 }
 
 export interface PoolDefinition {
