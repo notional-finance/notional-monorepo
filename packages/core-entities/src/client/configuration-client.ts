c1a9485a4f6231f956d73726416b8bd1d1b9d285
Jeff Wu <jeff@notional.finance>
2023-05-09 06:19:08 -0700

    feat: cleaning up inferred type

--- packages/core-entities/src/client/configuration-client.ts
+++ packages/core-entities/src/client/configuration-client.ts
@@ -1,9 +1,9 @@
 import { ClientRegistry } from '../registry/client-registry';
-import { AllConfigurationQuery } from '../.graphclient';
 import { Routes } from '../server';
+import { AllConfigurationQuery } from '../server/configuration-server';
 
 export class ConfigurationClient extends ClientRegistry<AllConfigurationQuery> {
-  protected cachePath = Routes.Configuration
+  protected cachePath = Routes.Configuration;
 
   // TODO: implement getters as required for convenience
 }
