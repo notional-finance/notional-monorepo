d037e37b540ad77a68e0f98b32f7abf82447a475
Jeff Wu <jeff@notional.finance>
2023-05-15 06:58:50 -0700

    fix: fixing type issues on account server

--- packages/core-entities/src/server/account-server.ts
+++ packages/core-entities/src/server/account-server.ts
@@ -1,12 +1,12 @@
 import { Network } from '@notional-finance/util';
-import { loadGraphClientDeferred, ServerRegistry } from './server-registry';
+import { AccountDefinition, CacheSchema } from '..';
+import { ServerRegistry } from './server-registry';
 
 export class AccountServer extends ServerRegistry<AccountDefinition> {
   /** Returns all the active accounts on the network */
-  protected async _refresh(network: Network) {
-    // const { AllConfigurationDocument } = await loadGraphClientDeferred();
-    // return this._fetchUsingGraph(network, AllConfigurationDocument, (r) => {
-    //   return { [network]: r };
-    // });
+  protected async _refresh(
+    _network: Network
+  ): Promise<CacheSchema<AccountDefinition>> {
+    throw Error('Unimplemented');
   }
 }
