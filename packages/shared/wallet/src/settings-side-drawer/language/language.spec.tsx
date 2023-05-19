92c9cc121a4986b79a93174de192c2fa52ffa7b4
Jeff Wu <jeff@notional.finance>
2023-05-19 10:19:51 -0700

    test: delete bullshit

--- packages/shared/wallet/src/settings-side-drawer/language/language.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import Language from './language';
-
-describe('Language', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Language />);
-    expect(baseElement).toBeTruthy();
-  });
-});
--- packages/shared/wallet/src/settings-side-drawer/settings-side-drawer.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import SettingsSideDrawer from './settings-side-drawer';
-
-describe('SettingsSideDrawer', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<SettingsSideDrawer />);
-    expect(baseElement).toBeTruthy();
-  });
-});
