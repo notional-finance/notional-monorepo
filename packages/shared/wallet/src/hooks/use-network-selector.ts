a571a21433955d1230954bdfdbc3334c0e7272ba
matthew-garrett <matthew.garrett252@gmail.com>
2023-06-01 12:05:41 -0400

    build: rebase updates

--- packages/shared/wallet/src/hooks/use-network-selector.ts
+++ packages/shared/wallet/src/hooks/use-network-selector.ts
@@ -1,14 +1,4 @@
-<<<<<<< feature/updates-network-selectors:packages/shared/wallet/src/hooks/use-network-selector.ts
-import { useOnboard, useNotional } from '@notional-finance/notionable-hooks';
-import {
-  chains,
-  chainEntities,
-  switchNetwork,
-} from '@notional-finance/notionable';
 import { Network } from '@notional-finance/util';
-=======
-import { CHAIN_NAMES } from '@notional-finance/shared-config';
->>>>>>> build: removes use onboard hook:packages/shared/wallet/src/network-selector/use-network-selector.ts
 import { defineMessages } from 'react-intl';
 
 export function useNetworkSelector() {
