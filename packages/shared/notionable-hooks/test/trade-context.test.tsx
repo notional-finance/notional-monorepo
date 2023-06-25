32f27d9aafa26f6eacb545ed616c8e8f3a90ec6b
Jeff Wu <jeff@notional.finance>
2023-06-25 12:28:29 -0700

    test: initial trade context tests passing

--- packages/shared/notionable-hooks/test/trade-context.test.tsx
+++ packages/shared/notionable-hooks/test/trade-context.test.tsx
@@ -1,16 +1,33 @@
 import { AccountFetchMode } from '@notional-finance/core-entities';
-import { TradeContext, createTradeContext, useTradeContext } from '../src';
+import {
+  NotionalContext,
+  useGlobalContext,
+  useNotionalContext,
+  useTradeContext,
+} from '../src';
 import { Network } from '@notional-finance/util';
 import { renderHook, act } from '@testing-library/react-hooks';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter5Adapter } from 'use-query-params/adapters/react-router-5';
+import { useEffect } from 'react';
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  const globalState = useGlobalContext();
+
+  const { updateState } = globalState;
+  useEffect(() => {
+    updateState({
+      selectedNetwork: Network.ArbitrumOne,
+    });
+  }, [updateState]);
+
   return (
     <BrowserRouter>
       <QueryParamProvider adapter={ReactRouter5Adapter}>
-        {children}
+        <NotionalContext.Provider value={globalState}>
+          {children}
+        </NotionalContext.Provider>
       </QueryParamProvider>
     </BrowserRouter>
   );
@@ -23,14 +40,65 @@ describe.withForkAndRegistry(
   },
   'Trade Context',
   () => {
-    it('loads a trade context', () => {
-      const { result } = renderHook(() => useTradeContext('MintNToken'), {
-        wrapper: Wrapper,
+    const renderTradeContext = async (trade: string) => {
+      const r = renderHook(
+        () => {
+          const { globalState, globalState$, updateNotional } =
+            useNotionalContext();
+          const { state, state$, updateState } = useTradeContext(trade);
+          return {
+            state,
+            updateState,
+            state$,
+            globalState,
+            globalState$,
+            updateNotional,
+          };
+        },
+        {
+          wrapper: Wrapper,
+        }
+      );
+
+      await r.waitFor(() => {
+        return (
+          r.result.current.globalState.isNetworkReady &&
+          r.result.current.state.isReady
+        );
       });
 
-      expect(result.current.updateState).toBeDefined();
-      expect(result.current.state).toBeDefined();
-      expect(result.current.state$).toBeDefined();
+      return r;
+    };
+
+    describe('Mint nToken', () => {
+      it('properly filters currencies', async () => {
+        const { result, waitForNextUpdate } = await renderTradeContext(
+          'MintNToken'
+        );
+        act(() => {
+          result.current.updateState({ selectedDepositToken: 'USDC' });
+        });
+
+        await waitForNextUpdate();
+
+        expect(result.current.state.selectedCollateralToken).toBe('nUSDC');
+      });
+    });
+
+    describe('Lend Fixed', () => {
+      it('properly filters currencies', async () => {
+        const { result, waitForNextUpdate } = await renderTradeContext(
+          'LendFixed'
+        );
+
+        act(() => {
+          result.current.updateState({ selectedDepositToken: 'USDC' });
+        });
+
+        await waitForNextUpdate();
+
+        expect(result.current.state.availableCollateralTokens).toHaveLength(2);
+      });
     });
   }
 );
