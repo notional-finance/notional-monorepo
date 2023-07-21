6612e60615643ddc7461d9f80c3100eebbb10e92
Jeff Wu <jeff@notional.finance>
2023-07-21 10:08:50 -0700

    fix: adding leverage slider inputs

--- packages/shared/trade/src/leverage-slider/leverage-slider.tsx
+++ packages/shared/trade/src/leverage-slider/leverage-slider.tsx
@@ -1,4 +1,5 @@
 import { SliderInput, useSliderInputRef } from '@notional-finance/mui';
+import { defineMessage } from 'react-intl';
 import { BaseContext } from '@notional-finance/notionable-hooks';
 import { useContext, useEffect } from 'react';
 import { MessageDescriptor } from 'react-intl';
@@ -25,7 +26,13 @@ export const LeverageSlider = ({
   context,
 }: LeverageSliderProps) => {
   const {
-    state: { riskFactorLimit },
+    state: {
+      riskFactorLimit,
+      debtBalance,
+      collateralBalance,
+      debt,
+      collateral,
+    },
     updateState,
   } = useContext(context);
   const { sliderInputRef, setSliderInput } = useSliderInputRef();
@@ -61,6 +68,14 @@ export const LeverageSlider = ({
       rightCaption={rightCaption}
       bottomCaption={bottomCaption}
       inputLabel={inputLabel}
+      sliderLeverageInfo={{
+        debtHeading: defineMessage({ defaultMessage: 'Borrow Amount' }),
+        assetHeading: defineMessage({ defaultMessage: 'Asset Amount' }),
+        debtSuffix: debt ? ` ${debt.symbol}` : '',
+        debtValue: debt && debtBalance?.abs().toFloat(),
+        assetSuffix: collateral ? ` ${collateral.symbol}` : '',
+        assetValue: collateral && collateralBalance?.toFloat(),
+      }}
     />
   );
 };
