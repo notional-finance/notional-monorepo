b07a02f36ca00915e56a4f05e443e05005a5b97f
Jeff Wu <jeff@notional.finance>
2023-07-23 05:55:26 -0700

    feat: add borrow fixed and variable

--- packages/features/borrow/src/borrow-fixed/components/borrow-fixed-sidebar.tsx
+++ packages/features/borrow/src/borrow-fixed/components/borrow-fixed-sidebar.tsx
@@ -1,68 +1,34 @@
-import { useEffect, useCallback } from 'react';
+import { useContext } from 'react';
+import { ActionSidebar, useCurrencyInputRef } from '@notional-finance/mui';
 import {
-  Maturities,
-  ActionSidebar,
-  useCurrencyInputRef,
-} from '@notional-finance/mui';
-import {
-  TransactionConfirmation,
   TradeActionButton,
-  LendBorrowInput,
-  // CollateralSelect,
-  TokenApprovalView,
-  // tradeErrors,
+  Confirmation2,
+  DepositInput,
+  MaturitySelect,
 } from '@notional-finance/trade';
-import { RiskSlider, AccountRiskTable } from '@notional-finance/risk';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
-import { LEND_BORROW } from '@notional-finance/shared-config';
+import { PRODUCTS } from '@notional-finance/shared-config';
 import { defineMessage, FormattedMessage } from 'react-intl';
-import { updateBorrowState } from '../../store/borrow-store';
-import { useBorrow } from '../../store/use-borrow';
-import { useBorrowTransaction } from '../../store/use-borrow-transaction';
-import { BorrowParams } from '../borrow-fixed';
+import { BorrowFixedContext } from '../borrow-fixed';
 
 export const BorrowFixedSidebar = () => {
   const {
-    selectedDepositToken: selectedToken,
-    selectedCollateralToken: selectedCollateral,
-  } = useParams<BorrowParams>();
-  const availableCurrencies = [] as string[];
-  const {
-    maturityData,
-    selectedMarketKey,
-    canSubmit,
-    updatedAccountData,
-    // insufficientCollateralError,
-    borrowToPortfolio,
-    warningMsg,
-  } = useBorrow(selectedToken);
-  const txnData = useBorrowTransaction(selectedToken);
-  const history = useHistory();
-  const { pathname, search } = useLocation();
+    state: { canSubmit, populatedTransaction, confirm },
+    updateState,
+  } = useContext(BorrowFixedContext);
   const { currencyInputRef } = useCurrencyInputRef();
+  const handleSubmit = () => {
+    updateState({ confirm: true });
+  };
 
-  const handleTxnCancel = useCallback(() => {
-    history.push(pathname);
-  }, [history, pathname]);
-
-  useEffect(() => {
-    if (search.includes('confirm=true')) {
-      handleTxnCancel();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return txnData ? (
-    <TransactionConfirmation
+  return confirm && populatedTransaction ? (
+    <Confirmation2
       heading={
         <FormattedMessage
           defaultMessage="Borrow Order"
           description="section heading"
         />
       }
-      onCancel={handleTxnCancel}
-      transactionProperties={txnData?.transactionProperties}
-      buildTransactionCall={txnData?.buildTransactionCall}
+      context={BorrowFixedContext}
     />
   ) : (
     <ActionSidebar
@@ -75,104 +41,28 @@ export const BorrowFixedSidebar = () => {
           'Borrow with confidence, fixed rates lock in what you pay.',
         description: 'helptext',
       })}
+      handleSubmit={handleSubmit}
       CustomActionButton={TradeActionButton}
       canSubmit={canSubmit}
-      advancedToggle={{
-        isChecked: borrowToPortfolio,
-        onToggle: (isChecked) => {
-          updateBorrowState({ borrowToPortfolio: isChecked });
-        },
-        label: <FormattedMessage defaultMessage={'Borrow To Portfolio'} />,
-      }}
     >
-      <Maturities
-        maturityData={maturityData || []}
-        onSelect={(selectedMarketKey) => {
-          updateBorrowState({ selectedMarketKey });
-        }}
-        selectedfCashId={selectedMarketKey || undefined}
+      <DepositInput
+        ref={currencyInputRef}
+        inputRef={currencyInputRef}
+        context={BorrowFixedContext}
+        newRoute={(newToken) => `/${PRODUCTS.BORROW_FIXED}/${newToken}`}
         inputLabel={defineMessage({
-          defaultMessage: '1. Select a maturity & fix your rate',
+          defaultMessage: '1. How much do you want to borrow?',
           description: 'input label',
         })}
       />
-      {availableCurrencies.length > 0 && selectedToken && (
-        <LendBorrowInput
-          ref={currencyInputRef}
-          inputRef={currencyInputRef}
-          availableTokens={availableCurrencies}
-          selectedToken={selectedToken}
-          isRemoveAsset={false}
-          cashOrfCash={'Cash'}
-          warningMsg={warningMsg}
-          lendOrBorrow={LEND_BORROW.BORROW}
-          selectedMarketKey={selectedMarketKey}
-          onChange={({
-            selectedToken: newToken,
-            inputAmount,
-            hasError,
-            netfCashAmount,
-          }) => {
-            if (newToken !== selectedToken) {
-              history.push(
-                `/${LEND_BORROW.BORROW}/${newToken}/${selectedCollateral}`
-              );
-            }
-            updateBorrowState({
-              inputAmount,
-              hasError,
-              fCashAmount: netfCashAmount,
-            });
-          }}
-          inputLabel={defineMessage({
-            defaultMessage: '2. How much do you want to borrow?',
-            description: 'input label',
-          })}
-        />
-      )}
-      {/* 
-      TODO: Figure out why this causes the page to crash
-      {collateralCurrencies.length > 0 && selectedCollateral && (
-        <CollateralSelect
-          tightMarginTop
-          availableTokens={collateralCurrencies}
-          selectedToken={selectedCollateral}
-          selectedBorrowMarketKey={selectedMarketKey}
-          errorMsg={
-            insufficientCollateralError
-              ? tradeErrors.insufficientCollateral
-              : undefined
-          }
-          inputLabel={defineMessage({
-            defaultMessage:
-              '3. How much additional collateral do you want to deposit?',
-            description: 'input label',
-          })}
-          onChange={(
-            newCollateral,
-            collateralAction,
-            hasCollateralError,
-            collateralApy,
-            collateralSymbol
-          ) => {
-            if (newCollateral !== selectedCollateral) {
-              history.push(
-                `/${LEND_BORROW.BORROW}/${selectedToken}/${newCollateral}`
-              );
-            }
-
-            updateBorrowState({
-              collateralAction,
-              hasCollateralError,
-              collateralApy,
-              collateralSymbol,
-            });
-          }}
-        />
-      )} */}
-      <TokenApprovalView symbol={selectedCollateral} />
-      <RiskSlider updatedAccountData={updatedAccountData} />
-      <AccountRiskTable updatedAccountData={updatedAccountData} />
+      <MaturitySelect
+        context={BorrowFixedContext}
+        category={'Debt'}
+        inputLabel={defineMessage({
+          defaultMessage: '2. Select a maturity & fix your rate',
+          description: 'input label',
+        })}
+      />
     </ActionSidebar>
   );
 };
--- packages/features/borrow/src/borrow-variable/components/borrow-variable-sidebar.tsx
+++ packages/features/borrow/src/borrow-variable/components/borrow-variable-sidebar.tsx
@@ -1,68 +1,33 @@
-import { useEffect, useCallback } from 'react';
+import { useContext } from 'react';
+import { ActionSidebar, useCurrencyInputRef } from '@notional-finance/mui';
 import {
-  Maturities,
-  ActionSidebar,
-  useCurrencyInputRef,
-} from '@notional-finance/mui';
-import {
-  TransactionConfirmation,
   TradeActionButton,
-  LendBorrowInput,
-  // CollateralSelect,
-  TokenApprovalView,
-  // tradeErrors,
+  Confirmation2,
+  DepositInput,
 } from '@notional-finance/trade';
-import { RiskSlider, AccountRiskTable } from '@notional-finance/risk';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
-import { LEND_BORROW } from '@notional-finance/shared-config';
+import { PRODUCTS } from '@notional-finance/shared-config';
 import { defineMessage, FormattedMessage } from 'react-intl';
-import { updateBorrowState } from '../../store/borrow-store';
-import { useBorrow } from '../../store/use-borrow';
-import { useBorrowTransaction } from '../../store/use-borrow-transaction';
-import { BorrowParams } from '../borrow-variable';
+import { BorrowVariableContext } from '../borrow-variable';
 
 export const BorrowVariableSidebar = () => {
   const {
-    selectedDepositToken: selectedToken,
-    selectedCollateralToken: selectedCollateral,
-  } = useParams<BorrowParams>();
-  const availableCurrencies = [] as string[];
-  const {
-    maturityData,
-    selectedMarketKey,
-    canSubmit,
-    updatedAccountData,
-    // insufficientCollateralError,
-    borrowToPortfolio,
-    warningMsg,
-  } = useBorrow(selectedToken);
-  const txnData = useBorrowTransaction(selectedToken);
-  const history = useHistory();
-  const { pathname, search } = useLocation();
+    state: { canSubmit, populatedTransaction, confirm },
+    updateState,
+  } = useContext(BorrowVariableContext);
   const { currencyInputRef } = useCurrencyInputRef();
+  const handleSubmit = () => {
+    updateState({ confirm: true });
+  };
 
-  const handleTxnCancel = useCallback(() => {
-    history.push(pathname);
-  }, [history, pathname]);
-
-  useEffect(() => {
-    if (search.includes('confirm=true')) {
-      handleTxnCancel();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return txnData ? (
-    <TransactionConfirmation
+  return confirm && populatedTransaction ? (
+    <Confirmation2
       heading={
         <FormattedMessage
           defaultMessage="Borrow Order"
           description="section heading"
         />
       }
-      onCancel={handleTxnCancel}
-      transactionProperties={txnData?.transactionProperties}
-      buildTransactionCall={txnData?.buildTransactionCall}
+      context={BorrowVariableContext}
     />
   ) : (
     <ActionSidebar
@@ -77,102 +42,20 @@ export const BorrowVariableSidebar = () => {
       })}
       CustomActionButton={TradeActionButton}
       canSubmit={canSubmit}
-      advancedToggle={{
-        isChecked: borrowToPortfolio,
-        onToggle: (isChecked) => {
-          updateBorrowState({ borrowToPortfolio: isChecked });
-        },
-        label: <FormattedMessage defaultMessage={'Borrow To Portfolio'} />,
-      }}
+      handleSubmit={handleSubmit}
     >
-      <Maturities
-        maturityData={maturityData || []}
-        onSelect={(selectedMarketKey) => {
-          updateBorrowState({ selectedMarketKey });
-        }}
-        selectedfCashId={selectedMarketKey || undefined}
+      <DepositInput
+        ref={currencyInputRef}
+        inputRef={currencyInputRef}
+        context={BorrowVariableContext}
+        newRoute={(newToken) => `/${PRODUCTS.BORROW_VARIABLE}/${newToken}`}
         inputLabel={defineMessage({
-          defaultMessage: '1. Select a maturity & fix your rate',
+          defaultMessage: '1. How much do you want to borrow?',
           description: 'input label',
         })}
       />
-      {availableCurrencies.length > 0 && selectedToken && (
-        <LendBorrowInput
-          ref={currencyInputRef}
-          inputRef={currencyInputRef}
-          availableTokens={availableCurrencies}
-          selectedToken={selectedToken}
-          isRemoveAsset={false}
-          cashOrfCash={'Cash'}
-          warningMsg={warningMsg}
-          lendOrBorrow={LEND_BORROW.BORROW}
-          selectedMarketKey={selectedMarketKey}
-          onChange={({
-            selectedToken: newToken,
-            inputAmount,
-            hasError,
-            netfCashAmount,
-          }) => {
-            if (newToken !== selectedToken) {
-              history.push(
-                `/${LEND_BORROW.BORROW}/${newToken}/${selectedCollateral}`
-              );
-            }
-            updateBorrowState({
-              inputAmount,
-              hasError,
-              fCashAmount: netfCashAmount,
-            });
-          }}
-          inputLabel={defineMessage({
-            defaultMessage: '2. How much do you want to borrow?',
-            description: 'input label',
-          })}
-        />
-      )}
-      {/* 
-      TODO: Figure out why this causes the page to crash
-      {collateralCurrencies.length > 0 && selectedCollateral && (
-        <CollateralSelect
-          tightMarginTop
-          availableTokens={collateralCurrencies}
-          selectedToken={selectedCollateral}
-          selectedBorrowMarketKey={selectedMarketKey}
-          errorMsg={
-            insufficientCollateralError
-              ? tradeErrors.insufficientCollateral
-              : undefined
-          }
-          inputLabel={defineMessage({
-            defaultMessage:
-              '3. How much additional collateral do you want to deposit?',
-            description: 'input label',
-          })}
-          onChange={(
-            newCollateral,
-            collateralAction,
-            hasCollateralError,
-            collateralApy,
-            collateralSymbol
-          ) => {
-            if (newCollateral !== selectedCollateral) {
-              history.push(
-                `/${LEND_BORROW.BORROW}/${selectedToken}/${newCollateral}`
-              );
-            }
-
-            updateBorrowState({
-              collateralAction,
-              hasCollateralError,
-              collateralApy,
-              collateralSymbol,
-            });
-          }}
-        />
-      )} */}
-      <TokenApprovalView symbol={selectedCollateral} />
-      <RiskSlider updatedAccountData={updatedAccountData} />
-      <AccountRiskTable updatedAccountData={updatedAccountData} />
+      {/** TODO: add a warning for withdrawing cash */}
+      {/** TODO: add enable prime borrow */}
     </ActionSidebar>
   );
 };
