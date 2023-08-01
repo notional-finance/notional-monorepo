e113d1050f46c029b88c47f6fd2dbde92ee59ebb
matthew-garrett <matthew.garrett252@gmail.com>
2023-08-01 17:21:52 -0400

    build: adds borrow fixed how it works dark images

--- packages/features/borrow/src/borrow-fixed/hooks/use-how-it-works.tsx
+++ packages/features/borrow/src/borrow-fixed/hooks/use-how-it-works.tsx
@@ -1,11 +1,11 @@
-import usdcGraphic from '@notional-finance/assets/images/lend-fixed-graphics/usdc.svg';
-import ethGraphic from '@notional-finance/assets/images/lend-fixed-graphics/eth.svg';
-import daiGraphic from '@notional-finance/assets/images/lend-fixed-graphics/dai.svg';
-import wbtcGraphic from '@notional-finance/assets/images/lend-fixed-graphics/wbtc.svg';
-import usdcDarkGraphic from '@notional-finance/assets/images/lend-fixed-graphics/usdc-dark.svg';
-import ethDarkGraphic from '@notional-finance/assets/images/lend-fixed-graphics/eth-dark.svg';
-import daiDarkGraphic from '@notional-finance/assets/images/lend-fixed-graphics/dai-dark.svg';
-import wbtcDarkGraphic from '@notional-finance/assets/images/lend-fixed-graphics/wbtc-dark.svg';
+import usdcGraphic from '@notional-finance/assets/images/borrow-fixed-graphics/usdc.svg';
+import ethGraphic from '@notional-finance/assets/images/borrow-fixed-graphics/eth.svg';
+import daiGraphic from '@notional-finance/assets/images/borrow-fixed-graphics/dai.svg';
+import wbtcGraphic from '@notional-finance/assets/images/borrow-fixed-graphics/wbtc.svg';
+import usdcDarkGraphic from '@notional-finance/assets/images/borrow-fixed-graphics/usdc-dark.svg';
+import ethDarkGraphic from '@notional-finance/assets/images/borrow-fixed-graphics/eth-dark.svg';
+import daiDarkGraphic from '@notional-finance/assets/images/borrow-fixed-graphics/dai-dark.svg';
+import wbtcDarkGraphic from '@notional-finance/assets/images/borrow-fixed-graphics/wbtc-dark.svg';
 import { useUserSettingsState } from '@notional-finance/user-settings-manager';
 import { THEME_VARIANTS } from '@notional-finance/shared-config';
 
