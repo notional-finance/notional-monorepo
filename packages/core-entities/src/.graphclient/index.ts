2e4b564f000be6a1f8ecbfa229919c42e5149060
Jeff Wu <jeff@notional.finance>
2023-08-18 05:51:56 -0700

    fix: bumping registry and graph endpoint

--- packages/core-entities/src/.graphclient/index.ts
+++ packages/core-entities/src/.graphclient/index.ts
@@ -6741,7 +6741,7 @@ const notionalV3Transforms = [];
 const additionalTypeDefs = [] as any[];
 const notionalV3Handler = new GraphqlHandler({
               name: "NotionalV3",
-              config: {"endpoint":"https://api.studio.thegraph.com/query/33671/notional-finance-v3-{context.chainName:arbitrum}/v0.0.140"},
+              config: {"endpoint":"https://api.studio.thegraph.com/query/33671/notional-finance-v3-{context.chainName:arbitrum}/v0.0.143"},
               baseDir,
               cache,
               pubsub,
