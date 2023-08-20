4c69c9b2943bd253f2fe62efc493aef0b6792b60
Jeff Wu <jeff@notional.finance>
2023-08-20 06:18:37 -0700

    fix: slippage issues and vault details

--- packages/features/vault/src/hooks/use-vault-details-table.ts
+++ packages/features/vault/src/hooks/use-vault-details-table.ts
@@ -6,7 +6,7 @@ import { useVaultLiquidationRisk } from '@notional-finance/notionable-hooks';
 export function useVaultDetailsTable() {
   const { state } = useContext(VaultActionContext);
   const { priorVaultBalances, collateralBalance } = state;
-  const { tableData, priorAccountNoRisk, postAccountNoRisk, tooRisky } =
+  const { tableData, priorAccountNoRisk, postAccountNoRisk, tooRisky, onlyCurrent } =
     useVaultLiquidationRisk(state);
 
   const maturity =
@@ -31,6 +31,7 @@ export function useVaultDetailsTable() {
   // }
 
   return {
+    onlyCurrent,
     tableData:
       priorAccountNoRisk && postAccountNoRisk
         ? []
