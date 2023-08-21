81f93ff1d75dcd35c803dc7b2844d82d4e684824
Jeff Wu <jeff@notional.finance>
2023-08-21 13:39:47 -0700

    feat: adding trading contest

---
+++ packages/trading-contest/src/lib/trading-contest.spec.ts
@@ -0,0 +1,7 @@
+import { tradingContest } from './trading-contest';
+
+describe('tradingContest', () => {
+  it('should work', () => {
+    expect(tradingContest()).toEqual('trading-contest');
+  });
+});
---
+++ packages/trading-contest/src/lib/trading-contest.ts
@@ -0,0 +1,3 @@
+export function tradingContest(): string {
+  return 'trading-contest';
+}
