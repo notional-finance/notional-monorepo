a475bb145c782f36ba1bf14363e8a1017667991e
Tianjie Wei <weitianjie@gmail.com>
2023-08-20 22:45:33 -0700

    fix: updating data worker

--- apps/data/src/index.ts
+++ apps/data/src/index.ts
@@ -1,19 +1,44 @@
-export interface Env {
+import {
+  DurableObjectNamespace,
+  Request,
+  Response,
+} from '@cloudflare/workers-types';
+import { RegistryDOEnv } from '@notional-finance/durable-objects';
+import { Routes } from '@notional-finance/core-entities/src/server';
+
+export interface Env extends RegistryDOEnv {
   VIEWS_DO: DurableObjectNamespace;
   DATA_SERVICE_URL: string;
   DATA_SERVICE_AUTH_TOKEN: string;
 }
 
 export { ViewsDO } from '@notional-finance/durable-objects';
+export { YieldRegistryDO } from '@notional-finance/durable-objects';
+
+async function runHealthCheck(ns: DurableObjectNamespace, version: string) {
+  const stub = ns.get(ns.idFromName(version));
+  await stub.fetch('http://hostname/healthcheck');
+}
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    const id = env.VIEWS_DO.idFromName('VIEWS_DO');
-    const stub = env.VIEWS_DO.get(id);
+    const url = new URL(request.url);
 
+    let ns: DurableObjectNamespace;
+    switch (url.pathname.split('/')[2]) {
+      case Routes.Yields:
+        ns = env.YIELD_REGISTRY_DO;
+        break;
+      case Routes.Analytics:
+        ns = env.VIEWS_DO;
+        break;
+    }
+
+    const stub = ns.get(ns.idFromName(env.VERSION));
     return stub.fetch(request);
   },
-  async scheduled(): Promise<void> {
-    console.log('Scheduled');
+  async scheduled(_: ScheduledController, env: Env): Promise<void> {
+    // Run a healthcheck against all of the durable objects.
+    await Promise.all([runHealthCheck(env.YIELD_REGISTRY_DO, env.VERSION)]);
   },
 };
