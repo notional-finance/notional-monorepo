079f258f45f92d8396996fe1e896c93e8d8a0f5f
Jeff Wu <jeff@notional.finance>
2023-08-22 12:27:37 -0700

    fix: small change

--- packages/features/portfolio/src/containers/portfolio-txn-history/hooks/use-txn-history-data.tsx
+++ packages/features/portfolio/src/containers/portfolio-txn-history/hooks/use-txn-history-data.tsx
@@ -22,7 +22,7 @@ export const useTxnHistoryData = (txnHistoryType: TXN_HISTORY_TYPE) => {
   const selectedNetwork = useSelectedNetwork();
 
   const allAccountHistoryData = accountHistory
-    ?.sort((x, y) => y.timestamp - x.timestamp)
+    .sort((x, y) => y.timestamp - x.timestamp)
     .map(
       ({
         bundleName,
