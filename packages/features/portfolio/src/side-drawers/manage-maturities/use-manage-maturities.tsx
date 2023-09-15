8e48912dbc531bf45ad4d9aee051b11a7d8fb10b
Jeff Wu <jeff@notional.finance>
2023-09-15 16:44:18 -0700

    fix: roll maturity bug

--- packages/features/portfolio/src/side-drawers/manage-maturities/use-manage-maturities.tsx
+++ packages/features/portfolio/src/side-drawers/manage-maturities/use-manage-maturities.tsx
@@ -1,11 +1,12 @@
 import { useAssetSummary } from '@notional-finance/notionable-hooks';
 import { LEND_BORROW } from '@notional-finance/shared-config';
-import { MaturityData } from '@notional-finance/notionable';
 import { formatMaturity } from '@notional-finance/helpers';
 
-export function useManageMaturities(borrowOrLend: LEND_BORROW) {
+export function useManageMaturities(
+  borrowOrLend: LEND_BORROW,
+  assetKey: string
+) {
   const assetSummary = useAssetSummary(borrowOrLend);
-  let rollMaturities = [];
   const formatMaturities = (maturities) => {
     return maturities.map((data) => ({
       marketKey: data.marketKey,
@@ -15,11 +16,8 @@ export function useManageMaturities(borrowOrLend: LEND_BORROW) {
     }));
   };
 
-  assetSummary.forEach((data) => {
-    if (data?.rollMaturities?.length) {
-      rollMaturities = formatMaturities(data.rollMaturities);
-    }
-  });
-
-  return rollMaturities as MaturityData[];
+  return formatMaturities(
+    assetSummary.find((data) => data.assetKey === assetKey)?.rollMaturities ||
+      []
+  );
 }
