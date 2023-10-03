1b1d47d40881cae25781680d14c1330a45ca12b1
Jeff Wu <jeff@notional.finance>
2023-10-03 09:44:58 -0700

    fix: historical charting

--- packages/shared/notionable-hooks/src/use-chart.tsx
+++ packages/shared/notionable-hooks/src/use-chart.tsx
@@ -184,7 +184,7 @@ export function useAccountHistoryChart(
 
         return { timestamp: start, assets, debts, netWorth: assets.sub(debts) };
       });
-  } catch {
+  } catch (e) {
     return undefined;
   }
 }
