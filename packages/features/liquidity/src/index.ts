dd5626b88c7a8e38e6725e19b5d9f22e3b8266e3
Jeff Wu <jeff@notional.finance>
2023-10-08 07:01:12 -0700

    fix: refactor card views

--- packages/features/liquidity/src/index.ts
+++ packages/features/liquidity/src/index.ts
@@ -1,3 +1,2 @@
 export { LiquidityVariable, LiquidityLeveraged } from './liquidity';
-export { LiquidityVariableCardView } from './liquidity-variable/liquidity-variable-card-view';
-export { LiquidityLeveragedCardView } from './liquidity-leveraged/liquidity-leveraged-card-view';
+export { LiquidityVariableCardView, LiquidityLeveragedCardView } from './cards';
