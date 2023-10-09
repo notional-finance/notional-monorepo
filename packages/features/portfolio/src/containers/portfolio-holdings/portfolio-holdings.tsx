9a8dd39e5fb51079fac345f00af30504cff6aa18
Jeff Wu <jeff@notional.finance>
2023-10-09 11:44:35 -0700

    fix: moves button bar to the top

--- packages/features/portfolio/src/containers/portfolio-holdings/portfolio-holdings.tsx
+++ packages/features/portfolio/src/containers/portfolio-holdings/portfolio-holdings.tsx
@@ -2,12 +2,10 @@ import { usePortfolioHoldings } from './use-portfolio-holdings';
 import { DataTable } from '@notional-finance/mui';
 import { FormattedMessage } from 'react-intl';
 import { TableActionRow } from '../../components';
-import { usePortfolioButtonBar } from '../../hooks';
 import { Box } from '@mui/material';
 import { PortfolioRisk } from './portfolio-risk';
 
 export const PortfolioHoldings = () => {
-  const buttonData = usePortfolioButtonBar();
   const {
     portfolioHoldingsColumns,
     detailedHoldings,
@@ -38,7 +36,6 @@ export const PortfolioHoldings = () => {
             description="table title"
           />
         }
-        tableTitleButtons={buttonData}
         initialState={initialState}
         setExpandedRows={setExpandedRows}
       />
