a975bc93a9199dbf0332ee784f40d1acfe56664f
Jeff Wu <jeff@notional.finance>
2023-10-09 11:13:19 -0700

    fix: holdings tables

--- packages/features/portfolio/src/containers/portfolio-holdings/portfolio-holdings.tsx
+++ packages/features/portfolio/src/containers/portfolio-holdings/portfolio-holdings.tsx
@@ -8,8 +8,8 @@ import { PortfolioRisk } from './portfolio-risk';
 export const PortfolioHoldings = () => {
   const {
     portfolioHoldingsColumns,
-    // detailedHoldings,
-    groupedHoldings,
+    detailedHoldings,
+    // groupedHoldings,
     pendingTokenData,
     setExpandedRows,
     initialState,
@@ -19,7 +19,7 @@ export const PortfolioHoldings = () => {
     <Box>
       <PortfolioRisk />
       <DataTable
-        data={groupedHoldings}
+        data={detailedHoldings}
         pendingTokenData={pendingTokenData}
         pendingMessage={
           <FormattedMessage
