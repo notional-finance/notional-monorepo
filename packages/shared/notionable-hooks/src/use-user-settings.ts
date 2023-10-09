0cc1c2e364a7dee972de1d8960dfb11a8d709ebc
Jeff Wu <jeff@notional.finance>
2023-10-09 11:03:30 -0700

    fix: grouping holdings logic

--- packages/shared/notionable-hooks/src/use-user-settings.ts
+++ packages/shared/notionable-hooks/src/use-user-settings.ts
@@ -1,12 +1,27 @@
+import { Registry } from '@notional-finance/core-entities';
 import { useNotionalContext } from './use-notional';
+import { Network } from '@notional-finance/util';
 
 export function useFiat() {
-  const { globalState: { baseCurrency } } = useNotionalContext();
-  return baseCurrency
+  const {
+    globalState: { baseCurrency },
+  } = useNotionalContext();
+  return baseCurrency;
+}
+
+export function useFiatToken() {
+  const {
+    globalState: { baseCurrency },
+  } = useNotionalContext();
+  return Registry.getTokenRegistry().getTokenBySymbol(
+    Network.All,
+    baseCurrency
+  );
 }
 
 export function useThemeVariant() {
-  const { globalState: { themeVariant } } = useNotionalContext();
-  return themeVariant
+  const {
+    globalState: { themeVariant },
+  } = useNotionalContext();
+  return themeVariant;
 }
-
