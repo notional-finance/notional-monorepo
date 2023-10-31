ee0af9d852310274d907522ddfa78afd4768467c
Jeff Wu <jeff@notional.finance>
2023-10-31 10:55:26 -0700

    fix: add ability to serve registry locally

---
+++ apps/localAPI.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+
+const app = express();
+const port = 4000; // Port for the Express server
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Define a proxy for /*/tokens to localhost:8787
+const tokensProxy = createProxyMiddleware(
+  ['/*/tokens', '/*/configuration', '/*/vaults'],
+  {
+    target: 'http://localhost:8888',
+    changeOrigin: true,
+  }
+);
+
+// Define a proxy for /*/oracles to localhost:8888
+const oraclesProxy = createProxyMiddleware(['/*/oracles', '/*/exchanges'], {
+  target: 'http://localhost:8889',
+  changeOrigin: true,
+});
+
+// Use the proxies for the specified paths
+app.use(tokensProxy);
+app.use(oraclesProxy);
+
+// Define a catch-all proxy to forward any other requests to the specified URL
+const catchAllProxy = createProxyMiddleware(['**'], {
+  target: 'https://data-dev.notional.finance',
+  changeOrigin: true,
+});
+app.use(catchAllProxy);
+
+app.listen(port, () => {
+  console.log(`Proxy server is running on http://localhost:${port}`);
+});
---
+++ apps/serveLocalRegistry.sh
@@ -0,0 +1,3 @@
+yarn nx serve registry --port 8888 &
+yarn nx serve registry-exchange --port 8889 &
+yarn node apps/localAPI.js
