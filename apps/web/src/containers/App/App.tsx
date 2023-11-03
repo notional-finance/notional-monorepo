242e20a2876a0dcfe712a398578b205d378d2b3a
matthew-garrett <matthew.garrett252@gmail.com>
2023-11-03 11:38:52 -0400

    build: disables contest pages and use nft contract hook

--- apps/web/src/containers/App/App.tsx
+++ apps/web/src/containers/App/App.tsx
@@ -49,11 +49,11 @@ import {
 import { TermsView } from '../../containers/TermsView';
 import { PrivacyView } from '../../containers/PrivacyView';
 import { LandingPageView } from '../../containers/LandingPageView';
-import {
-  ContestHome,
-  ContestRules,
-  ContestLeaderBoard,
-} from '../../containers/TradingContest';
+// import {
+//   ContestHome,
+//   ContestRules,
+//   ContestLeaderBoard,
+// } from '../../containers/TradingContest';
 import { Markets } from '../Markets';
 
 const AllRoutes = () => {
@@ -177,7 +177,7 @@ const AllRoutes = () => {
             component={ServerError}
             routeType="Error"
           />
-          <AppLayoutRoute
+          {/* <AppLayoutRoute
             path="/contest"
             component={ContestHome}
             routeType="Landing"
@@ -191,7 +191,7 @@ const AllRoutes = () => {
             path="/contest-leaderboard"
             component={ContestLeaderBoard}
             routeType="Landing"
-          />
+          /> */}
           <AppLayoutRoute
             path="/terms"
             component={TermsView}
