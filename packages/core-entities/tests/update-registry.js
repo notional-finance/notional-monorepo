ef4c60391ad9fe36a8a8533d0e1b81e0259b1f51
Jeff Wu <jeff@notional.finance>
2023-12-03 09:41:57 -0800

    fix: update registry

--- packages/core-entities/tests/update-registry.js
+++ packages/core-entities/tests/update-registry.js
@@ -1,39 +1,44 @@
 const crossFetch = require('cross-fetch');
 const fs = require('fs');
 
+const blockNumber = 142127726;
+// This always refers to a mainnet block
+const allBlock = 18387452;
+
 async function main() {
-  const apiHostname = 'https://data-dev.notional.finance';
+  // Serve the data worker in order to run the requests
+  const apiHostname = 'http://localhost:8787';
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const c = await crossFetch(`${apiHostname}/arbitrum/configuration`).then(
-    (r) => r.text()
-  );
+  const c = await crossFetch(
+    `${apiHostname}/arbitrum/configuration/${blockNumber}`
+  ).then((r) => r.text());
 
-  const t = await crossFetch(`${apiHostname}/arbitrum/tokens`).then((r) =>
-    r.text()
-  );
+  const t = await crossFetch(
+    `${apiHostname}/arbitrum/tokens/${blockNumber}`
+  ).then((r) => r.text());
 
-  const e = await crossFetch(`${apiHostname}/arbitrum/exchanges`).then((r) =>
-    r.text()
-  );
+  const e = await crossFetch(
+    `${apiHostname}/arbitrum/exchanges/${blockNumber}`
+  ).then((r) => r.text());
 
-  const o = await crossFetch(`${apiHostname}/arbitrum/oracles`).then((r) =>
-    r.text()
-  );
+  const o = await crossFetch(
+    `${apiHostname}/arbitrum/oracles/${blockNumber}`
+  ).then((r) => r.text());
 
-  const v = await crossFetch(`${apiHostname}/arbitrum/vaults`).then((r) =>
-    r.text()
-  );
+  const v = await crossFetch(
+    `${apiHostname}/arbitrum/vaults/${blockNumber}`
+  ).then((r) => r.text());
 
-  const a = await crossFetch(`${apiHostname}/arbitrum/accounts`).then((r) =>
-    r.text()
-  );
+  // const a = await crossFetch(`${apiHostname}/arbitrum/accounts/${blockNumber}`).then((r) =>
+  //   r.text()
+  // );
 
-  const allTokens = await crossFetch(`${apiHostname}/all/tokens`).then((r) =>
-    r.text()
-  );
-  const allOracles = await crossFetch(`${apiHostname}/all/oracles`).then((r) =>
-    r.text()
-  );
+  const allTokens = await crossFetch(
+    `${apiHostname}/all/tokens/${allBlock}`
+  ).then((r) => r.text());
+  const allOracles = await crossFetch(
+    `${apiHostname}/all/oracles/${allBlock}`
+  ).then((r) => r.text());
 
   fs.writeFileSync(
     `${__dirname}/clients/__snapshots__/arbitrum/configuration`,
@@ -43,7 +48,7 @@ async function main() {
   fs.writeFileSync(`${__dirname}/clients/__snapshots__/arbitrum/exchanges`, e);
   fs.writeFileSync(`${__dirname}/clients/__snapshots__/arbitrum/oracles`, o);
   fs.writeFileSync(`${__dirname}/clients/__snapshots__/arbitrum/vaults`, v);
-  fs.writeFileSync(`${__dirname}/clients/__snapshots__/arbitrum/accounts`, a);
+  // fs.writeFileSync(`${__dirname}/clients/__snapshots__/arbitrum/accounts`, a);
   fs.writeFileSync(`${__dirname}/clients/__snapshots__/all/tokens`, allTokens);
   fs.writeFileSync(
     `${__dirname}/clients/__snapshots__/all/oracles`,
--- tools/generators/cloudflare-worker/files/src/index.ts
+++ tools/generators/cloudflare-worker/files/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * Welcome to Cloudflare Workers! This is your first scheduled worker.
  *
- * - Run `wrangler dev --local` in your terminal to start a development server
+ * - Run `wrangler dev  in your terminal to start a development server
  * - Run `curl "http://localhost:8787/cdn-cgi/mf/scheduled"` to trigger the scheduled event
  * - Go back to the console to see what your worker has logged
  * - Update the Cron trigger in wrangler.toml (see https://developers.cloudflare.com/workers/wrangler/configuration/#triggers)
