0aaaa077854ad27a0b84dc600bf48ba35f9f6351
Jeff Wu <jeff@notional.finance>
2023-12-13 15:11:07 -0800

    fix: whitelisted vaults

---
+++ packages/core-entities/src/config/whitelisted-vaults.ts
@@ -0,0 +1,2 @@
+
+export const whitelistedVaults = ['0xdb08f663e5d765949054785f2ed1b2aa1e9c22cf'];
--- packages/core-entities/src/index.ts
+++ packages/core-entities/src/index.ts
@@ -9,4 +9,9 @@ export { ConfigurationClient } from './client';
 export * from './config/fiat-config';
 export * from './server';
 export { ClientRegistry } from './client/client-registry';
-export { ServerRegistry, fetchGraph, loadGraphClientDeferred } from './server/server-registry';
+export {
+  ServerRegistry,
+  fetchGraph,
+  loadGraphClientDeferred,
+} from './server/server-registry';
+export * from './config/whitelisted-vaults';
