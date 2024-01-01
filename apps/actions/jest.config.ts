59ec3afdb49da0d51f26a3368a6cd6e781fa454c
Jeff Wu <jeff@notional.finance>
2024-01-01 06:45:29 -0800

    feat: tenderly action building

---
+++ apps/actions/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'actions',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      useESM: true,
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  coverageDirectory: '../../coverage/apps/actions',
+  testTimeout: 50000,
+};
