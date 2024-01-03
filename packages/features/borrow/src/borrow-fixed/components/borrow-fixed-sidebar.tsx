046d7f82febcc5b242af05fbd267b9c89b61a90a
Jeff Wu <jeff@notional.finance>
2024-01-03 15:29:57 -0800

    fix: use approve prime on borrow

--- packages/features/borrow/src/borrow-fixed/components/borrow-fixed-sidebar.tsx
+++ packages/features/borrow/src/borrow-fixed/components/borrow-fixed-sidebar.tsx
@@ -8,13 +8,26 @@ import { PRODUCTS } from '@notional-finance/util';
 import { defineMessage } from 'react-intl';
 import { BorrowFixedContext } from '../borrow-fixed';
 import { useContext } from 'react';
+import { usePrimeCashBalance } from '@notional-finance/notionable-hooks';
 
 export const BorrowFixedSidebar = () => {
   const { currencyInputRef } = useCurrencyInputRef();
   const context = useContext(BorrowFixedContext);
+  const {
+    state: { selectedDepositToken },
+  } = context;
+  const cashBalance = usePrimeCashBalance(selectedDepositToken);
 
   return (
-    <TransactionSidebar context={context} showDrawer isWithdraw>
+    <TransactionSidebar
+      context={context}
+      showDrawer
+      isWithdraw
+      // If the cash balance is negative, then the account will require
+      // prime borrow to be enabled, even though a further negative balance
+      // will not be incurred
+      variableBorrowRequired={cashBalance?.isNegative()}
+    >
       <DepositInput
         isWithdraw
         showScrollPopper
