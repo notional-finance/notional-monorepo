8a3b345ce43c73f9fe58f94310c9f28cc3b12a83
Jeff Wu <jeff@notional.finance>
2024-01-03 11:08:16 -0800

    fix: slider gradients

--- packages/common/mui/src/lib/slider-risk/slider-risk.tsx
+++ packages/common/mui/src/lib/slider-risk/slider-risk.tsx
@@ -33,14 +33,24 @@ export const SliderRisk = ({ healthFactor }: SliderRiskProps) => {
         step={0.1}
         disabled={true}
         sx={{ marginBottom: '0px' }}
+        //         border-radius: 4px;
+        // background: linear-gradient(270deg, #FF3D71 2.15%, #FF3D71 5.07%, #F9DF3D 11.55%, #F9E73B 34.46%, #34DF3A 49.32%, #34DF3A 97.85%);
         railGradients={[
           {
             color: [255, 61, 113], // Red
-            value: 6,
+            value: 3,
+          },
+          {
+            color: [249, 223, 61], // Yellow
+            value: 11,
           },
           {
             color: [249, 223, 61], // Yellow
             value: 38,
+          },
+          {
+            color: [52, 223, 58], // Green
+            value: 50,
           },
           {
             color: [52, 223, 58], // Green
@@ -51,9 +61,9 @@ export const SliderRisk = ({ healthFactor }: SliderRiskProps) => {
       <LabelValue sx={{ marginLeft: theme.spacing(3), textWrap: 'nowrap' }}>
         {healthFactor ? (
           healthFactor > 5 ? (
-            '5+'
+            '5+ / 5.0'
           ) : (
-            healthFactor.toFixed(2)
+            `${healthFactor.toFixed(2)} / 5.0`
           )
         ) : (
           <FormattedMessage defaultMessage={'No Risk'} />
