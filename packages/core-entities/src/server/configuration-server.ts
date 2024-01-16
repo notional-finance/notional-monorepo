e09a1b6c0630821488ea6dc44ef202c468aca775
Jeff Wu <jeff@notional.finance>
2024-01-16 15:01:21 -0800

    fix: update secondary incentive values on server

--- packages/core-entities/src/server/configuration-server.ts
+++ packages/core-entities/src/server/configuration-server.ts
@@ -5,7 +5,10 @@ import {
   ServerRegistry,
   TypedDocumentReturnType,
 } from './server-registry';
-import { Network } from '@notional-finance/util';
+import { getProviderFromNetwork, Network } from '@notional-finance/util';
+import { BigNumber, Contract } from 'ethers';
+import { SecondaryRewarderABI } from '@notional-finance/contracts';
+import { aggregate } from '@notional-finance/multicall';
 
 export type AllConfigurationQuery = TypedDocumentReturnType<
   DocumentTypes['AllConfigurationDocument']
@@ -16,7 +19,8 @@ export class ConfigurationServer extends ServerRegistry<AllConfigurationQuery> {
   protected async _refresh(network: Network, blockNumber?: number) {
     const { AllConfigurationDocument, AllConfigurationByBlockDocument } =
       await loadGraphClientDeferred();
-    return this._fetchUsingGraph(
+
+    const data = await this._fetchUsingGraph(
       network,
       (blockNumber !== undefined
         ? AllConfigurationByBlockDocument
@@ -31,5 +35,42 @@ export class ConfigurationServer extends ServerRegistry<AllConfigurationQuery> {
         blockNumber,
       }
     );
+
+    const provider = getProviderFromNetwork(network, true);
+    const secondaryRewarderCalls = (
+      data.values[0][1]?.whitelistedContracts || []
+    )
+      .filter((s) => s.capability.includes('SecondaryIncentiveRewarder'))
+      .flatMap(({ id }) => {
+        const rewarder = new Contract(id, SecondaryRewarderABI, provider);
+        return [
+          {
+            key: `${id}.accumulatedRewardPerNToken`,
+            target: rewarder,
+            method: 'accumulatedRewardPerNToken',
+          },
+          {
+            key: `${id}.lastAccumulatedTime`,
+            target: rewarder,
+            method: 'lastAccumulatedTime',
+          },
+        ];
+      });
+
+    const { results } = await aggregate(secondaryRewarderCalls, provider);
+    data.values[0][1]?.currencyConfigurations.forEach((c) => {
+      if (c.incentives?.secondaryIncentiveRewarder) {
+        const rewarder = c.incentives?.secondaryIncentiveRewarder;
+        if (c.incentives) {
+          c.incentives.accumulatedSecondaryRewardPerNToken = (
+            results[`${rewarder}.accumulatedRewardPerNToken`] as BigNumber
+          ).toString();
+          c.incentives.lastSecondaryAccumulatedTime =
+            results[`${rewarder}.lastAccumulatedTime`];
+        }
+      }
+    });
+
+    return data;
   }
 }
