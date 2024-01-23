ee0a141b87aeb8f5b48037b2765edf2fcab70282
Jeff Wu <jeff@notional.finance>
2024-01-22 17:29:42 -0800

    feat: bot changes

--- apps/serveLocalRegistry.sh
+++ apps/serveLocalRegistry.sh
@@ -1,3 +1,4 @@
-yarn nx serve registry --port 8888 &
-yarn nx serve registry-exchange --port 8889 &
+# yarn nx serve registry --port 8888 &
+# yarn nx serve registry-exchange --port 8889 &
+yarn nx serve data --env dev --port 8890 &
 yarn node apps/localAPI.js
