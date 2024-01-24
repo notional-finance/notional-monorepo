6ca895f63c45ed19272d67df9fe982c32b2bf4fc
matthew-garrett <matthew.garrett252@gmail.com>
2024-01-24 14:21:22 -0500

    build: fixes vault maturity buttons, removes unused code

--- packages/features/vault/src/vault-view/vault-action-side-drawer.tsx
+++ packages/features/vault/src/vault-view/vault-action-side-drawer.tsx
@@ -118,32 +118,6 @@ export const VaultActionSideDrawer = () => {
             riskFactorLimit: currentPosition.riskFactorLimit,
           },
         },
-
-        // REMOVED ===================================================
-        // {
-        //   slug: 'DepositVaultCollateral',
-        //   Component: DepositCollateral,
-        //   requiredState: {
-        //     tradeType: 'DepositVaultCollateral',
-        //   },
-        // },
-        // {
-        //   slug: 'IncreaseVaultPosition',
-        //   Component: IncreaseVaultPosition,
-        //   requiredState: {
-        //     tradeType: 'IncreaseVaultPosition',
-        //     riskFactorLimit: defaultRiskLimit,
-        //   },
-        // },
-        // {
-        //   slug: 'WithdrawAndRepayVault',
-        //   Component: WithdrawAndRepayDebt,
-        //   requiredState: {
-        //     tradeType: 'WithdrawAndRepayVault',
-        //     depositBalance: deposit ? TokenBalance.zero(deposit) : undefined,
-        //     riskFactorLimit: defaultRiskLimit,
-        //   },
-        // },
       ]}
     />
   );
