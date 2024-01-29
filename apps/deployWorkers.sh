fe562f9337d60c32cdc6b4684fc2319d471ef4a0
Jeff Wu <jeff@notional.finance>
2024-01-29 15:31:57 -0800

    fix: update deployment script

--- apps/deployWorkers.sh
+++ apps/deployWorkers.sh
@@ -4,8 +4,13 @@ set -e
 yarn nx publish-wrangler-manual accounts --env dev
 yarn nx publish-wrangler-manual api --env dev
 yarn nx publish-wrangler-manual data --env dev
-yarn nx publish-wrangler-manual liquidation-bot --env dev
-yarn nx publish-wrangler-manual registry --env dev
+
+yarn nx publish-wrangler-manual registry-configuration --env dev
+yarn nx publish-wrangler-manual registry-vaults --env dev
+yarn nx publish-wrangler-manual registry-tokens --env dev
+yarn nx publish-wrangler-manual registry-oracles --env dev
 yarn nx publish-wrangler-manual registry-exchange --env dev
 yarn nx publish-wrangler-manual rewards --env dev
+
+yarn nx publish-wrangler-manual liquidation-bot --env dev
 yarn nx publish-wrangler-manual vault-liquidator --env dev
