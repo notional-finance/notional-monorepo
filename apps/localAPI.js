d6def245efafdb076f657b7d5364cfccff10b7e5
Jeff Wu <jeff@notional.finance>
2024-01-29 15:46:40 -0800

    fix: update local serve

--- apps/localAPI.js
+++ apps/localAPI.js
@@ -9,28 +9,37 @@ const port = 4000; // Port for the Express server
 app.use(cors());
 
 // Define a proxy for /*/tokens to localhost:8787
-// const tokensProxy = createProxyMiddleware(
-//   ['/*/tokens', '/*/configuration', '/*/vaults'],
-//   {
-//     target: 'http://localhost:8888',
-//     changeOrigin: true,
-//   }
-// );
-
-// // Define a proxy for /*/oracles to localhost:8888
-// const oraclesProxy = createProxyMiddleware(['/*/oracles', '/*/exchanges'], {
-//   target: 'http://localhost:8889',
-//   changeOrigin: true,
-// });
-
+const configProxy = createProxyMiddleware(['/*/configuration'], {
+  target: 'http://localhost:8885',
+  changeOrigin: true,
+});
+const tokensProxy = createProxyMiddleware(['/*/tokens'], {
+  target: 'http://localhost:8886',
+  changeOrigin: true,
+});
+const vaultsProxy = createProxyMiddleware(['/*/vaults'], {
+  target: 'http://localhost:8887',
+  changeOrigin: true,
+});
+const oraclesProxy = createProxyMiddleware(['/*/oracles'], {
+  target: 'http://localhost:8888',
+  changeOrigin: true,
+});
+const exchangesProxy = createProxyMiddleware(['/*/exchanges'], {
+  target: 'http://localhost:8889',
+  changeOrigin: true,
+});
 const dataProxy = createProxyMiddleware(['/*/views/*'], {
   target: 'http://localhost:8890',
   changeOrigin: true,
 });
 
 // Use the proxies for the specified paths
-// app.use(tokensProxy);
-// app.use(oraclesProxy);
+app.use(tokensProxy);
+app.use(configProxy);
+app.use(oraclesProxy);
+app.use(vaultsProxy);
+app.use(exchangesProxy);
 app.use(dataProxy);
 
 // Define a catch-all proxy to forward any other requests to the specified URL
