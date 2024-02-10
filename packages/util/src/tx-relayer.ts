74094665785b26670077995f726f5c49e84d32cd
Jeff Wu <jeff@notional.finance>
2024-02-10 06:17:39 -0800

    fix: url strings

--- packages/util/src/tx-relayer.ts
+++ packages/util/src/tx-relayer.ts
@@ -1,19 +1,27 @@
-import {Network} from './constants';
+import { Network } from './constants';
 
 interface Env {
   NETWORK: keyof typeof Network;
   TX_RELAY_AUTH_TOKEN: string;
 }
 
-const urls: Record<Network,string> = {
+const urls: Record<Network, string> = {
   all: '',
- // it has 'arbitrum' in url but it is also endpoint for mainnet relayers,
- // endpoint format is "/v1/txes/:relayerId", relayerId 0 is for arbitrum an 1 for mainnet
-  mainnet: 'https://tx-relay-arbitrum-dot-monitoring-agents.uc.r.appspot.com/v1/txes/1',
-  arbitrum: 'https://tx-relay-arbitrum-dot-monitoring-agents.uc.r.appspot.com/v1/txes/0',
-}
+  // it has 'arbitrum' in url but it is also endpoint for mainnet relayers,
+  // endpoint format is "/v1/txes/:relayerId", relayerId 0 is for arbitrum an 1 for mainnet
+  mainnet:
+    'https://tx-relay-arbitrum-dot-monitoring-agents.uc.r.appspot.com/v1/txes/1',
+  arbitrum:
+    'https://tx-relay-arbitrum-dot-monitoring-agents.uc.r.appspot.com/v1/txes/0',
+  optimism: '',
+  goerli: '',
+};
 
-export function sendTxThroughRelayer(arg: { env: Env, to: string, data: string }) {
+export function sendTxThroughRelayer(arg: {
+  env: Env;
+  to: string;
+  data: string;
+}) {
   const { to, data, env } = arg;
 
   const payload = JSON.stringify({
@@ -30,4 +38,3 @@ export function sendTxThroughRelayer(arg: { env: Env, to: string, data: string }
     body: payload,
   });
 }
-
--- packages/util/src/zero-x-exchange.ts
+++ packages/util/src/zero-x-exchange.ts
@@ -1,32 +1,44 @@
-import {Network} from './constants';
-import { BigNumber } from "ethers";
+import { Network } from './constants';
+import { BigNumber } from 'ethers';
 
 interface Env {
   NETWORK: keyof typeof Network;
-  ZERO_EX_API_KEY: string,
+  ZERO_EX_API_KEY: string;
 }
 
-const urls: Record<Network,string> = {
+const urls: Record<Network, string> = {
   all: '',
   mainnet: 'https://api.0x.org/swap/v1/quote',
   arbitrum: 'https://arbitrum.api.0x.org/swap/v1/quote',
-}
+  optimism: '',
+  goerli: '',
+};
 
 const DEFAULT_SLIPPAGE_PERCENT = 5;
 const zeroXDelay = 4000; // in ms
 const s = { lastGet0xDataCall: 0 };
 
-const wait = (ms: number) => new Promise(f => setTimeout(f, ms));
+const wait = (ms: number) => new Promise((f) => setTimeout(f, ms));
 
-export async function get0xData(
-  arg: { sellToken: string, buyToken: string, sellAmount: BigNumber, slippagePercentage?: number, env: Env }
-) {
-  const { sellToken, buyToken, sellAmount, env, slippagePercentage = DEFAULT_SLIPPAGE_PERCENT } = arg;
+export async function get0xData(arg: {
+  sellToken: string;
+  buyToken: string;
+  sellAmount: BigNumber;
+  slippagePercentage?: number;
+  env: Env;
+}) {
+  const {
+    sellToken,
+    buyToken,
+    sellAmount,
+    env,
+    slippagePercentage = DEFAULT_SLIPPAGE_PERCENT,
+  } = arg;
   if (sellAmount.isZero()) {
     return {
       buyAmount: BigNumber.from(0),
       limit: BigNumber.from(0),
-      data: ''
+      data: '',
     };
   }
   // ensure we don't hit rate limit on 0x API
@@ -39,26 +51,29 @@ export async function get0xData(
     sellToken: sellToken,
     buyToken: buyToken,
     sellAmount: sellAmount.toString(),
-    slippagePercentage: String(slippagePercentage / 100)
+    slippagePercentage: String(slippagePercentage / 100),
   }).toString();
 
-  const response = await fetch(`${urls[Network[env.NETWORK]]}?${searchParams}`, {
-    headers: {
-      "0x-api-key": env.ZERO_EX_API_KEY
-    },
-  });
+  const response = await fetch(
+    `${urls[Network[env.NETWORK]]}?${searchParams}`,
+    {
+      headers: {
+        '0x-api-key': env.ZERO_EX_API_KEY,
+      },
+    }
+  );
 
   const data = await response.json();
-  if (data["buyAmount"] == undefined) {
-    throw Error("Failed call to 0x api");
+  if (data['buyAmount'] == undefined) {
+    throw Error('Failed call to 0x api');
   }
 
   s.lastGet0xDataCall = Date.now();
 
-  const buyAmount = BigNumber.from(data["buyAmount"]);
+  const buyAmount = BigNumber.from(data['buyAmount']);
   return {
     buyAmount,
     limit: buyAmount.mul(100 - slippagePercentage).div(100),
-    data: data["data"] as string
+    data: data['data'] as string,
   };
 }
