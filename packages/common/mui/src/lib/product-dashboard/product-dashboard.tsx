47eeed7f837830ea3d06490aeea5e4ec7ab0a7e4
matthew-garrett <matthew.garrett252@gmail.com>
2024-02-29 10:42:19 -0500

    build: clean up

--- packages/common/mui/src/lib/product-dashboard/product-dashboard.tsx
+++ packages/common/mui/src/lib/product-dashboard/product-dashboard.tsx
@@ -66,7 +66,6 @@ export interface ProductDashboardProps extends DashboardViewProps {
     networkToggle: number;
     handleNetWorkToggle: (value: number) => void;
   };
-  threeWideGrid?: boolean;
 }
 
 export const ProductDashboard = ({
