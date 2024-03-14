7d94fe1875c196fbb2668685a44046f1b1f42767
matthew-garrett <matthew.garrett252@gmail.com>
2024-03-14 09:43:18 -0400

    build: addresses comment

--- packages/features/portfolio/src/containers/portfolio-holdings/use-portfolio-holdings.tsx
+++ packages/features/portfolio/src/containers/portfolio-holdings/use-portfolio-holdings.tsx
@@ -17,6 +17,39 @@ import { useDetailedHoldingsTable } from './use-detailed-holdings';
 import { useGroupedHoldingsTable } from './use-grouped-holdings';
 import { useTheme } from '@mui/material';
 
+function insertDebtDivider(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].asset.label.includes('Borrow')) {
+      arr.splice(i, 0, {
+        asset: {
+          symbol: '',
+          symbolBottom: '',
+          label: 'DEBT POSITIONS',
+          caption: '',
+        },
+        marketApy: {
+          data: [
+            {
+              displayValue: '',
+              isNegative: false,
+            },
+          ],
+        },
+        amountPaid: '',
+        presentValue: '',
+        earnings: '',
+        toolTipData: undefined,
+        actionRow: undefined,
+        tokenId: ' ',
+        isTotalRow: true,
+        isDividerRow: true,
+      });
+      break;
+    }
+  }
+  return arr;
+}
+
 export function usePortfolioHoldings() {
   const theme = useTheme();
   const isBlocked = useLeverageBlock();
@@ -119,39 +152,6 @@ export function usePortfolioHoldings() {
     toggleOption === 0 && !isBlocked && groupedRows.length > 0
       ? groupedHoldings
       : detailedHoldings;
-
-  function insertDebtDivider(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].asset.label.includes('Borrow')) {
-        arr.splice(i, 0, {
-          asset: {
-            symbol: '',
-            symbolBottom: '',
-            label: 'DEBT POSITIONS',
-            caption: '',
-          },
-          marketApy: {
-            data: [
-              {
-                displayValue: '',
-                isNegative: false,
-              },
-            ],
-          },
-          amountPaid: '',
-          presentValue: '',
-          earnings: '',
-          toolTipData: undefined,
-          actionRow: undefined,
-          tokenId: ' ',
-          isTotalRow: true,
-          isDividerRow: true,
-        });
-        break;
-      }
-    }
-    return arr;
-  }
 
   return {
     portfolioHoldingsColumns: Columns,
