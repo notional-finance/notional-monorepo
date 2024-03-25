41bac1b6c6106914872152cdce64771c545c684a
Jeff Wu <jeff@notional.finance>
2024-03-25 07:57:49 -0700

    fix: disable vaults and update apy chart (#697)

--- packages/features/liquidity/src/liquidity-variable/hooks/use-apy-chart.tsx
+++ packages/features/liquidity/src/liquidity-variable/hooks/use-apy-chart.tsx
@@ -1,10 +1,9 @@
 import { TokenDefinition } from '@notional-finance/core-entities';
-import { Network, THEME_VARIANTS } from '@notional-finance/util';
+import { THEME_VARIANTS } from '@notional-finance/util';
 import { colors } from '@notional-finance/styles';
 import {
   useTokenHistory,
   useThemeVariant,
-  useSelectedNetwork,
 } from '@notional-finance/notionable-hooks';
 import { FormattedMessage } from 'react-intl';
 import { useTheme } from '@mui/material';
@@ -12,7 +11,6 @@ import { BarConfigProps } from '@notional-finance/mui';
 
 export const useApyChart = (token?: TokenDefinition, defaultDataLimit = 50) => {
   const { apyIncentiveData } = useTokenHistory(token);
-  const selectedNetwork = useSelectedNetwork();
   const themeVariant = useThemeVariant();
   const theme = useTheme();
 
@@ -30,51 +28,55 @@ export const useApyChart = (token?: TokenDefinition, defaultDataLimit = 50) => {
   if (barChartData.length > defaultDataLimit) {
     barChartData = barChartData.slice(barChartData.length - defaultDataLimit);
   }
+  const hasNOTEIncentives = !!barChartData.find(
+    ({ noteApy }) => noteApy !== undefined
+  );
+  const hasARBIncentives = !!barChartData.find(
+    ({ arbApy }) => arbApy !== undefined
+  );
+  const hasIncentives = hasARBIncentives || hasNOTEIncentives;
 
   const barConfig: BarConfigProps[] = [
     {
       dataKey: 'organicApy',
-      title:
-        selectedNetwork === Network.ArbitrumOne ? (
-          <FormattedMessage defaultMessage="ORGANIC APY" />
-        ) : (
-          <FormattedMessage defaultMessage="TOTAL APY" />
-        ),
-      toolTipTitle:
-        selectedNetwork === Network.ArbitrumOne ? (
-          <FormattedMessage defaultMessage="ORGANIC APY" />
-        ) : (
-          <FormattedMessage defaultMessage="TOTAL APY" />
-        ),
-      fill:
-        selectedNetwork === Network.ArbitrumOne
-          ? theme.palette.background.accentDefault
-          : theme.palette.charts.main,
+      title: hasIncentives ? (
+        <FormattedMessage defaultMessage="ORGANIC APY" />
+      ) : (
+        <FormattedMessage defaultMessage="TOTAL APY" />
+      ),
+      toolTipTitle: hasIncentives ? (
+        <FormattedMessage defaultMessage="ORGANIC APY" />
+      ) : (
+        <FormattedMessage defaultMessage="TOTAL APY" />
+      ),
+      fill: hasIncentives
+        ? theme.palette.background.accentDefault
+        : theme.palette.charts.main,
       value: '0',
     },
   ];
 
-  if (selectedNetwork === Network.ArbitrumOne) {
-    barConfig.push(
-      {
-        dataKey: 'arbApy',
-        title: <FormattedMessage defaultMessage="ARB APY" />,
-        toolTipTitle: <FormattedMessage defaultMessage="ARB APY" />,
-        fill:
-          themeVariant === THEME_VARIANTS.LIGHT
-            ? colors.greenGrey
-            : colors.darkGrey,
-        value: '0',
-      },
-      {
-        dataKey: 'noteApy',
-        title: <FormattedMessage defaultMessage="NOTE APY" />,
-        toolTipTitle: <FormattedMessage defaultMessage="NOTE APY" />,
-        fill: theme.palette.primary.light,
-        radius: [8, 8, 0, 0],
-        value: '0',
-      }
-    );
+  if (hasARBIncentives) {
+    barConfig.push({
+      dataKey: 'arbApy',
+      title: <FormattedMessage defaultMessage="ARB APY" />,
+      toolTipTitle: <FormattedMessage defaultMessage="ARB APY" />,
+      fill:
+        themeVariant === THEME_VARIANTS.LIGHT
+          ? colors.greenGrey
+          : colors.darkGrey,
+      value: '0',
+    });
+  }
+  if (hasNOTEIncentives) {
+    barConfig.push({
+      dataKey: 'noteApy',
+      title: <FormattedMessage defaultMessage="NOTE APY" />,
+      toolTipTitle: <FormattedMessage defaultMessage="NOTE APY" />,
+      fill: theme.palette.primary.light,
+      radius: [8, 8, 0, 0],
+      value: '0',
+    });
   }
 
   return { barConfig, barChartData };
