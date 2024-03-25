e089834a930ee5736a19d6cc7222aa8de2d51036
Jeff Wu <jeff@notional.finance>
2024-03-24 20:52:48 -0700

    Bugfix/arb incentive rate (#696)
    * fix: filter secondary incentive rate on mainnet
    
    * fix: analytics registry errors

--- packages/core-entities/src/client/analytics-registry-client.ts
+++ packages/core-entities/src/client/analytics-registry-client.ts
@@ -215,7 +215,10 @@ export class AnalyticsRegistryClient extends ClientRegistry<unknown> {
                       floorToMidnight(r.timestamp)
                     )
                     .toFloat();
-                } else if (o.oracleType === 'nTokenSecondaryIncentiveRate') {
+                } else if (
+                  o.oracleType === 'nTokenSecondaryIncentiveRate' &&
+                  token.network === Network.ArbitrumOne
+                ) {
                   // NOTE: this token is currently hardcoded but we will need to make it configurable
                   // at some point in the future.
                   apy = TokenBalance.fromFloat(
@@ -254,10 +257,14 @@ export class AnalyticsRegistryClient extends ClientRegistry<unknown> {
   }
 
   getNTokenFeeRate(token: TokenDefinition) {
-    const apyData = this.getHistoricalAPY(token);
+    try {
+      const apyData = this.getHistoricalAPY(token);
 
-    if (apyData.length === 0) return 0;
-    else return apyData[apyData.length - 1]['nTokenFeeRate'] || 0;
+      if (apyData.length === 0) return 0;
+      else return apyData[apyData.length - 1]['nTokenFeeRate'] || 0;
+    } catch {
+      return 0;
+    }
   }
 
   getVault(network: Network, vaultAddress: string) {
