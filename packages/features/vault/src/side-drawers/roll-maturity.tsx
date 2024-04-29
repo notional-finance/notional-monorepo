0f5ea07572af6f76cf7cd864f5007dfcf3878db9
Jeff Wu <jeff@notional.finance>
2024-04-29 06:49:21 -0700

    fix: adding error messages

--- packages/features/vault/src/side-drawers/roll-maturity.tsx
+++ packages/features/vault/src/side-drawers/roll-maturity.tsx
@@ -1,9 +1,28 @@
 import { VaultActionContext } from '../vault';
 import { VaultSideDrawer } from '../components';
 import { useContext } from 'react';
+import { useVaultActionErrors } from '../hooks';
+import { ErrorMessage } from '@notional-finance/mui';
+import { FormattedMessage } from 'react-intl';
 
 export const RollMaturity = () => {
   const context = useContext(VaultActionContext);
+  const { leverageRatioError, inputErrorMsg } = useVaultActionErrors();
 
-  return <VaultSideDrawer context={context} />;
+  return (
+    <VaultSideDrawer context={context}>
+      {leverageRatioError && (
+        <ErrorMessage
+          variant="error"
+          message={<FormattedMessage {...leverageRatioError} />}
+        />
+      )}
+      {inputErrorMsg && (
+        <ErrorMessage
+          variant="error"
+          message={<FormattedMessage {...inputErrorMsg} />}
+        />
+      )}
+    </VaultSideDrawer>
+  );
 };
