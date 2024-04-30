58717a3a88155b464a2f5459c82abcc2047eb8d5
matthew-garrett <98406539+matthew-garrett@users.noreply.github.com>
2024-04-30 09:01:30 -0400

    build: adds portfolio link with view as address (#781)

--- packages/shared/web/src/header/header.tsx
+++ packages/shared/web/src/header/header.tsx
@@ -1,28 +1,38 @@
 import { useState } from 'react';
 import { Toolbar, Box, useTheme, ThemeProvider, styled } from '@mui/material';
-import { AppBar, AppBarProps, H4 } from '@notional-finance/mui';
+import { AppBar, AppBarProps, Body, H4 } from '@notional-finance/mui';
 import { NotionalLogo } from '@notional-finance/styles';
-import { THEME_VARIANTS } from '@notional-finance/util';
+import {
+  THEME_VARIANTS,
+  getFromLocalStorage,
+  setInLocalStorage,
+} from '@notional-finance/util';
 import { useNotionalTheme } from '@notional-finance/styles';
 import Navigation from './navigation/navigation';
 import { useNavLinks } from './use-nav-links';
 import MobileNavigation from './mobile-navigation/mobile-navigation';
 import { useLocation } from 'react-router';
 // import blitz from '@notional-finance/mui/src/assets/icons/blitz.svg';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import {
   useNotionalContext,
+  useSelectedNetwork,
   // showContestNavLink,
 } from '@notional-finance/notionable-hooks';
 import AnalyticsDropdown from './analytics-dropdown/analytics-dropdown';
 import ScrollIndicator from './scroll-indicator/scroll-indicator';
 import { colors } from '@notional-finance/styles';
 import { FormattedMessage } from 'react-intl';
+import { AlertIcon } from '@notional-finance/icons';
 
 /* eslint-disable-next-line */
 export interface HeaderProps extends AppBarProps {}
 
 export function Header({ children }: HeaderProps) {
   const [isTop, setIsTop] = useState(true);
+  const selectedNetwork = useSelectedNetwork();
+  const [hideError, setHideError] = useState(false);
+  const hideSubGraphError = getFromLocalStorage('hideSubGraphError');
   const landingTheme = useNotionalTheme(THEME_VARIANTS.DARK);
   const contestTheme = useNotionalTheme(THEME_VARIANTS.DARK, 'product');
   const appTheme = useTheme();
@@ -35,13 +45,26 @@ export function Header({ children }: HeaderProps) {
       : appTheme;
   const { navLinks } = useNavLinks(false, theme);
   const {
-    globalState: { hasSelectedChainError },
+    globalState: { hasSelectedChainError, networkAccounts },
   } = useNotionalContext();
+
+  const subGraphError =
+    networkAccounts &&
+    networkAccounts[selectedNetwork]?.isSubgraphDown &&
+    !hideError &&
+    hideSubGraphError !== true
+      ? true
+      : false;
 
   // NOTE: Leaving this here for when we want to have another contest
   // const handleContestClick = () => {
   //   history.push('/contest');
   // };
+
+  const handleErrorMessage = () => {
+    setHideError(true);
+    setInLocalStorage('hideSubGraphError', true);
+  };
 
   window.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -117,6 +140,40 @@ export function Header({ children }: HeaderProps) {
             {children}
           </WalletContainer>
         </Toolbar>
+        {subGraphError && (
+          <ErrorContainer>
+            <Box
+              sx={{
+                width: '100%',
+                paddingLeft: theme.spacing(3),
+                paddingRight: theme.spacing(3),
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <AlertIcon
+                sx={{ fill: colors.black }}
+                innerFill={colors.orange}
+              />
+              <Body sx={{ color: colors.black, padding: theme.spacing(2) }}>
+                <FormattedMessage
+                  defaultMessage={
+                    'Trouble connecting to Notional subgraph, historical data, transaction history and earnings calculations may not load. Transactions will continue to function properly.'
+                  }
+                />
+              </Body>
+              <HighlightOffIcon
+                onClick={handleErrorMessage}
+                sx={{
+                  fill: colors.black,
+                  fontSize: theme.spacing(3.6),
+                  cursor: 'pointer',
+                }}
+              />
+            </Box>
+          </ErrorContainer>
+        )}
         {hasSelectedChainError && (
           <Box
             sx={{
@@ -162,6 +219,26 @@ const WalletContainer = styled(Box)(
     align-items: center;
     ${theme.breakpoints.down('sm')} {
       display: none;
+    }
+      `
+);
+
+const ErrorContainer = styled(Box)(
+  ({ theme }) => `
+    width: ${theme.spacing(85)};
+    margin: auto;
+    background: ${colors.orange};
+    border-radius: ${theme.shape.borderRadius()};
+    display: flex;
+    align-items: center;
+    color: ${colors.black};
+    margin-top: ${theme.spacing(2)};
+    ${theme.breakpoints.down('sm')} {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      text-align: center;
     }
       `
 );
