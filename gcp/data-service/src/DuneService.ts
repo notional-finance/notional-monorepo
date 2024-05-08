c95802cd2f7bce8a7d881cd2ff870d4871d02317
Jeff Wu <jeff@notional.finance>
2024-05-08 16:36:53 -0700

    fix: dune service

--- gcp/data-service/src/DuneService.ts
+++ gcp/data-service/src/DuneService.ts
@@ -1,25 +1,29 @@
 import { PutObjectCommand } from '@aws-sdk/client-s3';
-import { DuneClient } from '@duneanalytics/client-sdk';
 import { S3 } from './RiskService';
+import fetch from 'cross-fetch';
 
 const DUNE_API_KEY = process.env['DUNE_API_KEY'] as string;
-const dune = new DuneClient(DUNE_API_KEY);
 const queries = [
   { queryId: 3709164, name: 'sNOTEPoolData' },
   { queryId: 3709178, name: 'sNOTEReinvestment' },
 ];
 
 export async function syncDune() {
-  await Promise.all(
-    queries.map(async ({ queryId, name }) => {
-      const query_result = await dune.runQuery({ queryId });
-      await S3.send(
-        new PutObjectCommand({
-          Bucket: 'account-cache-r2',
-          Key: `mainnet/note/${name}`,
-          Body: JSON.stringify(query_result),
-        })
-      );
-    })
-  );
+  for (const q of queries) {
+    const { queryId, name } = q;
+    const query_result = await fetch(
+      `https://api.dune.com/api/v1/query/${queryId}/results`,
+      {
+        method: 'GET',
+        headers: { 'X-DUNE-API-KEY': DUNE_API_KEY },
+      }
+    );
+    await S3.send(
+      new PutObjectCommand({
+        Bucket: 'account-cache-r2',
+        Key: `mainnet/note/${name}`,
+        Body: JSON.stringify(await query_result.json()),
+      })
+    );
+  }
 }
