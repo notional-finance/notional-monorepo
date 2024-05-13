a095b39a36a0f4872c0ce9be388bc0abda763dcc
Sime <sbuljac@gmail.com>
2024-05-13 19:34:22 +0200

    feat: add worker for market initialization and account settlement (#789)
    * feat: add worker for market initialization and account settlement
    
    * fix: address CR feedback
    
    * fix: increase graph limit to 1000
    
    * fix: fund the signer

---
+++ apps/initialize-markets/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'initialize-markets',
+  preset: '../../jest.preset.js',
+  globals: {
+    fetch: global.fetch,
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      useESM: true,
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  testTimeout: 50000,
+};
