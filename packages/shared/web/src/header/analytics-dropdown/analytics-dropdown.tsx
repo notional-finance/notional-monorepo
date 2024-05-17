d6e5e4894c882bfc8c1655af3d9f6657568790f8
matthew-garrett <matthew.garrett252@gmail.com>
2024-05-17 15:57:55 -0400

    build: removes comments and updates styles

--- packages/shared/web/src/header/analytics-dropdown/analytics-dropdown.tsx
+++ packages/shared/web/src/header/analytics-dropdown/analytics-dropdown.tsx
@@ -31,8 +31,6 @@ export function AnalyticsDropdown() {
     <DropdownButton
       popupId="analytics"
       labelKey={messages[NAV_DROPDOWN.ANALYTICS]}
-      // anchorPosition={{ top: 73, left: 0 }}
-      // anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
       activeTab={currentTab ? true : false}
       anchorReference="anchorEl"
       hideOnClick={false}
@@ -43,7 +41,6 @@ export function AnalyticsDropdown() {
         padding: '6px 15px',
       }}
       customPopOverStyles={{
-        // top: '73px',
         right: 0,
         width: theme.spacing(82),
         overflow: 'visible',
