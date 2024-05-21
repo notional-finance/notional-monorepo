fe0576a617025245bf413dcd6847940ed4848cb8
Jeff Wu <jeff@notional.finance>
2024-05-21 08:42:56 -0700

    Feat/support gho (#833)
    * feat: add gho icons
    
    * fix: restricting max currencies
    
    * fix: adding gho alt
    
    * fix: add liquidation override
    
    * fix: add expected nToken address
    
    * fix: add comment

---
+++ packages/core-entities/src/config/whitelisted-tokens.ts
@@ -0,0 +1,27 @@
+import { Network } from '@notional-finance/util';
+
+export function getSecondaryTokenIncentive(network: Network, base: string) {
+  if (network === Network.arbitrum) {
+    return 'ARB';
+  } else if (
+    network === Network.mainnet &&
+    // GHO token
+    base === '0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f'
+  ) {
+    return 'GHO';
+  } else {
+    return undefined;
+  }
+}
+
+/**
+ * If defined, this is an inclusive restriction on the currency ids shown on a given network,
+ * in other words, if there are 11 currency ids and the MaxCurrencyId is set to 10, then the 11th
+ * currency will not be shown on the UI.
+ */
+export const MaxCurrencyId: Record<Network, number | undefined> = {
+  [Network.all]: undefined,
+  [Network.optimism]: undefined,
+  [Network.mainnet]: 10,
+  [Network.arbitrum]: undefined,
+};
