43e0c34c21668ef4418c9d71a5e3d8f7b074c97c
sbuljac <sbuljac@gmail.com>
2024-06-12 18:05:44 +0200

    fix: throw if tx fails in tx-relayer

--- packages/util/src/tx-relayer.ts
+++ packages/util/src/tx-relayer.ts
@@ -40,5 +40,14 @@ export async function sendTxThroughRelayer(arg: {
       'X-Auth-Token': env.TX_RELAY_AUTH_TOKEN,
     },
     body: payload,
-  }).then(r => r.json());
+  }).then(async r => {
+      const returnData = await r.json();
+
+      if (299 < r.status) {
+        console.error(returnData);
+        throw new Error(returnData.reason || returnData.code || r.statusText);
+      }
+
+      return returnData;
+    });
 }
