2192ce9a4251d24a7797552e1e72a49e2adc1ddd
Sime <sbuljac@gmail.com>
2024-06-16 19:59:37 +0200

    feat: use secrets manager for vault-apy (#905)

--- gcp/vault-apy/deploy.sh
+++ gcp/vault-apy/deploy.sh
@@ -10,7 +10,10 @@ gcloud run jobs replace job.yaml
 # on each new deploy
 # gcloud scheduler jobs update http vault-apy-scheduler \
 #   --location "us-east1" \
-#   --schedule="0 0 * * *" \
+#   --schedule="1 0 * * *" \
+#   --max-retry-attempts=3 \
+#   --min-backoff=5m \
+#   --max-backoff=15m \
 #   --uri="https://us-east1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/monitoring-agents/jobs/vault-apy:run" \
 #   --http-method POST \
 #   --oauth-service-account-email 663932775145-compute@developer.gserviceaccount.com
