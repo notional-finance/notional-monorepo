228b05cc79b0e1d82c7dcf4870d87be8833a1568
Jeff Wu <jeff@notional.finance>
2024-06-18 12:56:23 -0700

    fix: turn off arb overrides for testing

--- apps/liquidation-bot/src/config/overrides.ts
+++ apps/liquidation-bot/src/config/overrides.ts
@@ -4,31 +4,31 @@ import { BigNumber } from 'ethers';
 
 export const overrides = {
   [Network.arbitrum]: [
-    {
-      // cbETH: flash borrow rETH
-      id: 9,
-      flashBorrowAsset: '0xEC70Dcb4A1EFa46b8F2D97C310C9c4790ba5ffA8',
-    },
-    {
-      // GMX: flash borrow WETH
-      id: 10,
-      flashBorrowAsset: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-    },
-    {
-      // RDNT: flash borrow WETH
-      id: 12,
-      flashBorrowAsset: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-    },
-    {
-      // UNI: flash borrow WETH
-      id: 14,
-      flashBorrowAsset: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-    },
-    {
-      // LDO: flash borrow WETH
-      id: 15,
-      flashBorrowAsset: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-    },
+    // {
+    //   // cbETH: flash borrow rETH
+    //   id: 9,
+    //   flashBorrowAsset: '0xEC70Dcb4A1EFa46b8F2D97C310C9c4790ba5ffA8',
+    // },
+    // {
+    //   // GMX: flash borrow WETH
+    //   id: 10,
+    //   flashBorrowAsset: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    // },
+    // {
+    //   // RDNT: flash borrow WETH
+    //   id: 12,
+    //   flashBorrowAsset: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    // },
+    // {
+    //   // UNI: flash borrow WETH
+    //   id: 14,
+    //   flashBorrowAsset: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    // },
+    // {
+    //   // LDO: flash borrow WETH
+    //   id: 15,
+    //   flashBorrowAsset: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    // },
   ] as CurrencyOverride[],
   [Network.mainnet]: [
     {
