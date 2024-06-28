7439bf85c02f380e580d9671efc105120f078bfd
Jeff Wu <jeff@notional.finance>
2024-06-28 09:12:22 -0700

    fix: update index

--- gcp/vault-apy/src/index.ts
+++ gcp/vault-apy/src/index.ts
@@ -5,12 +5,12 @@ import APYSimulator from './APYSimulator';
 
 const log = require('debug')('vault-apy');
 
-process.on('exit', async function() {
+process.on('exit', async function () {
   // cleanup
-  await execPromise("pkill anvil").finally(() => null);
+  await execPromise('pkill anvil').finally(() => null);
 });
 
-(async function() {
+(async function () {
   const info = `
     run without arguments for daily calculation for all vaults
     run with historical as first argument for calculations for the last 7 days for all vaults
@@ -24,42 +24,55 @@ process.on('exit', async function() {
 
   if (process.argv.length == 2) {
     for (const network of networks) {
-      log(`processing daily network ${network}`)
+      log(`processing daily network ${network}`);
 
       const apySimulator = new APYSimulator(network);
-      await apySimulator.run()
+      await apySimulator.run();
 
-      log("processing completed");
+      log('processing completed');
     }
-  } else if (process.argv[2].toLowerCase() == 'historical' && process.argv.length == 3) {
+  } else if (
+    process.argv[2].toLowerCase() == 'historical' &&
+    process.argv.length == 3
+  ) {
     for (const network of networks) {
       const apySimulator = new APYSimulator(network);
-      log(`processing historical apy network ${network} on date ${startOfToday.toISOString()}`)
+      log(
+        `processing historical apy network ${network} on date ${startOfToday.toISOString()}`
+      );
 
-      await apySimulator.runHistorical(84, startOfToday);
+      await apySimulator.runHistorical(90, startOfToday);
 
-      log("processing completed");
-
+      log('processing completed');
     }
-  } else if (process.argv[2].toLowerCase() == 'historical' && process.argv.length == 5) {
+  } else if (
+    process.argv[2].toLowerCase() == 'historical' &&
+    process.argv.length == 5
+  ) {
     const [, , , network, vaultAddress] = process.argv;
     if (!configPerNetwork[network]) {
       throw new Error('Invalid network name');
     }
-    if (!configPerNetwork[network as Network].vaults.find(v => v.address.toLowerCase() === vaultAddress.toLowerCase())) {
-      throw new Error(`Vault address ${vaultAddress} does not exist in config file`);
+    if (
+      !configPerNetwork[network as Network].vaults.find(
+        (v) => v.address.toLowerCase() === vaultAddress.toLowerCase()
+      )
+    ) {
+      throw new Error(
+        `Vault address ${vaultAddress} does not exist in config file`
+      );
     }
 
-    log(`processing historical apy for vault: ${vaultAddress} on network ${network}`)
+    log(
+      `processing historical apy for vault: ${vaultAddress} on network ${network}`
+    );
 
     const apySimulator = new APYSimulator(network as Network);
     await apySimulator.runHistoricalForVault(vaultAddress, 30, startOfToday);
 
-    log("processing completed");
+    log('processing completed');
   } else {
-    console.log("Invalid arguments");
+    console.log('Invalid arguments');
     console.log(info);
   }
-}()).then(() => process.exit());
-
-
+})().then(() => process.exit());
