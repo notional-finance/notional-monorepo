d34e462a9d707add3f26a9d58309e5f53c9c691f
Jeff Wu <jeff@notional.finance>
2024-07-01 07:07:44 -0700

    fix: hide arbitrum rseth vault

--- packages/core-entities/src/config/whitelisted-vaults.ts
+++ packages/core-entities/src/config/whitelisted-vaults.ts
@@ -31,10 +31,10 @@ export const PointsMultipliers: Record<
       Renzo: 4,
       EigenLayer: (v as SingleSidedLP).getTokenPoolShare(0),
     }),
-    '0xcac9c01d1207e5d06bb0fd5b854832f35fe97e68': (v) => ({
-      Kelp: 3,
-      EigenLayer: (v as SingleSidedLP).getTokenPoolShare(0),
-    }),
+    // '0xcac9c01d1207e5d06bb0fd5b854832f35fe97e68': (v) => ({
+    //   Kelp: 3,
+    //   EigenLayer: (v as SingleSidedLP).getTokenPoolShare(0),
+    // }),
   },
   [Network.optimism]: {},
 };
@@ -110,7 +110,7 @@ export const whitelistedVaults = (network: Network) => {
         // "[WBTC]:xWBTC_tBTC":
         '0xf95441f348eb2fd3d5d82f9b7b961137a734eedd',
         // "[ETH]:rsETH_xWETH":
-        '0xcac9c01d1207e5d06bb0fd5b854832f35fe97e68',
+        // '0xcac9c01d1207e5d06bb0fd5b854832f35fe97e68',
       ];
     case Network.optimism:
       return [];
