2d45717f8df243009b4bc9c85aa4923a8f468c1a
sbuljac <sbuljac@gmail.com>
2024-07-16 11:20:57 +0200

    fix: remove console log

--- packages/core-entities/src/base/base-registry.ts
+++ packages/core-entities/src/base/base-registry.ts
@@ -289,7 +289,6 @@ export abstract class BaseRegistry<T> {
     key: string,
     checkFreshness = this.defaultFreshnessIntervals
   ) {
-    console.log("network", network);
     // Don't check freshness if no interval is set
     const intervalMS = this._intervalMS.get(network);
     if (checkFreshness > 0 && intervalMS) {
