e48a2f6e8e51a716d38bf0a2e761807c01a96d70
Jeff Wu <jeff@notional.finance>
2024-07-29 14:07:49 -0700

    fix: worker deployment affected only (#1006)

--- apps/deployWorkers.sh
+++ apps/deployWorkers.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 set -e
 
-yarn nx publish-wrangler-manual accounts --env dev
-yarn nx publish-wrangler-manual api --env dev
-# NOTE: currently disabled because this causes issues
-# yarn nx publish-wrangler-manual data --env dev
+yarn nx affected -t publish-wrangler --base=v3/prod~1 --head=main
 
-# Registry
-yarn nx publish-wrangler-manual registry-configuration --env dev
-yarn nx publish-wrangler-manual registry-vaults --env dev
-yarn nx publish-wrangler-manual registry-tokens --env dev
-yarn nx publish-wrangler-manual registry-oracles --env dev
-yarn nx publish-wrangler-manual registry-exchanges --env dev
+# yarn nx publish-wrangler-manual accounts --env dev
+# yarn nx publish-wrangler-manual api --env dev
+# # NOTE: currently disabled because this causes issues
+# # yarn nx publish-wrangler-manual data --env dev
 
-# Bots
-yarn nx publish-wrangler-manual note-reward-manager --env dev
-yarn nx publish-wrangler-manual rewards --env dev
-yarn nx publish-wrangler-manual initialize-markets --env dev
-yarn nx publish-wrangler-manual liquidation-bot --env arbitrum
-yarn nx publish-wrangler-manual liquidation-bot --env mainnet
-yarn nx publish-wrangler-manual vault-liquidator --env arbitrum
-yarn nx publish-wrangler-manual vault-liquidator --env mainnet
+# # Registry
+# yarn nx publish-wrangler-manual registry-configuration --env dev
+# yarn nx publish-wrangler-manual registry-vaults --env dev
+# yarn nx publish-wrangler-manual registry-tokens --env dev
+# yarn nx publish-wrangler-manual registry-oracles --env dev
+# yarn nx publish-wrangler-manual registry-exchanges --env dev
 
-# Points
-yarn nx publish-wrangler-manual points --env prod
+# # Bots
+# yarn nx publish-wrangler-manual note-reward-manager --env dev
+# yarn nx publish-wrangler-manual rewards --env dev
+# yarn nx publish-wrangler-manual initialize-markets --env dev
+# yarn nx publish-wrangler-manual liquidation-bot --env arbitrum
+# yarn nx publish-wrangler-manual liquidation-bot --env mainnet
+# yarn nx publish-wrangler-manual vault-liquidator --env arbitrum
+# yarn nx publish-wrangler-manual vault-liquidator --env mainnet
+
+# # Points
+# yarn nx publish-wrangler-manual points --env prod
