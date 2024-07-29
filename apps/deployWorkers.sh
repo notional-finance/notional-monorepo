d115bc94f6fee57a1d8395399c0c478489c03c3b
Jeff Wu <jeff@notional.finance>
2024-07-29 14:15:14 -0700

    fix: deploy workers script

--- apps/deployWorkers.sh
+++ apps/deployWorkers.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 set -e
 
-yarn nx publish-wrangler-manual accounts --env dev
-yarn nx publish-wrangler-manual api --env dev
-# NOTE: currently disabled because this causes issues
-# yarn nx publish-wrangler-manual data --env dev
+yarn nx affected --target publish-wrangler --base=v3/prod~1 --head=main
 
-# Registry
-yarn nx publish-wrangler-manual registry-configuration --env dev
-yarn nx publish-wrangler-manual registry-vaults --env dev
-yarn nx publish-wrangler-manual registry-tokens --env dev
-yarn nx publish-wrangler-manual registry-oracles --env dev
-yarn nx publish-wrangler-manual registry-exchanges --env dev
+# # Registry
+# yarn nx publish-wrangler-manual registry-configuration --env dev
+# yarn nx publish-wrangler-manual registry-vaults --env dev
+# yarn nx publish-wrangler-manual registry-tokens --env dev
+# yarn nx publish-wrangler-manual registry-oracles --env dev
+# yarn nx publish-wrangler-manual registry-exchanges --env dev
 
-# Bots
-yarn nx publish-wrangler-manual note-reward-manager --env dev
-yarn nx publish-wrangler-manual rewards --env dev
-yarn nx publish-wrangler-manual initialize-markets --env dev
-yarn nx publish-wrangler-manual liquidation-bot --env arbitrum
-yarn nx publish-wrangler-manual liquidation-bot --env mainnet
-yarn nx publish-wrangler-manual vault-liquidator --env arbitrum
-yarn nx publish-wrangler-manual vault-liquidator --env mainnet
+# # Bots
+# yarn nx publish-wrangler-manual note-reward-manager --env dev
+# yarn nx publish-wrangler-manual rewards --env dev
+# yarn nx publish-wrangler-manual initialize-markets --env dev
+# yarn nx publish-wrangler-manual liquidation-bot --env arbitrum
+# yarn nx publish-wrangler-manual liquidation-bot --env mainnet
+# yarn nx publish-wrangler-manual vault-liquidator --env arbitrum
+# yarn nx publish-wrangler-manual vault-liquidator --env mainnet
 
-# Points
-yarn nx publish-wrangler-manual points --env prod
+# # Points
+# yarn nx publish-wrangler-manual points --env prod
