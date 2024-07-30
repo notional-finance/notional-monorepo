379a4af087407d8b5b9df9683758990de2c9a597
matthew-garrett <matthew.garrett252@gmail.com>
2024-07-30 13:32:56 -0400

    build: adds mm and cb wallet default as available

--- packages/shared/wallet/src/onboard-context.tsx
+++ packages/shared/wallet/src/onboard-context.tsx
@@ -6,6 +6,9 @@ import {
   EIP6963ProviderDetail,
 } from '@web3-onboard/injected-wallets/dist/types';
 import walletConnectModule from '@web3-onboard/walletconnect';
+import coinbaseModule from '@web3-onboard/coinbase';
+import MetaMask from './images/meta-mask.svg';
+import CoinbaseWallet from './images/coinbase-wallet.svg';
 import trezorModule from '@web3-onboard/trezor';
 import WalletConnect from './images/wallet-connect.svg';
 import Trezor from './images/trezor.svg';
@@ -47,6 +50,14 @@ interface ResultInterface {
 export const useWalletModules = () => {
   const modules = [
     {
+      label: 'MetaMask',
+      icon: MetaMask,
+    },
+    {
+      label: 'Coinbase Wallet',
+      icon: CoinbaseWallet,
+    },
+    {
       label: 'WalletConnect',
       icon: WalletConnect,
     },
@@ -58,15 +69,20 @@ export const useWalletModules = () => {
   const checkProvider = new Map<string, boolean>();
   const injectedWallets: ResultInterface[] = [];
 
-  providers.forEach(({ info }) => {
-    if (checkProvider.has(info.name)) return info;
-    checkProvider.set(info.name, true);
-    injectedWallets.push({
-      label: info.name,
-      icon: info.icon,
+  providers
+    .filter(
+      ({ info }) =>
+        !info.name?.includes('Coinbase') && !info.name?.includes('MetaMask')
+    )
+    .forEach(({ info }) => {
+      if (checkProvider.has(info.name)) return info;
+      checkProvider.set(info.name, true);
+      injectedWallets.push({
+        label: info.name,
+        icon: info.icon,
+      });
+      return info;
     });
-    return info;
-  });
 
   return injectedWallets.length > 0
     ? [...injectedWallets, ...modules]
@@ -84,6 +100,7 @@ const wcV2InitOptions = {
 
 const wallets = [
   injectedModule(),
+  coinbaseModule(),
   walletConnectModule(wcV2InitOptions),
   trezorModule({
     email,
