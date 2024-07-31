d9a1084d4afcdccf0cddd2a9916cdcc4d39271c8
Jeff Wu <jeff@notional.finance>
2024-07-31 11:03:45 -0700

    refactor: configuration registry

--- apps/registry-configuration/src/index.ts
+++ apps/registry-configuration/src/index.ts
@@ -1,19 +1,19 @@
 import { Request } from '@cloudflare/workers-types';
 import {
   BaseDOEnv,
-  ConfigurationRegistryDO,
+  refreshRegistry,
+  Servers,
 } from '@notional-finance/durable-objects';
-
-const run = async (env: BaseDOEnv) => {
-  const c = new ConfigurationRegistryDO(env);
-  await c.onRefresh();
-};
 
 export default {
   async fetch(_: Request, env: BaseDOEnv): Promise<Response> {
-    return run(env).then(() => new Response('OK'));
+    return refreshRegistry(
+      env,
+      'configuration',
+      Servers.ConfigurationServer
+    ).then(() => new Response('OK'));
   },
   async scheduled(_: ScheduledController, env: BaseDOEnv): Promise<void> {
-    return run(env);
+    return refreshRegistry(env, 'configuration', Servers.ConfigurationServer);
   },
 };
