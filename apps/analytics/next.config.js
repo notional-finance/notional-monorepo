162352b6947a635602dd1d5b07e678e6e7ef6f04
Jeff Wu <jeff@notional.finance>
2024-08-06 09:20:24 -0700

    fix: adding cloudflare types

--- apps/analytics/next.config.js
+++ apps/analytics/next.config.js
@@ -1,3 +1,10 @@
+// note: the if statement is present because you
+//       only need to use the function during development
+if (process.env.NODE_ENV === 'development') {
+  const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
+  setupDevPlatform();
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   modularizeImports: {
