f549838d43ef2fa477a9b99045e2156858fc233b
Sime <sbuljac@gmail.com>
2024-08-30 16:27:20 +0200

    feat: tx-relay aditional adaptations

--- apps/tx-relay/deploy.sh
+++ apps/tx-relay/deploy.sh
@@ -1,1 +1,20 @@
-gcloud --project monitoring-agents app deploy app.yaml
+echo "Deploying tx-relay service"
+
+cp package.json ../../dist/apps/tx-relay
+cd ../../dist/apps/tx-relay
+
+gcloud --project monitoring-agents \
+    functions deploy tx-relay \
+    --region us-central1 \
+    --runtime nodejs22 \
+    --trigger-http \
+    --allow-unauthenticated \
+    --entry-point=txRelayService \
+    --gen2 \
+    --timeout=600 \
+    --concurrency=1 \
+    --cpu=1 \
+    --memory=256 \
+  --set-secrets=\
+AUTH_TOKEN=projects/663932775145/secrets/TX_RELAY_AUTH_TOKEN/versions/latest,\
+DD_API_KEY=projects/663932775145/secrets/DD_API_KEY/versions/latest
--- apps/tx-relay/src/index.ts
+++ apps/tx-relay/src/index.ts
@@ -1,61 +1,48 @@
-import express from "express";
+import { Request, Response } from "@google-cloud/functions-framework";
 import { sendTransaction, getAllSignerAddresses } from "./Signer";
 import { Network } from "./types";
-import { getEnvSecrets } from 'gae-env-secrets';
 
-const DEFAULT_SERVICE_PORT = "8000";
-
-export async function main() {
-  await getEnvSecrets({ autoDetect: true });
+export async function txRelayService(req: Request, res: Response) {
   if (!process.env.AUTH_TOKEN) {
     throw Error("AUTH_TOKEN not defined");
   }
 
-  const app = express();
-  const port = parseInt(process.env.SERVICE_PORT || DEFAULT_SERVICE_PORT);
+  switch (req.path) {
+    case "/":
+      res.send("OK");
+      break;
 
-  app.use(express.json());
+    case "/v1/signers":
+      if (req.headers["x-auth-token"] !== process.env.AUTH_TOKEN) {
+        res.status(401).send("Unauthorized");
+        return;
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(await getAllSignerAddresses()));
+      break;
 
-  app.get("/", (_, res) => {
-    res.send("OK");
-  });
+    default:
+      if (req.path.startsWith("/v1/txes/")) {
+        if (req.headers["x-auth-token"] !== process.env.AUTH_TOKEN) {
+          res.status(401).send("Unauthorized");
+          return;
+        }
 
-  app.get("/v1/signers", async (req, res) => {
-    if (req.headers["x-auth-token"] !== process.env.AUTH_TOKEN) {
-      res.status(401).send("Unauthorized");
-      return;
-    }
+        const network: Network = req.path.split("/")[3] as Network;
 
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(await getAllSignerAddresses()));
-  });
-
-  app.post("/v1/txes/:network", async (req, res) => {
-    if (req.headers["x-auth-token"] !== process.env.AUTH_TOKEN) {
-      res.status(401).send("Unauthorized");
-      return;
-    }
-
-    const network: Network = req.params["network"] as Network;
-
-    try {
-      const result = await sendTransaction({
-        ...req.body,
-        network,
-      });
-      res.status(200).send(JSON.stringify(result));
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(JSON.stringify(e));
-    }
-  });
-
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
-}
-
-if (require.main === module) {
-  require("dotenv").config();
-  main();
+        try {
+          const result = await sendTransaction({
+            ...req.body,
+            network,
+          });
+          res.status(200).send(JSON.stringify(result));
+        } catch (e) {
+          console.log(e);
+          res.status(500).send(JSON.stringify(e));
+        }
+      } else {
+        res.status(404).send("Not Found");
+      }
+      break;
+  }
 }
