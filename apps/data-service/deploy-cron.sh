c8bec04c75278c31dbb6fb00f462d6ab92a2492a
Sime <sbuljac@gmail.com>
2024-09-03 19:14:53 +0200

    Fix/cron deployment (#1086)
    * fix: cron deployment script
    
    * fix: change deployment script

--- apps/data-service/deploy-cron.sh
+++ apps/data-service/deploy-cron.sh
@@ -2,17 +2,15 @@
 set -e
 
 # Cron jobs to deploy, format is "Description|URL|Schedule"
-cron_jobs=(
-    "Risk_Service|/calculateRisk|*/10 * * * *"
-    "Sync_Dune|/syncDune|30 1 * * *"
-    "Calculate_Points|/calculatePoints|5 1 * * *"
-    "Sync_Generic_Data|/syncGenericData|0 * * * *"
-    "Sync_Oracle_Data|/syncOracleData|0 * * * *"
-    "Sync_Accounts_Arbitrum|/syncAccounts?network=arbitrum|*/20 * * * *"
-    "Sync_Accounts_Mainnet|/syncAccounts?network=mainnet|*/20 * * * *"
-    "Sync_Vault_Accounts_Arbitrum|/syncVaultAccounts?network=arbitrum|*/20 * * * *"
-    "Sync_Vault_Accounts_Mainnet|/syncVaultAccounts?network=mainnet|*/20 * * * *"
-)
+cron_jobs="Risk_Service|/calculateRisk|*/10 * * * *
+Sync_Dune|/syncDune|30 1 * * *
+Calculate_Points|/calculatePoints|5 1 * * *
+Sync_Generic_Data|/syncGenericData|0 * * * *
+Sync_Oracle_Data|/syncOracleData|0 * * * *
+Sync_Accounts_Arbitrum|/syncAccounts?network=arbitrum|*/20 * * * *
+Sync_Accounts_Mainnet|/syncAccounts?network=mainnet|*/20 * * * *
+Sync_Vault_Accounts_Arbitrum|/syncVaultAccounts?network=arbitrum|*/20 * * * *
+Sync_Vault_Accounts_Mainnet|/syncVaultAccounts?network=mainnet|*/20 * * * *"
 
 # Set the base URI as an environment variable
 # Get the function URI using gcloud
@@ -20,9 +18,7 @@ BASE_URI=$(gcloud functions describe cron-service --region=us-central1 --format=
 echo "Base URI: $BASE_URI"
 
 # Create jobs from cron_jobs array
-for job in "${cron_jobs[@]}"; do
-    IFS='|' read -r description url schedule <<< "$job"
-    
+echo "$cron_jobs" | while IFS='|' read -r description url schedule; do
     # Create/update the job
     job_name=$(echo "$description" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
 
