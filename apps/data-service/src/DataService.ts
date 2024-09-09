bf921d83e381c0670ec8f44660596746b864cb13
Jeff Wu <jeff@notional.finance>
2024-09-09 11:50:55 -0700

    fix: gcp and vault apy merge data (#1100)

--- apps/data-service/src/DataService.ts
+++ apps/data-service/src/DataService.ts
@@ -601,7 +601,7 @@ export default class DataService {
       )
       .into(DataService.VAULT_APY_NAME)
       .onConflict(['network_id', 'timestamp', 'vault_address', 'reward_token'])
-      .ignore();
+      .merge();
   }
 
   public async accounts(network: Network) {
