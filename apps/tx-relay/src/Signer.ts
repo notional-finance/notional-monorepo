2dfaf8837a25c617700f981957b1d4c9772df6ea
Sime <sbuljac@gmail.com>
2024-09-11 19:10:53 +0200

    feat: reabalance service (#1099)

--- apps/tx-relay/src/Signer.ts
+++ apps/tx-relay/src/Signer.ts
@@ -1,99 +1,29 @@
-import { TransactionRequest, TransactionResponse } from "@ethersproject/abstract-provider"
-import { ethers } from "ethers";
-import { GcpKmsSigner } from "ethers-gcp-kms-signer";
-import { Network } from "./types";
+import {
+  TransactionRequest,
+  TransactionResponse,
+} from '@ethersproject/abstract-provider';
+import { ethers, providers } from 'ethers';
+import { GcpKmsSigner } from 'ethers-gcp-kms-signer';
+import { Address, Sign, rpcUrls } from './config';
+import { Network } from './types';
+import { logToDataDog } from './util';
 
 const kmsCredentials = {
-  projectId: "monitoring-agents", // your project id in gcp
-  locationId: "global", // the location where your key ring was created
-  keyRingId: "autotasks", // the id of the key ring
-  keyId: "", // the name/id of your key in the key ring
-  keyVersion: "1", // the version of the key
+  projectId: 'monitoring-agents', // your project id in gcp
+  locationId: 'global', // the location where your key ring was created
+  keyRingId: 'autotasks', // the id of the key ring
+  keyId: '', // the name/id of your key in the key ring
+  keyVersion: '1', // the version of the key
 };
 
-const rpcUrls = {
-  mainnet: "https://rpc.mevblocker.io",
-  arbitrum: "https://arb-mainnet.g.alchemy.com/v2/pq08EwFvymYFPbDReObtP-SFw3bCes8Z",
-  sepolia: "https://eth-sepolia.g.alchemy.com/v2/pq08EwFvymYFPbDReObtP-SFw3bCes8Z",
-}
 enum Key {
   reinvestment = 'reinvestment',
-  liquidation = 'liquidation-2'
-}
-
-// addresses need to be lowercased
-const Address = {
-  TREASURY_MANAGER: '0x53144559c0d4a3304e2dd9dafbd685247429216d'.toLowerCase(),
-  AaveV3Pool_ARBITRUM: '0x794a61358d6845594f94dc1db02a252b5b4814ad'.toLowerCase(),
-  AaveV3Pool_MAINNET: '0x87870bca3f3fd6335c3f4ce8392d69350b4fa4e2'.toLowerCase(),
-  AaveFlashLiquidator_MAINNET: '0xb33e0b9e5ff443fdfe48d8a49f45828918bdab8c'.toLowerCase(),
-  AaveFlashLiquidator_ARBITRUM: '0x78e41d1389f3db7c196f13088d1f99a319ffcfbe'.toLowerCase(),
-  Multicall3: '0xcA11bde05977b3631167028862bE2a173976CA11'.toLowerCase(),
-  initializeAllMarkets_ARBITRUM: '0x9b6C04D1481473B2e52CaEB85822072C35460f27'.toLowerCase(),
-  settleAccounts_ARBITRUM: '0x22349F0b9b6294dA5526c9E9383164d97c45ACCD'.toLowerCase(),
-
-  AaveWrappedFlashLender_MAINNET: '0x0c86c636ed5593705b5675d370c831972C787841'.toLowerCase(),
-  BalancerWrappedFlashLender_MAINNET: '0x9E092cb431e5F1aa70e47e052773711d2Ba4917E'.toLowerCase(),
-  UniV3WrappedFlashLender_MAINNET: '0x319300462C37AD2D4f26B584C2b67De51F51f289'.toLowerCase(),
-  AaveWrappedFlashLender_ARBITRUM: '0x9D4D2C08b29A2Db1c614483cd8971734BFDCC9F2'.toLowerCase(),
-  BalancerWrappedFlashLender_ARBITRUM: '0x9E092cb431e5F1aa70e47e052773711d2Ba4917E'.toLowerCase(),
-  CamelotWrappedFlashLender_ARBITRUM: '0x5E8820B2832aD8451f65Fa2CCe2F3Cef29016D0d'.toLowerCase(),
-  UniV3WrappedFlashLender_ARBITRUM: '0x319300462C37AD2D4f26B584C2b67De51F51f289'.toLowerCase(),
-}
-
-enum Sign {
-  flash = '0xaf2511c0',
-  flashLoan = '0xab9c4b5d',
-  flashLiquidate = '0x7bbeeafd',
-  claimVaultRewardTokens = '0x36e8053c',
-  reinvestVaultReward = '0xe800d559',
-  investWETHAndNOTE = '0x162c2a8e',
-  executeTrade = '0x8df6c3ce',
-  aggregate3 = '0x82ad56cb',
-  harvestAssetsFromNotional = '0x295732fb',
-  initializeAllMarkets = '0x9f8fad32',
-  settleAccounts = '0xd7f4893e',
-  settleVaultsAccounts = '0x8261f4ba',
-}
-
-const whitelist: Record<Network, Partial<Record<string, Sign[]>>> = {
-  mainnet: {
-    [Address.AaveFlashLiquidator_MAINNET]: [Sign.flashLiquidate],
-    [Address.TREASURY_MANAGER]: [Sign.claimVaultRewardTokens, Sign.reinvestVaultReward, Sign.investWETHAndNOTE, Sign.executeTrade],
-    [Address.AaveV3Pool_MAINNET]: [Sign.flashLoan],
-    [Address.Multicall3]: [Sign.aggregate3],
-    [Address.AaveWrappedFlashLender_MAINNET]: [Sign.flash],
-    [Address.BalancerWrappedFlashLender_MAINNET]: [Sign.flash],
-    [Address.UniV3WrappedFlashLender_MAINNET]: [Sign.flash],
-  },
-  arbitrum: {
-    [Address.AaveFlashLiquidator_ARBITRUM]: [Sign.flashLiquidate],
-    [Address.TREASURY_MANAGER]: [Sign.claimVaultRewardTokens, Sign.reinvestVaultReward, Sign.harvestAssetsFromNotional, Sign.executeTrade],
-    [Address.AaveV3Pool_ARBITRUM]: [Sign.flashLoan],
-    [Address.Multicall3]: [Sign.aggregate3],
-    [Address.initializeAllMarkets_ARBITRUM]: [Sign.initializeAllMarkets],
-    [Address.settleAccounts_ARBITRUM]: [Sign.settleAccounts, Sign.settleVaultsAccounts],
-    [Address.AaveWrappedFlashLender_ARBITRUM]: [Sign.flash],
-    [Address.BalancerWrappedFlashLender_ARBITRUM]: [Sign.flash],
-    [Address.UniV3WrappedFlashLender_ARBITRUM]: [Sign.flash],
-    [Address.CamelotWrappedFlashLender_ARBITRUM]: [Sign.flash],
-  },
-  sepolia: {
-    [Address.AaveFlashLiquidator_ARBITRUM]: [Sign.flashLiquidate],
-    [Address.TREASURY_MANAGER]: [Sign.claimVaultRewardTokens, Sign.reinvestVaultReward],
-    [Address.AaveV3Pool_ARBITRUM]: [Sign.flashLoan],
-    [Address.Multicall3]: [Sign.aggregate3],
-    [Address.initializeAllMarkets_ARBITRUM]: [Sign.initializeAllMarkets],
-    [Address.settleAccounts_ARBITRUM]: [Sign.settleAccounts, Sign.settleVaultsAccounts],
-    [Address.AaveWrappedFlashLender_ARBITRUM]: [Sign.flash],
-    [Address.BalancerWrappedFlashLender_ARBITRUM]: [Sign.flash],
-    [Address.UniV3WrappedFlashLender_ARBITRUM]: [Sign.flash],
-    [Address.CamelotWrappedFlashLender_ARBITRUM]: [Sign.flash],
-  }
+  liquidation = 'liquidation-2',
 }
 
 const keysToUse = {
   [Address.TREASURY_MANAGER]: Key.reinvestment,
+  [Address.REBALANCE_HELPER]: Key.reinvestment,
   [Address.AaveV3Pool_ARBITRUM]: Key.liquidation,
   [Address.AaveV3Pool_MAINNET]: Key.liquidation,
   [Address.AaveFlashLiquidator_ARBITRUM]: Key.liquidation,
@@ -112,11 +42,78 @@ const keysToUse = {
   [Address.CamelotWrappedFlashLender_ARBITRUM]: Key.liquidation,
 };
 
+const whitelist: Record<Network, Partial<Record<string, Sign[]>>> = {
+  mainnet: {
+    [Address.AaveFlashLiquidator_MAINNET]: [Sign.flashLiquidate],
+    [Address.TREASURY_MANAGER]: [
+      Sign.claimVaultRewardTokens,
+      Sign.reinvestVaultReward,
+      Sign.investWETHAndNOTE,
+      Sign.executeTrade,
+    ],
+    [Address.AaveV3Pool_MAINNET]: [Sign.flashLoan],
+    [Address.Multicall3]: [Sign.aggregate3],
+    [Address.AaveWrappedFlashLender_MAINNET]: [Sign.flash],
+    [Address.BalancerWrappedFlashLender_MAINNET]: [Sign.flash],
+    [Address.UniV3WrappedFlashLender_MAINNET]: [Sign.flash],
+  },
+  arbitrum: {
+    [Address.AaveFlashLiquidator_ARBITRUM]: [Sign.flashLiquidate],
+    [Address.TREASURY_MANAGER]: [
+      Sign.claimVaultRewardTokens,
+      Sign.reinvestVaultReward,
+      Sign.harvestAssetsFromNotional,
+      Sign.executeTrade,
+    ],
+    [Address.AaveV3Pool_ARBITRUM]: [Sign.flashLoan],
+    [Address.Multicall3]: [Sign.aggregate3],
+    [Address.initializeAllMarkets_ARBITRUM]: [Sign.initializeAllMarkets],
+    [Address.settleAccounts_ARBITRUM]: [
+      Sign.settleAccounts,
+      Sign.settleVaultsAccounts,
+    ],
+    [Address.AaveWrappedFlashLender_ARBITRUM]: [Sign.flash],
+    [Address.BalancerWrappedFlashLender_ARBITRUM]: [Sign.flash],
+    [Address.UniV3WrappedFlashLender_ARBITRUM]: [Sign.flash],
+    [Address.CamelotWrappedFlashLender_ARBITRUM]: [Sign.flash],
+  },
+  sepolia: {
+    [Address.AaveFlashLiquidator_ARBITRUM]: [Sign.flashLiquidate],
+    [Address.TREASURY_MANAGER]: [
+      Sign.claimVaultRewardTokens,
+      Sign.reinvestVaultReward,
+    ],
+    [Address.AaveV3Pool_ARBITRUM]: [Sign.flashLoan],
+    [Address.Multicall3]: [Sign.aggregate3],
+    [Address.initializeAllMarkets_ARBITRUM]: [Sign.initializeAllMarkets],
+    [Address.settleAccounts_ARBITRUM]: [
+      Sign.settleAccounts,
+      Sign.settleVaultsAccounts,
+    ],
+    [Address.AaveWrappedFlashLender_ARBITRUM]: [Sign.flash],
+    [Address.BalancerWrappedFlashLender_ARBITRUM]: [Sign.flash],
+    [Address.UniV3WrappedFlashLender_ARBITRUM]: [Sign.flash],
+    [Address.CamelotWrappedFlashLender_ARBITRUM]: [Sign.flash],
+    [Address.REBALANCE_HELPER]: [Sign.checkAndRebalance],
+  },
+};
+
 function getTxType({ signature }: { signature: Sign }) {
-  if ([Sign.flash, Sign.flashLoan, Sign.flashLiquidate, Sign.settleVaultsAccounts, Sign.settleAccounts, Sign.initializeAllMarkets].includes(signature)) {
+  if (
+    [
+      Sign.flash,
+      Sign.flashLoan,
+      Sign.flashLiquidate,
+      Sign.settleVaultsAccounts,
+      Sign.settleAccounts,
+      Sign.initializeAllMarkets,
+    ].includes(signature)
+  ) {
     return 'liquidation';
   }
-  if ([Sign.claimVaultRewardTokens, Sign.reinvestVaultReward].includes(signature)) {
+  if (
+    [Sign.claimVaultRewardTokens, Sign.reinvestVaultReward].includes(signature)
+  ) {
     return 'reinvestment';
   }
   if ([Sign.executeTrade, Sign.investWETHAndNOTE].includes(signature)) {
@@ -125,30 +122,22 @@ function getTxType({ signature }: { signature: Sign }) {
   if ([Sign.harvestAssetsFromNotional].includes(signature)) {
     return 'harvesting';
   }
-  if ([Sign.settleVaultsAccounts, Sign.settleAccounts, Sign.initializeAllMarkets].includes(signature)) {
+  if (
+    [
+      Sign.settleVaultsAccounts,
+      Sign.settleAccounts,
+      Sign.initializeAllMarkets,
+    ].includes(signature)
+  ) {
     return 'initialize-markets';
+  }
+  if ([Sign.checkAndRebalance].includes(signature)) {
+    return 'rebalance';
   }
   return 'unknown';
 }
 
-async function logToDataDog(message: any, ddtags = '') {
-  return fetch("https://http-intake.logs.datadoghq.com/api/v2/logs", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      'DD-API-KEY': process.env.DD_API_KEY as string
-    },
-    body: JSON.stringify({
-      ddsource: "tx-relay",
-      ddtags,
-      service: "signer",
-      message
-    })
-  }).catch(err => console.error(err));
-}
-
-const wait = (ms: number) => new Promise(r => setTimeout(r, ms));
+const wait = (ms: number) => new Promise((r) => setTimeout(r, ms));
 // will only work properly if node app is not run in cluster
 const throttle = (() => {
   const delay = 2000;
@@ -162,23 +151,42 @@ const throttle = (() => {
   };
 })();
 
-export async function sendTransaction({ to, data, gasLimit, nonce, network }: { to: string, data: string, nonce: number, gasLimit: number, network: string }) {
-  to = to.toLowerCase();
+interface TransactionParams {
+  to: string;
+  data: string;
+  network: string;
+  nonce?: number;
+  gasLimit?: number;
+}
+
+interface ExecutionContext {
+  provider?: providers.Provider;
+  log: ReturnType<typeof logToDataDog>;
+}
+
+export async function sendTransaction(
+  params: TransactionParams,
+  context: ExecutionContext
+) {
+  const { data, gasLimit, nonce, network } = params;
+  const { log } = context;
+  const to = params.to.toLowerCase();
   const signature = data.slice(0, 10) as Sign;
 
-  let sharedLogData: any = {
+  let sharedLogData = {
     txType: getTxType({ signature: signature }),
     signature,
     network,
     to,
+    from: '',
   };
 
-  if (
-    !whitelist[network][to] ||
-    !whitelist[network][to].includes(signature)
-  ) {
+  if (!whitelist[network][to] || !whitelist[network][to].includes(signature)) {
     const errorMessage = `Call to: ${to} method: ${signature} on network: ${network} is not allowed`;
-    await logToDataDog({ ...sharedLogData, err: errorMessage, status: 'error', }, 'event:txn_failed');
+    await log(
+      { ...sharedLogData, err: errorMessage, status: 'error' },
+      { tags: 'event:txn_failed' }
+    );
 
     throw new Error(errorMessage);
   }
@@ -186,9 +194,13 @@ export async function sendTransaction({ to, data, gasLimit, nonce, network }: { 
   // in order to properly fetch correct nonce for tx we need to throttle here per key
   await throttle(keysToUse[to]);
 
-  const provider = ethers.getDefaultProvider(rpcUrls[network]);
+  const provider =
+    context.provider || ethers.getDefaultProvider(rpcUrls[network]);
 
-  const signer = new GcpKmsSigner({ ...kmsCredentials, keyId: keysToUse[to] }).connect(provider);
+  const signer = new GcpKmsSigner({
+    ...kmsCredentials,
+    keyId: keysToUse[to],
+  }).connect(provider);
 
   const transaction: TransactionRequest = {
     to,
@@ -214,25 +226,29 @@ export async function sendTransaction({ to, data, gasLimit, nonce, network }: { 
       await wait(1000);
       txResponse = await signer.sendTransaction(transaction);
     } catch (err) {
-      await logToDataDog({
-        ...sharedLogData,
-        retry,
-        err: JSON.stringify(err),
-        status: 'error',
-      }, 'event:txn_failed');
+      await log({
+        message: {
+          ...sharedLogData,
+          retry,
+          err: JSON.stringify(err),
+          status: 'error',
+        },
+        tags: 'event:txn_failed',
+      });
 
       throw err;
     }
-
   }
 
-  await logToDataDog({
-    ...sharedLogData,
-    hash: txResponse.hash,
-    gasLimit: txResponse.gasLimit?.toString(),
-    gasPrice: txResponse.gasPrice?.toString(),
-    status: 'success',
-    retry,
+  await log({
+    message: {
+      ...sharedLogData,
+      hash: txResponse.hash,
+      gasLimit: txResponse.gasLimit?.toString(),
+      gasPrice: txResponse.gasPrice?.toString(),
+      status: 'success',
+      retry,
+    },
   });
 
   return txResponse;
@@ -240,8 +256,13 @@ export async function sendTransaction({ to, data, gasLimit, nonce, network }: { 
 
 export async function getAllSignerAddresses() {
   return {
-    [Key.reinvestment]: await (new GcpKmsSigner({ ...kmsCredentials, keyId: Key.reinvestment })).getAddress(),
-    [Key.liquidation]: await (new GcpKmsSigner({ ...kmsCredentials, keyId: Key.liquidation })).getAddress(),
-  }
+    [Key.reinvestment]: await new GcpKmsSigner({
+      ...kmsCredentials,
+      keyId: Key.reinvestment,
+    }).getAddress(),
+    [Key.liquidation]: await new GcpKmsSigner({
+      ...kmsCredentials,
+      keyId: Key.liquidation,
+    }).getAddress(),
+  };
 }
-
