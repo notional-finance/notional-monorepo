8b9e58035c38292ec988a1b80517f6f26ed2b6e1
Sime <sbuljac@gmail.com>
2024-09-16 20:14:08 +0200

    Feat/integrate tenderly nx (#1112)
    * feat: move tenderly into apps folder
    
    * feat: add linting and formating to tenderly actions
    
    * feat: add github action workflow for Tenderly actions
    
    * fix: update package.json
    
    * feat: simplify github workflow
    
    * feat: remove tenderly workflow and move deployment command to workers workflow
    
    * fix: properly name all steps

--- apps/tenderly/actions/src/index.ts
+++ apps/tenderly/actions/src/index.ts
@@ -1,77 +1,91 @@
 import { ActionFn, Context, Event, TransactionEvent } from '@tenderly/actions';
 import { ethers } from 'ethers';
 import NotionalV3ABI from '@notional-finance/contracts/src/abi/NotionalV3.json';
-import { TransferSingleEventObject, TransferBatchEventObject, AccountContextUpdateEventObject, }
-  from '@notional-finance/contracts/src/types/NotionalV3';
+import {
+  TransferSingleEventObject,
+  TransferBatchEventObject,
+  AccountContextUpdateEventObject,
+} from '@notional-finance/contracts/src/types/NotionalV3';
 import fetch from 'node-fetch';
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-const url = 'https://us-central1-monitoring-agents.cloudfunctions.net/data-service/events';
+const url =
+  'https://us-central1-monitoring-agents.cloudfunctions.net/data-service/events';
 
 type DataServiceAccountContextUpdate = {
-  name: 'AccountContextUpdate',
+  name: 'AccountContextUpdate';
   params: {
-    account: string,
-  }
+    account: string;
+  };
 };
 
 type DataServiceTransferBatch = {
-  name: 'TransferBatch',
+  name: 'TransferBatch';
   params: {
-    operator: string,
-    from: string,
-    to: string,
-    ids: string[],
-    values: string[],
-  }
+    operator: string;
+    from: string;
+    to: string;
+    ids: string[];
+    values: string[];
+  };
 };
 type DataServiceTransferSingle = {
-  name: 'TransferSingle',
+  name: 'TransferSingle';
   params: {
-    operator: string,
-    from: string,
-    to: string,
-    id: string,
-    value: string,
-  }
+    operator: string;
+    from: string;
+    to: string;
+    id: string;
+    value: string;
+  };
 };
 
-type DataServiceEvent = DataServiceTransferSingle | DataServiceTransferBatch | DataServiceAccountContextUpdate;
+type DataServiceEvent =
+  | DataServiceTransferSingle
+  | DataServiceTransferBatch
+  | DataServiceAccountContextUpdate;
 
 const parseEventsOfInterest = async (txEvent: TransactionEvent) => {
   const events: DataServiceEvent[] = [];
   const INotional = new ethers.utils.Interface(NotionalV3ABI);
 
-  const accountContextUpdateTopic = INotional.getEventTopic('AccountContextUpdate');
-  const accountContextUpdateLog = txEvent.logs.find(log => {
-    return log.topics.find(topic => topic == accountContextUpdateTopic) !== undefined
+  const accountContextUpdateTopic = INotional.getEventTopic(
+    'AccountContextUpdate'
+  );
+  const accountContextUpdateLog = txEvent.logs.find((log) => {
+    return (
+      log.topics.find((topic) => topic == accountContextUpdateTopic) !==
+      undefined
+    );
   });
 
   if (accountContextUpdateLog) {
     const accountContextInputs = INotional.decodeEventLog(
       'AccountContextUpdate',
       accountContextUpdateLog.data,
-      accountContextUpdateLog.topics,
+      accountContextUpdateLog.topics
     ) as unknown as AccountContextUpdateEventObject;
 
     events.push({
       name: 'AccountContextUpdate',
       params: {
-        account: accountContextInputs.account
-      }
-    })
+        account: accountContextInputs.account,
+      },
+    });
   }
 
   const transferSingleTopic = INotional.getEventTopic('TransferSingle');
-  const transferSingleLog = txEvent.logs.find(log => {
-    return log.topics.find(topic => topic == transferSingleTopic) !== undefined
+  const transferSingleLog = txEvent.logs.find((log) => {
+    return (
+      log.topics.find((topic) => topic == transferSingleTopic) !== undefined
+    );
   });
 
   if (transferSingleLog) {
     const transferSingleInputs = INotional.decodeEventLog(
       'TransferSingle',
       transferSingleLog.data,
-      transferSingleLog.topics,
+      transferSingleLog.topics
     ) as unknown as TransferSingleEventObject;
     if (transferSingleInputs.to !== ZERO_ADDRESS) {
       events.push({
@@ -81,23 +95,24 @@ const parseEventsOfInterest = async (txEvent: TransactionEvent) => {
           from: transferSingleInputs.from,
           to: transferSingleInputs.to,
           id: transferSingleInputs.id.toString(),
-          value: transferSingleInputs.value.toString()
-        }
-      })
+          value: transferSingleInputs.value.toString(),
+        },
+      });
     }
   }
 
   const transferBatchTopic = INotional.getEventTopic('TransferBatch');
-  const transferBatchLog = txEvent.logs.find(log => {
-    return log.topics.find(topic => topic == transferBatchTopic) !== undefined
+  const transferBatchLog = txEvent.logs.find((log) => {
+    return (
+      log.topics.find((topic) => topic == transferBatchTopic) !== undefined
+    );
   });
-
 
   if (transferBatchLog) {
     const transferBatchInputs = INotional.decodeEventLog(
       'TransferBatch',
       transferBatchLog.data,
-      transferBatchLog.topics,
+      transferBatchLog.topics
     ) as unknown as TransferBatchEventObject;
     if (transferBatchInputs.to !== ZERO_ADDRESS) {
       events.push({
@@ -110,21 +125,21 @@ const parseEventsOfInterest = async (txEvent: TransactionEvent) => {
           // strange issue with returned event object, if "values"
           // is accessed as property, function is returned instead of array
           // so we need to access it here as array element
+          // eslint-disable-next-line
           // @ts-ignore
-          values: transferBatchInputs[4].map(String)
-        }
-      })
+          values: transferBatchInputs[4].map(String),
+        },
+      });
     }
   }
 
   return events;
 };
 
-export const accountsFn: ActionFn = async (
-  context: Context,
-  event: Event
-) => {
-  const dataServiceAuthToken = await context.secrets.get('DATA_SERVICE_AUTH_TOKEN');
+export const accountsFn: ActionFn = async (context: Context, event: Event) => {
+  const dataServiceAuthToken = await context.secrets.get(
+    'DATA_SERVICE_AUTH_TOKEN'
+  );
   const txEvent = event as TransactionEvent;
   console.log('Processing events');
   const events = await parseEventsOfInterest(txEvent);
@@ -135,12 +150,12 @@ export const accountsFn: ActionFn = async (
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-auth-token': dataServiceAuthToken
+        'x-auth-token': dataServiceAuthToken,
       },
       body: JSON.stringify({
         network: context.metadata.getNetwork(),
-        events
-      })
+        events,
+      }),
     });
   }
 };
