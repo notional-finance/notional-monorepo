6ef1b296f7b298ac51f8dd429fdcb8c694db7c57
Sime <sbuljac@gmail.com>
2024-10-02 08:23:25 +0200

    refactor: move token addresses to util folder

--- apps/rewards/src/vaults.ts
+++ apps/rewards/src/vaults.ts
@@ -1,51 +1,53 @@
-import { Network } from '@notional-finance/util';
+import {
+  Network,
+  tokens,
+  vaults as vaultAddresses,
+} from '@notional-finance/util';
 
 // all addresses must be properly checksummed
 // mainnet
-const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-const rETH = '0xae78736Cd615f374D3085123A210448E74Fc6393';
-const PYUSD = '0x6c3ea9036406852006290770BEdFcAbA0e23A0e8';
-const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
-const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
-const crvUSD = '0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E';
-const CRV = '0xD533a949740bb3306d119CC777fa900bA034cd52';
-const CVX = '0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B';
-const BAL = '0xba100000625a3754423978a60c9317c58a424e3D';
-const AURA = '0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF';
-const GHO = '0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f';
+const rETH = tokens.mainnet.rETH;
+const PYUSD = tokens.mainnet.PYUSD;
+const USDC = tokens.mainnet.USDC;
+const USDT = tokens.mainnet.USDT;
+const crvUSD = tokens.mainnet.crvUSD;
+const CRV = tokens.mainnet.CRV;
+const CVX = tokens.mainnet.CVX;
+const BAL = tokens.mainnet.BAL;
+const AURA = tokens.mainnet.AURA;
+const GHO = tokens.mainnet.GHO;
 // arbitrum
-const ARB_WETH = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1';
-const ARB_BAL = '0x040d1EdC9569d4Bab2D15287Dc5A4F10F56a56B8';
-const ARB_AURA = '0x1509706a6c66CA549ff0cB464de88231DDBe213B';
-const ARB_CRV = '0x11cDb42B0EB46D95f990BeDD4695A6e3fA034978';
-const ARB = '0x912CE59144191C1204E64559FE8253a0e49E6548';
-const ARB_USDCe = '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8';
-const ARB_USDC = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831';
-const ARB_USDT = '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9';
-const ARB_wstETH = '0x5979D7b546E38E414F7E9822514be443A4800529';
-const ARB_WBTC = '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f';
+const ARB_BAL = tokens.arbitrum.BAL;
+const ARB_AURA = tokens.arbitrum.AURA;
+const ARB_CRV = tokens.arbitrum.CRV;
+const ARB = tokens.arbitrum.ARB;
+const ARB_USDCe = tokens.arbitrum.USDCe;
+const ARB_USDC = tokens.arbitrum.USDC;
+const ARB_USDT = tokens.arbitrum.USDT;
+const ARB_wstETH = tokens.arbitrum.wstETH;
+const ARB_WBTC = tokens.arbitrum.WBTC;
 
 export const ETH = '0x0000000000000000000000000000000000000000';
 
 export const wEthMapper: Partial<Record<Network, string>> = {
-  mainnet: WETH,
-  arbitrum: ARB_WETH,
+  mainnet: tokens.mainnet.WETH,
+  arbitrum: tokens.arbitrum.WETH,
 };
 
 // set minimum values for tokens that are going to be claim/sell (~$15 for arbitrum, ~$150 for mainnet)
 // if token is also pool token it won't be sold
 export const minTokenAmount = {
-  [USDC]: '150000000', // 150e6
-  [PYUSD]: '150000000', // 150e6
-  [GHO]: '150000000000000000000', // 150e18
-  [CRV]: '230000000000000000000', // 230e18
-  [CVX]: '40000000000000000000', // 40e18
-  [BAL]: '30000000000000000000', // 30e18
-  [AURA]: '200000000000000000000', // 200e18
-  [ARB]: '7000000000000000000', // 7e18
-  [ARB_BAL]: '3500000000000000000', // 3.5e18
-  [ARB_AURA]: '21000000000000000000', // 21e18
-  [ARB_CRV]: '30000000000000000000', // 10e18
+  [USDC]: Number(150e6).toString(),
+  [PYUSD]: Number(150e6).toString(),
+  [GHO]: Number(150e18).toString(),
+  [CRV]: Number(230e18).toString(),
+  [CVX]: Number(40e18).toString(),
+  [BAL]: Number(30e18).toString(),
+  [AURA]: Number(200e18).toString(),
+  [ARB]: Number(7e18).toString(),
+  [ARB_BAL]: Number(3.5e18).toString(),
+  [ARB_AURA]: Number(21e18).toString(),
+  [ARB_CRV]: Number(10e18).toString(),
 };
 
 export type Vault = {
@@ -60,84 +62,84 @@ export type Vault = {
 export const vaults: Partial<Record<Network, Array<Vault>>> = {
   mainnet: [
     {
-      address: '0x84e58d8faA4e3B74d55D9fc762230f15d95570B8',
+      address: vaultAddresses.mainnet.Convex_pyUSD_xUSDC,
       rewardTokens: [CRV, CVX, PYUSD],
       reinvestToken: USDC,
     },
     {
-      address: '0xba4eb30f7F2e378249cf94E08F581e704326e9c6',
+      address: vaultAddresses.mainnet.Convex_xUSDC_crvUSD,
       rewardTokens: [CRV, CVX],
       reinvestToken: USDC,
     },
     {
-      address: '0x86B222d44AC6cC56e75b3df01fdAD5Dc371EF538',
+      address: vaultAddresses.mainnet.Convex_xUSDT_crvUSD,
       rewardTokens: [CRV, CVX],
       reinvestToken: USDT,
     },
     {
-      address: '0xeEB885Af7C8075Aa3b93e2F95E1c0bD51c758F91',
+      address: vaultAddresses.mainnet.Aura_GHO_USDT_xUSDC,
       rewardTokens: [BAL, AURA, GHO],
       reinvestToken: USDC,
     },
     {
-      address: '0x914255c0c289AEa36E378EBB5e28293b5ED278Ca',
+      address: vaultAddresses.mainnet.Aura_ezETH_xWETH,
       rewardTokens: [BAL, AURA],
       reinvestToken: ETH,
     },
     {
-      address: '0x32D82A1C8618c7Be7Fe85B2F1C44357A871d52D1',
+      address: vaultAddresses.mainnet.Aura_xrETH_weETH,
       rewardTokens: [BAL, AURA],
       reinvestToken: rETH,
     },
     {
-      address: '0x30fBa4a7ec8591f25B4D37fD79943a4bb6E553e2',
+      address: vaultAddresses.mainnet.Convex_xGHO_crvUSD,
       rewardTokens: [CRV, CVX],
       reinvestToken: crvUSD,
     },
     {
-      address: '0xF94507F3dECE4CC4c73B6cf228912b85Eadc9CFB',
+      address: vaultAddresses.mainnet.Balancer_rsETH_xWETH,
       rewardTokens: [USDC],
       reinvestToken: ETH,
     },
   ],
   arbitrum: [
     {
-      address: '0xdb08f663e5D765949054785F2eD1b2aa1e9C22Cf',
+      address: vaultAddresses.arbitrum.Curve_xFRAX_USDC,
       rewardTokens: [ARB_CRV, ARB],
       reinvestToken: ARB_USDCe,
     },
     {
-      address: '0x431dbfE3050eA39abBfF3E0d86109FB5BafA28fD',
+      address: vaultAddresses.arbitrum.Convex_USDCe_xUSDT,
       rewardTokens: [ARB_CRV, ARB],
       reinvestToken: ARB_USDT,
     },
     {
-      address: '0x5c36a0DeaB3531d29d848E684E8bDf5F81cDB643',
+      address: vaultAddresses.arbitrum.Convex_crvUSD_xUSDC,
       rewardTokens: [ARB_CRV, ARB],
       reinvestToken: ARB_USDC,
     },
     {
-      address: '0xae04e4887cBf5f25c05aC1384BcD0b7e885a1F4A',
+      address: vaultAddresses.arbitrum.Convex_crvUSD_xUSDT,
       rewardTokens: [ARB_CRV, ARB],
       reinvestToken: ARB_USDT,
     },
     {
-      address: '0xF95441f348eb2fd3D5D82f9B7B961137a734eEdD',
+      address: vaultAddresses.arbitrum.Convex_xWBTC_tBTC,
       rewardTokens: [ARB_CRV, ARB],
       reinvestToken: ARB_WBTC,
     },
     {
-      address: '0x3Df035433cFACE65b6D68b77CC916085d020C8B8',
+      address: vaultAddresses.arbitrum.Aura_xrETH_WETH,
       rewardTokens: [ARB, ARB_BAL, ARB_AURA],
       reinvestToken: ETH,
     },
     {
-      address: '0x8Ae7A8789A81A43566d0ee70264252c0DB826940',
+      address: vaultAddresses.arbitrum.Aura_xUSDC_DAI_USDT_USDCe,
       rewardTokens: [ARB, ARB_BAL, ARB_AURA],
       reinvestToken: ARB_USDC,
     },
     {
-      address: '0x0E8C1A069f40D0E8Fa861239D3e62003cBF3dCB2',
+      address: vaultAddresses.arbitrum.Aura_wstETH_xWETH,
       rewardTokens: [ARB, ARB_BAL, ARB_AURA],
       reinvestToken: ETH,
       maxSellAmount: {
@@ -145,32 +147,32 @@ export const vaults: Partial<Record<Network, Array<Vault>>> = {
       },
     },
     {
-      address: '0x37dD23Ab1885982F789A2D6400B583B8aE09223d',
+      address: vaultAddresses.arbitrum.Aura_xwstETH_cbETH_rETH,
       rewardTokens: [ARB, ARB_BAL, ARB_AURA],
       reinvestToken: ARB_wstETH,
     },
     {
-      address: '0xD7c3Dc1C36d19cF4e8cea4eA143a2f4458Dd1937',
+      address: vaultAddresses.arbitrum.Aura_ezETH_xwstETH,
       rewardTokens: [ARB, ARB_BAL, ARB_AURA],
       reinvestToken: ARB_wstETH,
     },
     {
-      address: '0xA0d61c08e642103158Fc6a1495E7Ff82bAF25857',
+      address: vaultAddresses.arbitrum.Aura_rETH_xWETH,
       rewardTokens: [ARB, ARB_BAL, ARB_AURA],
       reinvestToken: ETH,
     },
     {
-      address: '0xCAc9c01d1207e5D06bB0fD5b854832F35FE97E68',
+      address: vaultAddresses.arbitrum.Aura_rsETH_xWETH,
       rewardTokens: [ARB, ARB_BAL, ARB_AURA],
       reinvestToken: ETH,
     },
     {
-      address: '0x91B79F4081D3522Af2760B7698810d501eBC8010',
+      address: vaultAddresses.arbitrum.Aura_cbETH_xwstETH_rETH,
       rewardTokens: [ARB, ARB_BAL, ARB_AURA],
       reinvestToken: ARB_wstETH,
     },
     {
-      address: '0x3533F05B2C54Ce1C2321cfe3c6F693A3cBbAEa10',
+      address: vaultAddresses.arbitrum.Convex_tBTC_xWBTC,
       rewardTokens: [ARB_CRV, ARB],
       reinvestToken: ARB_WBTC,
     },
--- packages/util/src/constants.ts
+++ packages/util/src/constants.ts
@@ -61,6 +61,9 @@ export const STABLE_COINS = ['USDC', 'USDT', 'DAI', 'FRAX', 'sDAI'];
 export const LSDS = ['wstETH', 'cbETH', 'rETH'];
 export const NATIVE_YIELD = [...LSDS, 'sDAI'];
 
+export const DATA_SERVICE_URL =
+  'https://us-central1-monitoring-agents.cloudfunctions.net/data-service/vaultApy';
+
 export const AlchemyUrl: Record<Network, string> = {
   [Network.all]: 'https://eth-mainnet.g.alchemy.com/v2',
   [Network.mainnet]: 'https://eth-mainnet.g.alchemy.com/v2',
@@ -82,6 +85,21 @@ export const NotionalAddress: Record<Network, string> = {
   [Network.optimism]: '',
 };
 
+export const treasuryManagerAddresses = {
+  [Network.mainnet]: '0x53144559C0d4a3304e2DD9dAfBD685247429216d',
+  [Network.arbitrum]: '0x53144559C0d4a3304e2DD9dAfBD685247429216d',
+};
+
+export const managerBotAddresses = {
+  [Network.mainnet]: '0x745915418D8B70f39ce9e61A965cBB0C87f9f7Ed',
+  [Network.arbitrum]: '0x745915418D8B70f39ce9e61A965cBB0C87f9f7Ed',
+};
+
+export const tradingModuleAddresses = {
+  [Network.mainnet]: '0x594734c7e06C3D483466ADBCe401C6Bd269746C8',
+  [Network.arbitrum]: '0xBf6B9c5608D520469d8c4BD1E24F850497AF0Bb8',
+};
+
 export const SubgraphId: Record<Network, string> = {
   [Network.all]: '4oVxkMtN4cFepbiYrSKz1u6HWnJym435k5DQRAFt2vHW',
   [Network.mainnet]: '4oVxkMtN4cFepbiYrSKz1u6HWnJym435k5DQRAFt2vHW',
@@ -92,13 +110,6 @@ export const SubgraphId: Record<Network, string> = {
 export function getSubgraphEndpoint(network: Network, subgraphKey: string) {
   return `https://gateway-arbitrum.network.thegraph.com/api/${subgraphKey}/subgraphs/id/${SubgraphId[network]}`;
 }
-
-export const WETHAddress: Record<Network, string> = {
-  [Network.all]: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-  [Network.mainnet]: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-  [Network.arbitrum]: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
-  [Network.optimism]: '',
-};
 
 export const sNOTE = '0x38de42f4ba8a35056b33a746a6b45be9b1c3b9d2';
 
@@ -141,3 +152,73 @@ export const ACCOUNT_ID_RANGES = [
   '0xf000000000000000000000000000000000000000',
   '0xffffffffffffffffffffffffffffffffffffffff',
 ];
+
+// all addresses must be properly checksummed
+export const tokens = {
+  [Network.mainnet]: {
+    WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    GHO: '0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f',
+    rETH: '0xae78736Cd615f374D3085123A210448E74Fc6393',
+    PYUSD: '0x6c3ea9036406852006290770BEdFcAbA0e23A0e8',
+    USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    crvUSD: '0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E',
+    CRV: '0xD533a949740bb3306d119CC777fa900bA034cd52',
+    CVX: '0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B',
+    BAL: '0xba100000625a3754423978a60c9317c58a424e3D',
+    AURA: '0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF',
+    WBTC: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+    GHO: '0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f',
+  },
+  [Network.arbitrum]: {
+    WETH: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    BAL: '0x040d1EdC9569d4Bab2D15287Dc5A4F10F56a56B8',
+    AURA: '0x1509706a6c66CA549ff0cB464de88231DDBe213B',
+    CRV: '0x11cDb42B0EB46D95f990BeDD4695A6e3fA034978',
+    ARB: '0x912CE59144191C1204E64559FE8253a0e49E6548',
+    USDCe: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
+    USDC: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+    USDT: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+    wstETH: '0x5979D7b546E38E414F7E9822514be443A4800529',
+    WBTC: '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f',
+    FRAX: '0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F',
+  },
+};
+
+export const vaults = {
+  [Network.mainnet]: {
+    Convex_pyUSD_xUSDC: '0x84e58d8faA4e3B74d55D9fc762230f15d95570B8',
+    Convex_xUSDC_crvUSD: '0xba4eb30f7F2e378249cf94E08F581e704326e9c6',
+    Convex_xUSDT_crvUSD: '0x86B222d44AC6cC56e75b3df01fdAD5Dc371EF538',
+    Aura_GHO_USDT_xUSDC: '0xeEB885Af7C8075Aa3b93e2F95E1c0bD51c758F91',
+    Aura_ezETH_xWETH: '0x914255c0c289AEa36E378EBB5e28293b5ED278Ca',
+    Aura_xrETH_weETH: '0x32D82A1C8618c7Be7Fe85B2F1C44357A871d52D1',
+    Convex_xGHO_crvUSD: '0x30fBa4a7ec8591f25B4D37fD79943a4bb6E553e2',
+    Balancer_rsETH_xWETH: '0xF94507F3dECE4CC4c73B6cf228912b85Eadc9CFB',
+    Curve_USDe_xUSDC: '0xd6aa58cf21a0edb33375d6c0434b8bb5b589f021',
+    Convex_xGHO_USDe: '0xb1113cf888a019693b254da3d90f841072d85172',
+  },
+  [Network.arbitrum]: {
+    Curve_xFRAX_USDC: '0xdb08f663e5D765949054785F2eD1b2aa1e9C22Cf',
+    Convex_USDCe_xUSDT: '0x431dbfE3050eA39abBfF3E0d86109FB5BafA28fD',
+    Convex_crvUSD_xUSDC: '0x5c36a0DeaB3531d29d848E684E8bDf5F81cDB643',
+    Convex_crvUSD_xUSDT: '0xae04e4887cBf5f25c05aC1384BcD0b7e885a1F4A',
+    Convex_xWBTC_tBTC: '0xF95441f348eb2fd3D5D82f9B7B961137a734eEdD',
+    Aura_xrETH_WETH: '0x3Df035433cFACE65b6D68b77CC916085d020C8B8',
+    Aura_xUSDC_DAI_USDT_USDCe: '0x8Ae7A8789A81A43566d0ee70264252c0DB826940',
+    Aura_wstETH_xWETH: '0x0E8C1A069f40D0E8Fa861239D3e62003cBF3dCB2',
+    Aura_xwstETH_cbETH_rETH: '0x37dD23Ab1885982F789A2D6400B583B8aE09223d',
+    Aura_ezETH_xwstETH: '0xD7c3Dc1C36d19cF4e8cea4eA143a2f4458Dd1937',
+    Aura_rETH_xWETH: '0xA0d61c08e642103158Fc6a1495E7Ff82bAF25857',
+    Aura_rsETH_xWETH: '0xCAc9c01d1207e5D06bB0fD5b854832F35FE97E68',
+    Aura_cbETH_xwstETH_rETH: '0x91B79F4081D3522Af2760B7698810d501eBC8010',
+    Convex_tBTC_xWBTC: '0x3533F05B2C54Ce1C2321cfe3c6F693A3cBbAEa10',
+  },
+};
+
+export const WETHAddress: Record<Network, string> = {
+  [Network.all]: tokens.mainnet.WETH,
+  [Network.mainnet]: tokens.mainnet.WETH,
+  [Network.arbitrum]: tokens.arbitrum.WETH,
+  [Network.optimism]: '',
+};
