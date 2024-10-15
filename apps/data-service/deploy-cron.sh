9edad376219192f7534e04f2dd9073ac7fd57f56
Sime <sbuljac@gmail.com>
2024-10-15 19:35:03 +0200

    feat: remove jobs for points calculation

--- apps/data-service/deploy-cron.sh
+++ apps/data-service/deploy-cron.sh
@@ -4,7 +4,6 @@ set -e
 # Cron jobs to deploy, format is "Description|URL|Schedule"
 cron_jobs="Risk_Service|/calculateRisk|*/10 * * * *
 Sync_Dune|/syncDune|30 1 * * *
-Calculate_Points|/calculatePoints|5 1 * * *
 Sync_Generic_Data|/syncGenericData|0 * * * *
 Sync_Oracle_Data|/syncOracleData|0 * * * *
 Sync_Accounts_Arbitrum|/syncAccounts?network=arbitrum|*/20 * * * *
@@ -19,29 +18,28 @@ echo "Base URI: $BASE_URI"
 
 # Create jobs from cron_jobs array
 echo "$cron_jobs" | while IFS='|' read -r description url schedule; do
-    # Create/update the job
-    job_name=$(echo "$description" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+  # Create/update the job
+  job_name=$(echo "$description" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
 
-    # Check if the job already exists
-    if gcloud scheduler jobs describe "$job_name" --location=us-central1 &>/dev/null; then
-      ACTION="update"
-    else
-      ACTION="create"
-    fi
+  # Check if the job already exists
+  if gcloud scheduler jobs describe "$job_name" --location=us-central1 &>/dev/null; then
+    ACTION="update"
+  else
+    ACTION="create"
+  fi
 
-    gcloud scheduler jobs $ACTION http "$job_name" \
-        --location=us-central1 \
-        --schedule="$schedule" \
-        --uri="${BASE_URI}${url}" \
-        --http-method GET \
-        --attempt-deadline=1800s \
-        --oidc-service-account-email=monitoring-agents@appspot.gserviceaccount.com \
-        --oidc-token-audience="${BASE_URI}" \
-        --description="$description"
-    
-    echo "${ACTION}d job: $job_name $url $schedule"
+  gcloud scheduler jobs $ACTION http "$job_name" \
+    --location=us-central1 \
+    --schedule="$schedule" \
+    --uri="${BASE_URI}${url}" \
+    --http-method GET \
+    --attempt-deadline=1800s \
+    --oidc-service-account-email=monitoring-agents@appspot.gserviceaccount.com \
+    --oidc-token-audience="${BASE_URI}" \
+    --description="$description"
+
+  echo "${ACTION}d job: $job_name $url $schedule"
 done
-
 
 # gcloud run services add-iam-policy-binding cron-service \
 #     --member="serviceAccount:monitoring-agents@appspot.gserviceaccount.com" \
