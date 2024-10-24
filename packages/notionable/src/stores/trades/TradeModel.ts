23f5b78b42934bc510e3523acd0271f901c962cd
Jeff Wu <jeff@notional.finance>
2024-10-24 13:02:47 -0700

    fix: attempt rewrite on trade context

--- packages/notionable/src/stores/trades/TradeModel.ts
+++ packages/notionable/src/stores/trades/TradeModel.ts
@@ -1,84 +1,145 @@
-import { TokenDefinitionModel } from '@notional-finance/core-entities';
+import {
+  NotionalTypes,
+  TokenDefinitionModel,
+} from '@notional-finance/core-entities';
 import { AllTradeTypes } from '@notional-finance/notionable';
 import { types } from 'mobx-state-tree';
 
+export const TokenOptionModel = types.model('TokenOption', {
+  token: types.reference(TokenDefinitionModel),
+  balance: types.maybe(NotionalTypes.TokenBalance),
+  interestRate: types.maybe(types.number),
+  error: types.maybe(types.string),
+  utilization: types.maybe(types.number),
+});
+
 export const TradeModel = types.model('TradeModel', {
-  tradeType: types.enumeration<AllTradeTypes>('TradeType', [
-    'Deposit',
-    'Withdraw',
-    'LendVariable',
-    'LendFixed',
-    'MintNToken',
-    'BorrowVariable',
-    'BorrowFixed',
-    'RepayDebt',
-    'ConvertAsset',
-    'RollDebt',
-  ]),
-
-  availableDepositTokens: types.optional(
-    types.array(types.reference(TokenDefinitionModel)),
-    []
-  ),
+  /** A key into the trade configuration object */
+  tradeType: types.enumeration<AllTradeTypes>('TradeType', ['LendVariable']),
+  /** True if the page is ready to be displayed */
+  isReady: types.optional(types.boolean, false),
+  /** A list of tokens that can be deposited */
+  availableDepositTokens: types.optional(types.array(TokenDefinitionModel), []),
+  /** A list of collateral tokens that can be selected */
   availableCollateralTokens: types.optional(
-    types.array(types.reference(TokenDefinitionModel)),
+    types.array(TokenDefinitionModel),
     []
   ),
-  availableDebtTokens: types.optional(
-    types.array(types.reference(TokenDefinitionModel)),
-    []
-  ),
-  selectedDepositToken: types.maybe(types.string),
-  selectedToken: types.maybe(types.string),
+  /** A list of debt tokens that can be selected */
+  availableDebtTokens: types.optional(types.array(TokenDefinitionModel), []),
+  /** A parameter key set by the url params */
+  sideDrawerKey: types.optional(types.maybe(types.string), undefined),
+  /** Maximum collateral slippage for the vault */
+  maxCollateralSlippage: types.optional(types.maybe(types.number), undefined),
 
-  userInputs: types.optional(
-    types.model({
-      redeemToWETH: types.boolean,
-      maxWithdraw: types.boolean,
-      inputErrors: types.boolean,
-      leverageRatio: types.maybe(types.number),
-    }),
-    {
-      redeemToWETH: false,
-      maxWithdraw: false,
-      inputErrors: false,
-      leverageRatio: undefined,
-    }
+  /** Symbol of the selected deposit token, if any */
+  selectedDepositToken: types.optional(types.maybe(types.string), undefined),
+  /**
+   * Symbol of the selected token on portfolio transaction screens such as withdraw,
+   * repay debt, and roll maturity
+   */
+  selectedToken: types.optional(types.maybe(types.string), undefined),
+  /** NOTE: this is currently unused throughout the site */
+  redeemToWETH: types.optional(types.boolean, false),
+  /** Signals that the input is a max withdraw or max repayment */
+  maxWithdraw: types.optional(types.boolean, false),
+  /** True if there are any input validation errors */
+  inputErrors: types.optional(types.boolean, false),
+
+  /** Selected network for the current trade, the user can change this */
+  selectedNetwork: types.optional(
+    types.maybe(NotionalTypes.Network),
+    undefined
   ),
-  // tokenInputs: types.model({
-  //   collateral: types.maybe(TokenDefinition),
-  //   debt: types.maybe(TokenDefinition),
-  //   deposit: types.maybe(TokenDefinition),
-  //   depositBalance: types.maybe(TokenBalance),
-  //   secondaryDepositBalance: types.maybe(TokenBalance),
-  //   collateralBalance: types.maybe(TokenBalance),
-  //   debtBalance: types.maybe(TokenBalance),
-  // }),
-  transactionState: types.optional(
-    types.model({
-      inputsSatisfied: types.boolean,
-      calculationSuccess: types.boolean,
-      canSubmit: types.boolean,
-      confirm: types.boolean,
-      transactionError: types.maybe(types.string),
-      simulationError: types.maybe(types.string),
-    }),
-    {
-      inputsSatisfied: false,
-      calculationSuccess: false,
-      canSubmit: false,
-      confirm: false,
-      transactionError: undefined,
-      simulationError: undefined,
-    }
+
+  /** Used as a flag to reset the state on path changes */
+  reset: types.optional(types.boolean, false),
+
+  /** The current URL path for tracking URL route updates */
+  pathname: types.optional(types.maybe(types.string), undefined),
+
+  /** Collateral token definition */
+  collateral: types.optional(types.maybe(TokenDefinitionModel), undefined),
+  /** Debt token definition */
+  debt: types.optional(types.maybe(TokenDefinitionModel), undefined),
+  /** Deposit token definition, always in underlying */
+  deposit: types.optional(types.maybe(TokenDefinitionModel), undefined),
+
+  /** Parsed from selected risk factors */
+  leverageRatio: types.maybe(types.number),
+  /** Calculated deposit balance, always in underlying */
+  depositBalance: types.optional(
+    types.maybe(NotionalTypes.TokenBalance),
+    undefined
   ),
-  // transactionSummary: types.model({
-  //   netAssetBalance: types.maybe(TokenBalance),
-  //   netDebtBalance: types.maybe(TokenBalance),
-  //   netRealizedCollateralBalance: types.maybe(TokenBalance),
-  //   netRealizedDebtBalance: types.maybe(TokenBalance),
-  //   postTradeBalances: types.optional(types.array(TokenBalance), []),
-  //   postTradeIncentives: types.optional(types.array(TokenBalance), []),
-  //   // TODO: write all the trade summary fields into the model here...
-  // }),
+  /** A secondary deposit balance, used for NOTE balances in staking */
+  secondaryDepositBalance: types.optional(
+    types.maybe(NotionalTypes.TokenBalance),
+    undefined
+  ),
+  /** Calculated deposit balance, always in `collateral` token denomination */
+  collateralBalance: types.optional(
+    types.maybe(NotionalTypes.TokenBalance),
+    undefined
+  ),
+  /** Calculated deposit balance, always in `debt` token denomination */
+  debtBalance: types.optional(
+    types.maybe(NotionalTypes.TokenBalance),
+    undefined
+  ),
+  /** Calculated fee for creating collateral balance, always in prime cash token denomination */
+  collateralFee: types.optional(
+    types.maybe(NotionalTypes.TokenBalance),
+    undefined
+  ),
+  /** Calculated fee for creating debt balance, always in prime cash token denomination */
+  debtFee: types.optional(types.maybe(NotionalTypes.TokenBalance), undefined),
+  /** Error message from calculation */
+  calculateError: types.maybe(types.string),
+  /** Alternative debt options given if all the inputs are satisfied */
+  debtOptions: types.optional(types.array(TokenOptionModel), []),
+  /** Alternative collateral options given if all the inputs are satisfied */
+  collateralOptions: types.optional(types.array(TokenOptionModel), []),
+
+  /** Default leverage ratio for the selected debt and collateral */
+  defaultLeverageRatio: types.maybe(types.number),
+  /** Minimum allowed leverage ratio (only applies to vaults) */
+  minLeverageRatio: types.maybe(types.number),
+  /** Maximum allowed leverage ratio */
+  maxLeverageRatio: types.maybe(types.number),
+
+  /** True if all the required inputs are satisfied */
+  inputsSatisfied: types.optional(types.boolean, false),
+  /** True if all calculations have been completed */
+  calculationSuccess: types.optional(types.boolean, false),
+  /** True if the calculations are successful and the risk check has completed */
+  canSubmit: types.optional(types.boolean, false),
+  /** True if the form is in the confirmation state */
+  confirm: types.optional(types.boolean, false),
+  /** Transaction call information for the confirmation page */
+  populatedTransaction: types.maybe(types.frozen({})),
+  /** Error creating transaction */
+  transactionError: types.maybe(types.string),
+  /** Simulation error transaction */
+  simulationError: types.maybe(types.string),
+
+  /** Net amount of assets, when rolling refers to the new asset */
+  netAssetBalance: types.maybe(NotionalTypes.TokenBalance),
+  /** Net amount of debts, when rolling refers to the new debt  */
+  netDebtBalance: types.maybe(NotionalTypes.TokenBalance),
+  /** Net cost of assets in underlying terms */
+  netRealizedCollateralBalance: types.maybe(NotionalTypes.TokenBalance),
+  /** Net cost of debts in underlying terms*/
+  netRealizedDebtBalance: types.maybe(NotionalTypes.TokenBalance),
+
+  /** Calculated updates to the account balances post trade */
+  postTradeBalances: types.optional(
+    types.array(NotionalTypes.TokenBalance),
+    []
+  ),
+  /** Calculated incentives to the account balances post trade */
+  postTradeIncentives: types.optional(
+    types.array(NotionalTypes.TokenBalance),
+    []
+  ),
 });
