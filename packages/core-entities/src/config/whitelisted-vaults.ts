1f22fb90b78b9915852da83490254f1c0fc0c601
Jeff Wu <jeff@notional.finance>
2024-10-29 10:28:53 -0700

    fix: add pendle vault addresses (#1240)

--- packages/core-entities/src/config/whitelisted-vaults.ts
+++ packages/core-entities/src/config/whitelisted-vaults.ts
@@ -63,7 +63,11 @@ const PendlePTVaults: Record<Network, string[]> = {
     vaults.arbitrum.Pendle_rsETH_25SEP2024,
     vaults.arbitrum.Pendle_rsETH_26DEC2024,
   ].map(toLowercase),
-  [Network.mainnet]: [],
+  [Network.mainnet]: [
+    vaults.mainnet.Pendle_ezETH_25DEC2024,
+    vaults.mainnet.Pendle_USDe_25DEC2024,
+    vaults.mainnet.Pendle_USDe_26MAR2025,
+  ].map(toLowercase),
   [Network.all]: [],
 };
 
@@ -101,8 +105,6 @@ export const whitelistedVaults = (
         vaults.arbitrum.Aura_rsETH_xWETH,
         vaults.arbitrum.Aura_cbETH_xwstETH_rETH,
         vaults.arbitrum.Convex_tBTC_xWBTC,
-        vaults.arbitrum.Pendle_rsETH_25SEP2024,
-        vaults.arbitrum.Pendle_rsETH_26DEC2024,
       ].map(toLowercase);
   }
 };
