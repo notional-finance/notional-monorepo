8c07ad512dc903908a0bbcd64a9c065a0fb3349e
matthew-garrett <matthew.garrett252@gmail.com>
2024-11-11 10:14:09 -0500

    build: adds maturity to new user boost products table

--- apps/web/src/containers/NewUserView/use-products-table.tsx
+++ apps/web/src/containers/NewUserView/use-products-table.tsx
@@ -6,9 +6,11 @@ import {
   LinkCell,
   IconCell,
   MultiValueIconCell,
+  MultiValueCell,
 } from '@notional-finance/mui';
 import { useAllMarkets, useAppState } from '@notional-finance/notionable-hooks';
 import {
+  formatMaturity,
   formatNumberAsPercent,
   Network,
   PRIME_CASH_VAULT_MATURITY,
@@ -44,6 +46,7 @@ export const useProductsTable = (selectedNetwork: Network) => {
           description={'Product header'}
         />
       ),
+      cell: MultiValueCell,
       accessorKey: 'product',
       textAlign: 'left',
       marginRight: theme.spacing(1.25),
@@ -63,6 +66,7 @@ export const useProductsTable = (selectedNetwork: Network) => {
       enableSorting: true,
       sortingFn: 'basic',
       sortDescFirst: true,
+      width: theme.spacing(16),
     },
     {
       header: (
@@ -87,14 +91,12 @@ export const useProductsTable = (selectedNetwork: Network) => {
         formatNumberAsAbbr(val, 2, baseCurrency, { removeKAbbr: true }),
       accessorKey: 'tvl',
       textAlign: 'right',
-      width: '250px',
     },
     {
       header: '',
       cell: LinkCell,
       accessorKey: 'view',
       textAlign: 'right',
-      width: theme.spacing(14.5),
       marginRight: theme.spacing(1.25),
     },
   ];
@@ -108,9 +110,20 @@ export const useProductsTable = (selectedNetwork: Network) => {
       )
       .map((data) => {
         const { underlying, product, tvl, link, totalAPY } = data;
+
         return {
           currency: underlying.symbol,
-          product: product,
+          product: {
+            data: [
+              { displayValue: product, isNegtive: false },
+              {
+                displayValue: data.token.maturity
+                  ? formatMaturity(data.token.maturity)
+                  : '',
+                isNegtive: false,
+              },
+            ],
+          },
           boostedAPY: totalAPY + 5,
           apy: totalAPY,
           tvl: tvl ? tvl.toFiat(baseCurrency).toFloat() : 0,
