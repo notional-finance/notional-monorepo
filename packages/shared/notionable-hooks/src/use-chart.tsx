cdaf3c570ae3bde45191a46bfcedd067efb04d68
Jeff Wu <jeff@notional.finance>
2025-01-07 14:28:19 -0800

    fix: overview chart

--- packages/shared/notionable-hooks/src/use-chart.tsx
+++ packages/shared/notionable-hooks/src/use-chart.tsx
@@ -240,6 +240,7 @@ export function useAccountHistoryChart(
                   .unwrapVaultToken()
                   .toUnderlying()
                   .toFiat(baseCurrency, floorToMidnight(end))
+                  .neg()
               );
             }, new TokenBalance(0, baseCurrency, Network.all));
 
