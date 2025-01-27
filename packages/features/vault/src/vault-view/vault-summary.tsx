f0cd5e4a1a6715f0f5152570446d5adb72343c73
Jeff Wu <jeff@notional.finance>
2025-01-27 14:51:07 -0800

    fix: incentive apy in model

--- packages/features/vault/src/vault-view/vault-summary.tsx
+++ packages/features/vault/src/vault-view/vault-summary.tsx
@@ -16,8 +16,9 @@ import { TradeActionSummary } from '@notional-finance/trade';
 import { useVaultFaq } from '../hooks';
 import { useParams } from 'react-router-dom';
 import { APYData } from '@notional-finance/core-entities';
+import { observer } from 'mobx-react-lite';
 
-export const VaultSummary = () => {
+export const VaultSummary = observer(() => {
   const theme = useTheme();
   const trade = useCurrentTradeContext();
   const { deposit } = trade?.selectedTokens ?? {};
@@ -109,6 +110,6 @@ export const VaultSummary = () => {
       </Box>
     </Box>
   );
-};
+});
 
 export default VaultSummary;
