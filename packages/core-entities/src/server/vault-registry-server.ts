1cad31f304c989f4cde9c76f1684693b51cc515f
Jeff Wu <jeff@notional.finance>
2025-03-21 12:13:01 -0700

    Bugfix/update rewards and vault name (#1395)
    * fix: update rewards and vault name
    
    * fix: vault name

--- packages/core-entities/src/server/vault-registry-server.ts
+++ packages/core-entities/src/server/vault-registry-server.ts
@@ -45,7 +45,7 @@ function getBaseProtocol(boosterProtocol: string) {
 function parseVaultName(name: string) {
   if (name === 'Curve FRAX/USDC LP (FRAX Leverage)') {
     name = 'SingleSidedLP:Convex:[FRAX]/USDC.e';
-  } else if (name === 'Pendle:PT USDe 28MAY2025:[USDC] ') {
+  } else if (name === 'Pendle:PT USDe 28MAY2025:[USDC]') {
     name = 'Pendle:PT eUSDe 28MAY2025:[USDC]';
   }
 
