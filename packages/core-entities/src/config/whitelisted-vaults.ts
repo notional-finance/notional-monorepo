ea273f7e8ec835d4d79935d5d978c54a3d74f901
Jeff Wu <jeff@notional.finance>
2025-07-30 14:57:43 -0700

    fix: trading parameters

--- packages/core-entities/src/config/whitelisted-vaults.ts
+++ packages/core-entities/src/config/whitelisted-vaults.ts
@@ -248,16 +248,18 @@ export const VaultDefaultDexParameters: Record<
     },
     [vaults.mainnet.Pendle_sUSDe_24SEP2025.toLowerCase()]: {
       dexId: DexIds.CURVE_V2,
+      // On entry, the trade is from USDC to USDe
       depositExchangeData: defaultAbiCoder.encode(
         ['address', 'int128', 'int128'],
         ['0x02950460E2b9529D0E00284A5fA2d7bDF3fA4d72', 1, 0]
       ),
       depositPoolAddress: '0x02950460E2b9529D0E00284A5fA2d7bDF3fA4d72',
+      // On exit, the trade is from DAI to USDC
       redeemExchangeData: defaultAbiCoder.encode(
         ['address', 'int128', 'int128'],
-        ['0x02950460E2b9529D0E00284A5fA2d7bDF3fA4d72', 0, 1]
+        ['0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7', 0, 1]
       ),
-      redeemPoolAddress: '0x02950460E2b9529D0E00284A5fA2d7bDF3fA4d72',
+      redeemPoolAddress: '0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7',
     },
   },
   [Network.all]: {},
