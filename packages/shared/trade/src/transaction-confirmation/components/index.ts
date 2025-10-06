9bb22f0cbabac8f12624bff6e7446f0412f0cc83
Jeff Wu <jeff@notional.finance>
2025-10-06 10:12:17 -0700

    fix: remove portfolio comaprison

--- packages/shared/trade/src/transaction-confirmation/components/index.ts
+++ packages/shared/trade/src/transaction-confirmation/components/index.ts
@@ -1,5 +1,4 @@
 export { PendingTransaction } from './pending-transaction';
 export { StatusHeading } from './status-heading';
 export { TransactionButtons } from './transaction-buttons';
-export { PortfolioCompare } from './portfolio-compare';
 export { OrderDetails } from './order-details';
