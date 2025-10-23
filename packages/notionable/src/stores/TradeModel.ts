9966d7b838be99dc66d345b74aacdda54a121253
Jeff Wu <jeff@notional.finance>
2025-10-23 12:55:09 -0700

    fix: average apy data

--- packages/notionable/src/stores/TradeModel.ts
+++ packages/notionable/src/stores/TradeModel.ts
@@ -947,30 +947,86 @@ export const TradeModel = types
           model.getSpotAPY(self.collateral.id)
         : undefined;
 
-      const updatedAPY =
-        holdings && self.tradeType === 'InitiateWithdraw'
-          ? // Clear all the asset APY for a withdraw
-            createLeveragedAPYData(
+      let updatedAPY: APYData | undefined;
+      if (holdings && self.tradeType === 'InitiateWithdraw') {
+        // Clear all the asset APY for a withdraw
+        updatedAPY = createLeveragedAPYData(
+          {
+            totalAPY: 0,
+            assetAPY: 0,
+            organicAPY: 0,
+            incentiveAPY: 0,
+            feeAPY: 0,
+            incentives: [],
+            pointMultiples: {},
+          },
+          currentAPY?.debtAPY || 0,
+          holdings.leverageRatio
+        );
+      } else if (postVaultRisk && self.collateralBalance && self.debtBalance) {
+        const netPositionAPY = model.getLeveragedAPY(
+          self.collateralBalance,
+          self.debtBalance,
+          postVaultRisk.leverageRatio() || 0,
+          self.vaultTradeMetadata
+        );
+
+        if (
+          priorVaultRisk &&
+          (self.tradeType === 'AdjustVaultLeverage' ||
+            self.tradeType === 'IncreaseVaultPosition')
+        ) {
+          // Average into the updated apy
+          updatedAPY = {
+            ...netPositionAPY,
+            totalAPY: averageAPY([
               {
-                totalAPY: 0,
-                assetAPY: 0,
-                organicAPY: 0,
-                incentiveAPY: 0,
-                feeAPY: 0,
-                incentives: [],
-                pointMultiples: {},
+                apy: currentAPY?.totalAPY || 0,
+                amount: priorVaultRisk.vaultShares,
               },
-              currentAPY?.debtAPY || 0,
-              holdings.leverageRatio
-            )
-          : postVaultRisk
-          ? model.getLeveragedAPY(
-              postVaultRisk.vaultShares,
-              postVaultRisk.vaultDebt,
-              postVaultRisk.leverageRatio() || 0,
-              self.vaultTradeMetadata
-            )
-          : undefined;
+              {
+                apy: netPositionAPY.totalAPY || 0,
+                amount: self.collateralBalance,
+              },
+            ]),
+            assetAPY: averageAPY([
+              {
+                apy: currentAPY?.assetAPY || 0,
+                amount: priorVaultRisk.vaultShares,
+              },
+              {
+                apy: netPositionAPY.assetAPY || 0,
+                amount: self.collateralBalance,
+              },
+            ]),
+            organicAPY: averageAPY([
+              {
+                apy: currentAPY?.organicAPY || 0,
+                amount: priorVaultRisk.totalAssets(),
+              },
+              {
+                apy: netPositionAPY.organicAPY || 0,
+                amount: self.collateralBalance,
+              },
+            ]),
+            incentiveAPY: averageAPY([
+              {
+                apy: currentAPY?.incentiveAPY || 0,
+                amount: priorVaultRisk.totalAssets(),
+              },
+              {
+                apy: netPositionAPY.incentiveAPY || 0,
+                amount: self.collateralBalance,
+              },
+            ]),
+          };
+        } else if (
+          self.tradeType === 'CreateVaultPosition' ||
+          self.tradeType === 'RollVaultPosition'
+        ) {
+          updatedAPY = netPositionAPY;
+        }
+      }
 
       return {
         leveragedAPY: (updatedAPY || currentAPY || undefined) as
@@ -1345,4 +1401,20 @@ function sortByMaturity<T extends { maturity?: number }>(a: T, b: T) {
       ? 0
       : b.maturity)
   );
+}
+
+function averageAPY(
+  apys: {
+    apy: number;
+    amount: TokenBalance;
+  }[]
+): number {
+  const totalAmount = apys.reduce(
+    (acc, curr) => acc + curr.amount.toFloat(),
+    0
+  );
+  const weightedApys = apys.map(
+    ({ apy, amount }) => (apy * amount.toFloat()) / totalAmount
+  );
+  return weightedApys.reduce((acc, curr) => acc + curr, 0);
 }
