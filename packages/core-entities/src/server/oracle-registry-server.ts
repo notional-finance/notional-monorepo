22a392036a5d2c6578f0c0254ada4ce25cb4a046
Jeff Wu <jeff@notional.finance>
2025-10-30 11:43:11 -0700

    Feat/defi llama oracles (#1458)
    * fix: attempt to replace oracles with defi llama
    
    * fix: update pt return types

--- packages/core-entities/src/server/oracle-registry-server.ts
+++ packages/core-entities/src/server/oracle-registry-server.ts
@@ -15,12 +15,7 @@ import {
   ZERO_ADDRESS,
 } from '@notional-finance/util';
 import { BigNumber, Contract, ethers } from 'ethers';
-import {
-  OracleDefinition,
-  CacheSchema,
-  SNOTEWeightedPool,
-  PendlePTVaults,
-} from '..';
+import { OracleDefinition, CacheSchema, SNOTEWeightedPool } from '..';
 import { loadGraphClientDeferred, ServerRegistry } from './server-registry';
 import { fiatOracles } from '../config/fiat-config';
 import { TypedDocumentNode } from '@apollo/client/core';
@@ -28,6 +23,7 @@ import { TypedDocumentNode } from '@apollo/client/core';
 import { AllOraclesQuery } from '../.graphclient';
 import { Block } from '@ethersproject/providers';
 import { fetchFromRegistry } from '../client';
+import { parseUnits } from 'ethers/lib/utils';
 
 // NOTE: this is currently hardcoded because we cannot access the worker
 // process environment directly here.
@@ -40,6 +36,7 @@ const VaultOracleABI = new ethers.utils.Interface([
   'function convertBorrowSharesToAssets(address vault, uint256 shares) view external returns (uint256)',
   // This one is on the withdraw request manager
   'function getExchangeRate() view external returns (uint256)',
+  'function CURVE_POOL_TOKEN() view external returns (address)',
 ]);
 
 const sNOTE_Pool = '0x5122E01D819E58BB2E22528c0D68D310f0AA6FD7';
@@ -145,11 +142,9 @@ export class OracleRegistryServer extends ServerRegistry<OracleDefinition> {
               base: v.base.id,
               baseDecimals: v.base.decimals,
               quote: v.quote.id,
-              decimals:
-                // Override PT vault addresses b/c the decimals are not right in the subgraph
-                PendlePTVaults[network].includes(v.oracleAddress)
-                  ? 18
-                  : v.decimals,
+              decimals: v.decimals,
+              yieldToken: v.quote.vaultAddress?.yieldToken?.id,
+              strategyType: v.quote.vaultAddress?.strategyType,
               latestRate: {
                 rate: BigNumber.from(v.latestRate),
                 timestamp: v.lastUpdateTimestamp,
@@ -158,7 +153,7 @@ export class OracleRegistryServer extends ServerRegistry<OracleDefinition> {
             };
 
             return obj;
-          }, {} as Record<string, OracleDefinition>);
+          }, {} as Record<string, OracleDefinition & { yieldToken?: string; strategyType?: string }>);
         },
         this.env.NX_SUBGRAPH_API_KEY,
         {
@@ -201,7 +196,8 @@ export class OracleRegistryServer extends ServerRegistry<OracleDefinition> {
     schema: CacheSchema<OracleDefinition>,
     blockNumber?: number
   ): Promise<CacheSchema<OracleDefinition>> {
-    const calls = await this._getAggregateCalls(schema);
+    const realTimePrices = await this._getPriceFromDefiLlama(schema);
+    const calls = await this._getOnChainCalls(schema, realTimePrices);
     const batchedCalls = batchArray(calls, 100);
 
     let block: Block | undefined;
@@ -211,7 +207,8 @@ export class OracleRegistryServer extends ServerRegistry<OracleDefinition> {
         rate: BigNumber;
         timestamp?: number | undefined;
       }
-    > = {};
+    > = realTimePrices;
+
     for (const b of batchedCalls) {
       const { block: _b, results: _r } = await aggregate(
         b,
@@ -248,15 +245,111 @@ export class OracleRegistryServer extends ServerRegistry<OracleDefinition> {
     };
   }
 
+  private async _getPriceFromDefiLlama(
+    results: CacheSchema<
+      OracleDefinition & { yieldToken?: string; strategyType?: string }
+    >
+  ): Promise<Record<string, { rate: BigNumber; timestamp: number }>> {
+    const provider = this.getProvider(results.network);
+    const poolTokens = await aggregate<string>(
+      results.values
+        .filter(
+          ([_, oracle]) =>
+            oracle?.oracleType === 'VaultShareOracleRate' &&
+            oracle?.strategyType === 'CurveConvex2Token'
+        )
+        .map(([_, oracle]) => {
+          if (!oracle || !oracle.yieldToken) return null;
+
+          return {
+            key: oracle.yieldToken,
+            target: new Contract(
+              oracle.oracleAddress,
+              VaultOracleABI,
+              provider
+            ),
+            method: 'CURVE_POOL_TOKEN',
+            args: [],
+          };
+        })
+        .filter((v) => v !== null) as AggregateCall[],
+      provider
+    ).then(({ results: r }) => r);
+
+    // All vaults have a Chainlink oracle, so we get them here.
+    const coins = results.values
+      .filter(
+        ([_, oracle]) =>
+          oracle?.oracleType === 'Chainlink' && oracle.id !== 'UNIT_RATE'
+      )
+      .map(([id, oracle]) => {
+        if (!oracle) return null;
+        // If there is a pool token it is because we are rewriting the Convex token
+        // to the LP pool token.
+        const coin = poolTokens[oracle.quote] || oracle.quote;
+        if (!coin) return null;
+
+        return {
+          id,
+          coin: `${
+            oracle.network === Network.mainnet ? 'ethereum' : oracle.network
+          }:${coin}`,
+        };
+      })
+      .filter((v) => v !== null);
+
+    const url = `https://coins.llama.fi/prices/current/${coins
+      .map((c) => c.coin)
+      .join(',')}`;
+
+    // All of these prices are in USD, so we need to convert them using the oracle decimals
+    const prices: {
+      coins: { [key: string]: { price: number; timestamp: number } };
+    } = (await fetch(url)
+      .then((r) => r.json())
+      .catch((e) => {
+        console.error('Error fetching prices from DefiLlama', e);
+        return {
+          coins: {},
+        };
+      })) as unknown as {
+      coins: { [key: string]: { price: number; timestamp: number } };
+    };
+
+    // All these prices are to USD.
+    return coins
+      .map((c) => {
+        const quotePrice = prices.coins[c.coin]?.price;
+        const oracle = results.values.find(([id, _]) => id === c.id)?.[1];
+        if (!quotePrice || !oracle) return null;
+
+        // Scale the floating point number to BigNumber with specified decimal precision
+        const formattedPrice = Number(quotePrice).toFixed(oracle.decimals);
+        return {
+          id: c.id,
+          rate: parseUnits(formattedPrice, oracle.decimals),
+          timestamp: prices.coins[c.coin]?.timestamp,
+        };
+      })
+      .filter((v) => v !== null)
+      .reduce((acc, v) => {
+        acc[v.id] = v;
+        return acc;
+      }, {} as Record<string, { rate: BigNumber; timestamp: number }>);
+  }
+
   /** Returns an array of aggregate calls that will override the latest rates in the oracles */
-  private async _getAggregateCalls(
-    results: CacheSchema<OracleDefinition>
+  private async _getOnChainCalls(
+    results: CacheSchema<OracleDefinition>,
+    realTimePrices: Record<string, { rate: BigNumber; timestamp: number }>
   ): Promise<AggregateCall<{ rate: BigNumber; timestamp?: number }>[]> {
     const provider = this.getProvider(results.network);
 
     return results.values
       .map(([id, oracle]) => {
         if (!oracle) return null;
+        // Prefer real time prices over on chain prices
+        if (realTimePrices[id]) return null;
 
         if (
           oracle.oracleType === 'Chainlink' &&
