e965da36dacbbff358b6e347a5da1028618502ee
T-Woodward <50867520+T-Woodward@users.noreply.github.com>
2025-10-31 11:45:03 -0400

    feat: whitelisting v2 liquidator (#1460)
    * feat: whitelisting v2 liquidator
    
    * Update Signer.ts
    
    * Update config.ts

--- apps/tx-relay/src/config.ts
+++ apps/tx-relay/src/config.ts
@@ -36,7 +36,7 @@ export const Address = {
     '0x319300462C37AD2D4f26B584C2b67De51F51f289',
   initializeAllMarkets_MAINNET: '0x40a77E61c230BB3B847231E3F47E47c6c62257db',
   settleAccounts_MAINNET: '0x04f917B9772920BC7E39Da36b465395A7B8E893C',
-  exponentLiquidator_MAINNET: '0x65b4ede768852C0359bAc066eC65CD2e1767eE75',
+  exponentLiquidator_MAINNET: '0x3d3DD434c6fd94FBd20e1d0649595Cc7612fFBeB',
 };
 
 export enum Sign {
@@ -54,5 +54,5 @@ export enum Sign {
   settleAccounts = '0xd7f4893e',
   settleVaultsAccounts = '0x8261f4ba',
   checkAndRebalance = '0x3ed55f0c',
-  flashLiquidateExponent = '0x0a4f8685',
+  flashLiquidateExponent = '0xfaa31da8',
 }
