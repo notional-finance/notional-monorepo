be52b23215ceb1bb624cdcf00514927ba221c119
Jeff Wu <jeff@notional.finance>
2026-02-03 10:29:41 -0800

    fix: datetime

--- apps/web/src/containers/StakeNOTE/use-reinvestment-data.tsx
+++ apps/web/src/containers/StakeNOTE/use-reinvestment-data.tsx
@@ -39,7 +39,7 @@ export const useReinvestmentData = () => {
   const model = getNetworkModel(Network.mainnet);
   const snoteData = model.getSNOTEReinvestment()?.map((r) => ({
     ...r,
-    day: new Date(r.day).getTime(),
+    day: new Date(r.day).getTime() / 1000,
     txnHash: {
       href: getEtherscanTransactionLink(r.transaction_hash, Network.mainnet),
       hash: r.transaction_hash,
