b519f47ba9181f329e3bd6b80342b2d374a0b4d4
Jeff Wu <jeff@notional.finance>
2026-03-10 10:42:00 -0700

    fix: fetching analytics views

--- apps/registry/src/views-helpers.ts
+++ apps/registry/src/views-helpers.ts
@@ -2,7 +2,10 @@ import { AnalyticsServer } from '@notional-finance/core-entities/src/server/anal
 import { putStorageKey } from './registry-helpers';
 import { BaseDOEnv } from '.';
 import { Network } from '@notional-finance/util';
-import { destroyGraphClient } from '@notional-finance/core-entities';
+import {
+  AnalyticsData,
+  destroyGraphClient,
+} from '@notional-finance/core-entities';
 
 async function fetchDBView(env: BaseDOEnv, network: Network, name: string) {
   try {
@@ -44,6 +47,15 @@ async function fetchAllDBViews(env: BaseDOEnv, network: Network) {
   return vaultAddresses;
 }
 
+export function getFetchView(env: BaseDOEnv) {
+  return async (network: Network, view: string) => {
+    return env.VIEW_CACHE_R2.get(`${network}/views/${view}`).then((res) => {
+      if (!res) throw new Error(`View ${view} not found`);
+      return res.json() as Promise<AnalyticsData>;
+    });
+  };
+}
+
 export async function refreshViews(env: BaseDOEnv, network: Network) {
   const analyticsServer = new AnalyticsServer(env);
 
@@ -51,7 +63,8 @@ export async function refreshViews(env: BaseDOEnv, network: Network) {
   // Saves time series data to R2 for the registry to serve
   const { timeSeries, priceChanges } = await analyticsServer.fetchTimeSeries(
     network,
-    vaultAddresses
+    vaultAddresses,
+    getFetchView(env)
   );
 
   await putStorageKey(
