2902ecdd65e72212e04cb002c59989020ce73b6e
Jeff Wu <jeff@notional.finance>
2026-04-15 04:41:45 -0700

    fix: repay code

--- apps/web/src/containers/TransactionScreen/screens/repay.tsx
+++ apps/web/src/containers/TransactionScreen/screens/repay.tsx
@@ -2,16 +2,50 @@ import { useCurrencyInputRef } from '@notional-finance/mui';
 import { DepositInput } from '@notional-finance/trade';
 import { TransactionScreen } from '../transaction-screen';
 import { observer } from 'mobx-react-lite';
-import { defineMessage } from 'react-intl';
+import { defineMessage, MessageDescriptor } from 'react-intl';
 import {
   useCurrentTradeContext,
   useTradeContext,
 } from '@notional-finance/notionable-hooks';
+import { TokenBalance } from '@notional-finance/core-entities';
+import { useState } from 'react';
+
+const insufficientBalanceMsg = defineMessage({
+  defaultMessage: 'Insufficient balance',
+});
+
+const overMaxRepayAmountMsg = defineMessage({
+  defaultMessage: 'Over max repay amount',
+});
 
 export const VaultRepayScreen = observer(() => {
   const { currencyInputRef } = useCurrencyInputRef();
   useTradeContext('RepayVault');
   const trade = useCurrentTradeContext();
+  const maxWalletBalance = useWalletBalances(trade?.selectedNetwork);
+  const maxRepayAmount = trade?.getVaultMaxRepayAmount();
+  const [balanceError, setBalanceError] = useState<
+    MessageDescriptor | undefined
+  >(undefined);
+
+  const onUpdate = (inputAmount: TokenBalance | undefined) => {
+    trade?.setDepositBalance(inputAmount, false);
+
+    if (inputAmount && inputAmount.gt(maxWalletBalance)) {
+      setBalanceError(insufficientBalanceMsg);
+    } else if (inputAmount && inputAmount.gt(maxRepayAmount)) {
+      setBalanceError(overMaxRepayAmountMsg);
+    } else {
+      setBalanceError(undefined);
+    }
+  };
+
+  const onMaxValue = () => {
+    trade?.setDepositBalance(
+      maxRepayAmount.lt(maxWalletBalance) ? maxRepayAmount : maxWalletBalance,
+      true
+    );
+  };
 
   return (
     <TransactionScreen
@@ -25,6 +59,9 @@ export const VaultRepayScreen = observer(() => {
           })}
           inputRef={currencyInputRef}
           depositTokens={trade?.availableTokens.deposit}
+          onUpdate={onUpdate}
+          onMaxValue={onMaxValue}
+          errorMsgOverride={balanceError}
         />,
       ]}
     />
